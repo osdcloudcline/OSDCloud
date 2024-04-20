5f8868436f8e61f181eec56814d05770a0f78992
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-04-20 00:14:20 -0400

    Update OSKitsDL.ps1

--- OS Kits/OSKitsDL.ps1
+++ OS Kits/OSKitsDL.ps1
@@ -4,23 +4,23 @@ Write-Host "Accessing OSD Cloud\OS Kits GitHub Repo from OSDCloudCline to downlo
 
 Write-Host "Processing and Downloading File 1 of 5: Windows 11 22H2 SDK Setup File..." -ForegroundColor Cyan
 $Win11_22H2SDKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/winsdksetup.exe' -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $Win11_22H2SDKUrl -DestinationDirectory $OSDCloudGHdownloads
 
 Write-Host "Processing and Downloading File 2 of 5: Windows 11 22H2 ADK Setup File..." -ForegroundColor Cyan
-$Win11_22H2SDKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe' -DestinationDirectory $OSDCloudGHdownloads
+$Win11_22H2ADKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adksetup.exe'
+Save-WebFile -SourceUrl $Win11_22H2ADKUrl -DestinationDirectory $OSDCloudGHdownloads
 
 Write-Host "Processing and Downloading File 3 of 5: Windows 11 22H2 PE ADK Add-On Setup File..." -ForegroundColor Cyan
 $Win11_22H2PEADKUrl = 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adkwinpesetup.exe'
-Save-WebFile 'https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/adkwinpesetup.exe' -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $Win11_22H2PEADKUrl -DestinationDirectory $OSDCloudGHdownloads
 
 Write-Host "Processing and Downloading File 4 of 5: MSDaRT Setup File..." -ForegroundColor Cyan
 $MSDARTURL = "https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/MSDaRT100.msi"
-Save-WebFile "https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/MSDaRT100.msi" -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $MSDARTURL -DestinationDirectory $OSDCloudGHdownloads
 
 Write-Host "Processing and Downloading File 5 of 5: MDT Setup File..." -ForegroundColor Cyan
 $MDTURL = "https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/MicrosoftDeploymentToolkit_x64.msi"
-Save-WebFile "https://github.com/osdcloudcline/OSDCloud/raw/main/OS%20Kits/MicrosoftDeploymentToolkit_x64.msi" -DestinationDirectory $OSDCloudGHdownloads
+Save-WebFile -SourceUrl $MDTURL -DestinationDirectory $OSDCloudGHdownloads
 
 
 Write-Host "Completed All Downloads..." -ForegroundColor Green
