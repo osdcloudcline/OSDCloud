8a41380130b26c4fe9ac4d84c69b9636074110ac
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-04 09:19:59 -0400

    Update OtherTemplatesMain.ps1

--- Scripts/Templates/OtherTemplatesMain.ps1
+++ Scripts/Templates/OtherTemplatesMain.ps1
@@ -1,1 +1,61 @@
+##########################################################
+# Main Menu
+##########################################################
+Function Show-MainMenu{
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'OSD Cloud Initial Template Creation - Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
 
+Clear-Host
+Write-Host "======= $Title ======"
+Write-Host " 1. HP Laptops"
+Write-Host " 2. ASUS Laptops"
+Write-Host " 3. Acer Laptops"
+Write-Host " 4. Lenovo Laptops"
+Write-Host " 5. Virtual Machines"
+Write-Host " 6. MS Surface Devices"
+Write-Host " 7. Return to Main Menu"
+
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+
+  '1' { cls
+        $HPTemplateConfigScript = Invoke-WebRequest("")
+        Invoke-Expression $($HPTemplateConfigScript.Content)
+        }
+  '2' { cls
+        $ASUSTemplateConfigScript = Invoke-WebRequest("")
+        Invoke-Expression $($ASUSTemplateConfigScript.Content)
+        }
+  '3' { cls
+        $AcerTemplateConfigScript = Invoke-WebRequest("")
+        Invoke-Expression $($AcerTemplateConfigScript.Content)
+        }
+  '4' { cls
+        $LenovoTemplateConfigScript = Invoke-WebRequest("")
+        Invoke-Expression $($LenovoTemplateConfigScript.Content)
+        }
+  '5' { cls
+         $ESXIVMTemplateConfigScript = Invoke-WebRequest("")
+        Invoke-Expression $($ESXIVMTemplateConfigScript.Content)
+        }
+  '6' { cls
+        $MSSurfaceTemplateConfigScript = Invoke-WebRequest("")
+        Invoke-Expression $($MSSurfaceTemplateConfigScript.Content)
+        }
+  '7'{cls
+        $OSDMain = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/OSDMain.ps1")
+        Invoke-Expression $($OSDMain.Content)
+       }
+    }
+    }
+     until ($selection -eq '7'){Invoke-Expression $($OSDMain.Content)}
+    }
+
+    
+Show-MainMenu
