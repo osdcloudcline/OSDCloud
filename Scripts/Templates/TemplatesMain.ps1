8602947108d046b062f51ba089f09edffef52416
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-04 09:21:02 -0400

    Update TemplatesMain.ps1

--- Scripts/Templates/TemplatesMain.ps1
+++ Scripts/Templates/TemplatesMain.ps1
@@ -13,12 +13,6 @@ Write-Host "======= $Title ======"
 Write-Host " 1. Dell Laptops"
 Write-Host " 2. Dell Desktops"
 Write-Host " 3. Custom Built Desktops + AMD/Intel Motherboards"
-Write-Host " 4. HP Laptops"
-Write-Host " 5. ASUS Laptops"
-Write-Host " 6. Acer Laptops"
-Write-Host " 7. Lenovo Laptops"
-Write-Host " 8. Virtual Machines"
-Write-Host " 9. MS Surface Devices"
 Write-Host " 10. Return to Main Menu"
 
 do 
@@ -39,37 +33,13 @@ do
         $CustomPCOSDCloudConfigScript = Invoke-WebRequest ("https://github.com/osdcloudcline/Scripts/raw/main/OSD%20Cloud/ISO%20Files%20/Desktops/Custom%20Build%20/CustomBuildConfigureOSDCloudMain.ps1")
         Invoke-Expression $($CustomPCOSDCloudConfigScript.Content)
         }
-  '4' { cls
-        $HPLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
-        Invoke-Expression $($HPLaptopsOSDCloudConfigScript.Content)
-        }
-  '5' { cls
-        $ASUSLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
-        Invoke-Expression $($ASUSLaptopsOSDCloudConfigScript.Content)
-        }
-  '6' { cls
-        $AcerLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
-        Invoke-Expression $($AcerLaptopsOSDCloudConfigScript.Content)
-        }
-  '7' { cls
-        $LenovoLaptopsOSDCloudConfigScript = Invoke-WebRequest ""
-        Invoke-Expression $($LenovoLaptopsOSDCloudConfigScript.Content)
-        }
-  '8' { cls
-         $ESXIVMOSDCloudConfigScript = Invoke-WebRequest ""
-        Invoke-Expression $($ESXIVMOSDCloudConfigScript.Content)
-        }
-  '9' { cls
-        $MSSurfaceOSDCloudConfigScript = Invoke-WebRequest ""
-        Invoke-Expression $($MSSurfaceOSDCloudConfigScript.Content)
-        }
-   '10'{cls
+   '4'{cls
         $OSDMain = Invoke-WebRequest("https://github.com/osdcloudcline/Scripts/raw/main/OSDMain.ps1")
         Invoke-Expression $($OSDMain.Content)
        }
     }
     }
-     until ($selection -eq '10'){Invoke-Expression $($OSDMain.Content)}
+     until ($selection -eq '4'){Invoke-Expression $($OSDMain.Content)}
     }
 
     
