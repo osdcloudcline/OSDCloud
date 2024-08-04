5108b490b4c8a604c7b3f73f924919f794e516aa
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-08-04 01:25:18 -0400

    Update Dell-Laptops.ps1

--- Scripts/Templates/OEM/Dell-Laptops.ps1
+++ Scripts/Templates/OEM/Dell-Laptops.ps1
@@ -2,7 +2,7 @@ $DellLaptopTemplateLog = "C:\Logs\OSDCloud\Templates\OEM\Dell\Laptops.log"
 
 Start-Transcript -Path $DellLaptopTemplateLog
 
-Function Show-DellAlienware(){
+
 $AlienwareWorkspace = "C:\OSDCloud\Laptops\Dell\Alienware"
 Write-Verbose "Creating new OSD Cloud Workspace for Dell Alienware laptops..."
 New-OSDCloudWorkspace -WorkspacePath $AlienwareWorkspace
@@ -10,9 +10,9 @@ Get-OSDCloudWorkspace
 Write-Verbose "Creating New OSD Cloud Template for Dell Alienware Laptops..." -Verbose
 New-OSDCloudTemplate -Name 'Dell Alienware Laptops'
 Write-Verbose "Finished creating Dell Alienware Laptop Template..." -Verbose
-}
 
-Function Show-DellGSeries(){
+
+
 $GSeriesWorkspace = "C:\OSDCloud\Laptops\Dell\G-Series"
 Write-Verbose "Creating new OSD Cloud Workspace for Dell G-Series laptops..."
 New-OSDCloudWorkspace -WorkspacePath $GSeriesWorkspace
@@ -20,9 +20,9 @@ Get-OSDCloudWorkspace
 Write-Verbose "Creating New OSD Cloud Template for Dell G-Series Laptops..." -Verbose
 New-OSDCloudTemplate -Name 'Dell G-Series Laptops'
 Write-Verbose "Finished creating Dell G-Series Laptop Template..." -Verbose
-}
 
-Function Show-DellXPS(){
+
+
 $XPSWorkspace = "C:\OSDCloud\Laptops\Dell\XPS"
 Write-Verbose "Creating new OSD Cloud Workspace for Dell XPS laptops..."
 New-OSDCloudWorkspace -WorkspacePath $XPSWorkspace
@@ -30,9 +30,9 @@ Get-OSDCloudWorkspace
 Write-Verbose "Creating New OSD Cloud Template for Dell XPS Laptops..." -Verbose
 New-OSDCloudTemplate -Name 'Dell XPS Laptops'
 Write-Verbose "Finished creating Dell XPS Laptop Template..." -Verbose
-}
 
-Function Show-DellInspiron(){
+
+
 $InspironWorkspace = "C:\OSDCloud\Laptops\Dell\Inspiron"
 Write-Verbose "Creating new OSD Cloud Workspace for Dell Inspiron laptops..."
 New-OSDCloudWorkspace -WorkspacePath $InspironWorkspace
@@ -40,9 +40,9 @@ Get-OSDCloudWorkspace
 Write-Verbose "Creating New OSD Cloud Template for Dell Inspiron Laptops..." -Verbose
 New-OSDCloudTemplate -Name 'Dell Inspiron Laptops'
 Write-Verbose "Finished creating Dell Inspiron Laptop Template..." -Verbose
-}
 
-Function Show-DellLatitude(){
+
+
 $LatitudeWorkspace = "C:\OSDCloud\Laptops\Dell\Latitude"
 Write-Verbose "Creating new OSD Cloud Workspace for Dell Latitude laptops..."
 New-OSDCloudWorkspace -WorkspacePath $LatitudeWorkspace
@@ -50,19 +50,6 @@ Get-OSDCloudWorkspace
 Write-Verbose "Creating New OSD Cloud Template for Dell Latitude Laptops..." -Verbose
 New-OSDCloudTemplate -Name 'Dell Latitude Laptops'
 Write-Verbose "Finished creating Dell Latitude Laptop Template..." -Verbose
-}
-
-Show-DellAlienware
-
-Show-DellGSeries
-
-Show-DellXPS
-
-Show-DellInspiron
-
-Show-DellLatitude
-
-
 
 
 Stop-Transcript
