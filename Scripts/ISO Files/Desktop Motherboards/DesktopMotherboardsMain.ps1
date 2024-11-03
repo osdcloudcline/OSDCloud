9b8189b23c4cdd7dd18a0123dca9d7e71419be25
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-03 00:36:27 -0400

    Update DesktopMotherboardsMain.ps1

--- Scripts/ISO Files/Desktop Motherboards/DesktopMotherboardsMain.ps1
+++ Scripts/ISO Files/Desktop Motherboards/DesktopMotherboardsMain.ps1
@@ -125,9 +125,8 @@ do
     Invoke-Expression $($AsrockMotherboards.Content)
     }
 '5'{cls
-    $OSDHelp = "C:\OSDCloud\Help\Commands\OSD\OSDFunctions.txt"
-    Get-Command -Module OSD | Out-File -FilePath $OSDHelp
-    Start-Process -FilePath $OSDHelp
+    Get-Command -Module OSD | Out-File -FilePath "C:\OSDCloud\Help\Commands\OSD\OSDFunctions.txt"
+    Start-Process -FilePath "C:\OSDCloud\Help\Commands\OSD\OSDFunctions.txt"
     pause
     Show-MainMenu
     }
