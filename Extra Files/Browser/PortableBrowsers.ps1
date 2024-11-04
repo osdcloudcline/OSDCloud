6e6c99d049d3d59ecc954abd28d380298cf9b681
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 17:23:13 -0500

    Create PortableBrowsers.ps1

---
+++ Extra Files/Browser/PortableBrowsers.ps1
@@ -0,0 +1,8 @@
+Write-Host '                                                                              '
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' The following extras will be integrated into OSDCloud boot.wim:         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Google Chrome Portable Web Browser                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+Write-Host '                                                                              '
+pause
+
