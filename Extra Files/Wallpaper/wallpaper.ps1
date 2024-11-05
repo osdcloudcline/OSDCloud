bcee2338a4525719d30547e1d7a7fa032f0f245c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 20:52:23 -0500

    Update wallpaper.ps1

--- Extra Files/Wallpaper/wallpaper.ps1
+++ Extra Files/Wallpaper/wallpaper.ps1
@@ -5,3 +5,10 @@ Write-Host '     - Custom OSD Cloud Wallpaper                                   
 Write-Host '                                                                              ' -BackgroundColor White
 Write-Host '                                                                              '
 pause
+
+
+######################################################################################
+####    OSDCloud Wallpaper URL                                                 #######
+######################################################################################
+$OSDCloudWallpaperURL = "https://github.com/osdcloudcline/OSDCloud/blob/main/Extra%20Files/Wallpaper/winpe.jpg"
+
