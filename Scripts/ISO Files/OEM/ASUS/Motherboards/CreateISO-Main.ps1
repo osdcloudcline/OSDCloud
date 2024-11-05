6f2866378d020129a78b52c6fc85c8f4320cefea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 08:50:33 -0500

    Update CreateISO-Main.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/CreateISO-Main.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/CreateISO-Main.ps1
@@ -1,1 +1,61 @@
+$Date = Get-Date
+Write-Host "Today is:" "$Date"
+Write-Host
+Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
+Write-Host
+Write-Host 
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host '            This OSDCloud section allows the user to perform:                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Creating ISO images for Desktop motherboards                           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '              for the following manufacturers:                                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASUS: Intel LGA 1700/LGA 1851 and AMD AM5/AM4 socket types             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+pause
+Clear-Host
 
+Write-Host "======= $Title ======"
+Write-Host " 1. OSDCloud: Create ISO - ASUS Intel LGA 1851"
+Write-Host " 2. OSDCloud: Create ISO - ASUS Intel LGA 1700"
+Write-Host " 3. OSDCloud: Create ISO - ASUS AMD AM 5"
+Write-Host " 4. OSDCloud: Create ISO - ASUS AMD AM 4"
+Write-Host " 5. OSDCloud: Get Help"
+Write-Host " 6. Return to Main Menu"
+
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+'1'{cls
+    $LGA1851 = Invoke-WebRequest("")
+    Invoke-Expression $($LGA1851.Content)
+    }
+'2'{cls
+    $LGA1700 = Invoke-WebRequest("")
+    Invoke-Expression $($LGA1700.Content)
+    }
+'3'{cls
+    $AM5 = Invoke-WebRequest("")
+    Invoke-Expression $($AM5.Content)
+    }
+'4'{cls
+    $AM4 = Invoke-WebRequest("")
+    Invoke-Expression $($AM4.Content)
+    }
+'5'{cls
+    Get-Command -Module OSD | Out-File -FilePath "C:\OSDCloud\Help\Commands\OSD\OSDFunctions.txt"
+    Start-Process -FilePath "C:\OSDCloud\Help\Commands\OSD\OSDFunctions.txt"
+    pause
+    Show-MainMenu
+    }
+'6'{cls
+    $OSDCloudMain = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/NEW-OSDCloudMain.ps1")
+    Invoke-Expression $($OSDCloudMain.Content)
+   }
+    }
+    }
+     until ($selection -eq '6'){Invoke-Expression $($OSDCloudMain.Content)}
+    }
+
+    
+Show-MainMenu
