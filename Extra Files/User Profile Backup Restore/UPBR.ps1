3a1cf0a524d7099e78995ddf4670ac6a9440efc2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 20:45:36 -0500

    Update UPBR.ps1

--- Extra Files/User Profile Backup Restore/UPBR.ps1
+++ Extra Files/User Profile Backup Restore/UPBR.ps1
@@ -1,1 +1,9 @@
+Write-Host '                                                                              '
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' The following extra files will be integrated into OSDCloud boot.wim:         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Operating System User Profile Backup and Restore                       ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+Write-Host '                                                                              '
+pause
+
 
