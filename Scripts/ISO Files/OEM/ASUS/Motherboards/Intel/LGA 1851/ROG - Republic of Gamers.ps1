d6fcfc13eb3730a0f6c22181540452e85e375679
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 08:03:38 -0500

    Update ROG - Republic of Gamers.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/ROG - Republic of Gamers.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/ROG - Republic of Gamers.ps1
@@ -43,3 +43,33 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASUS LGA 1851 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 ROG - Republic of Gamers Motherboard Ethernet Drivers..." -Verbose 
+$ROGEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Ethernet1"
+$ROGEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Ethernet2"
+$ROGEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Ethernet3"
+
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet1
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet2
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet3
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 ROG - Republic of Gamers Motherboard WiFi Drivers..." -Verbose 
+$ROGWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi1"
+$ROGWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi2"
+$ROGWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi3"
+$ROGWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi4"
+$ROGWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi5"
+
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi1
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi2
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi3
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi4
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi5
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 ROG - Republic of Gamers Motherboard Storage Drivers..." -Verbose 
+$ROGStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ROGStorage
