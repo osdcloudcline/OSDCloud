ff752a2adce5a577f3d0523a86a6232c6bb48b4c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 14:07:13 -0500

    Update CSM.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/CSM.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/CSM.ps1
@@ -41,3 +41,34 @@ New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 CSM Motherboard Ethernet Drivers..." -Verbose 
+$CSMEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet1"
+$CSMEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet2"
+$CSMEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet3"
+$CSMEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet4"
+
+Edit-OSDCloudWinPE -DriverPath $CSMEthernet1
+Edit-OSDCloudWinPE -DriverPath $CSMEthernet2
+Edit-OSDCloudWinPE -DriverPath $CSMEthernet3
+Edit-OSDCloudWinPE -DriverPath $CSMEthernet4
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 CSM Motherboard WiFi Drivers..." -Verbose 
+$CSMWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\WiFi1"
+$CSMWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\WiFi2"
+$CSMWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\WiFi3"
+
+Edit-OSDCloudWinPE -DriverPath $CSMWiFi1
+Edit-OSDCloudWinPE -DriverPath $CSMWiFi2
+Edit-OSDCloudWinPE -DriverPath $CSMWiFi3
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 CSM Motherboard Storage Drivers..." -Verbose 
+$CSMStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA700\CSM\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $CSMStorage
+
+Write-Host
+
