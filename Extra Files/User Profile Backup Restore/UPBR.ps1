b3c3acaae4a427b2497bfe6fe83c7aa936ec8569
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 20:46:06 -0500

    Update UPBR.ps1

--- Extra Files/User Profile Backup Restore/UPBR.ps1
+++ Extra Files/User Profile Backup Restore/UPBR.ps1
@@ -6,4 +6,15 @@ Write-Host '                                                                    
 Write-Host '                                                                              '
 pause
 
+######################################################################################
+####    USER PROFILE BACKUP AND RESTORE URL                                    #######
+######################################################################################
+
+$UPBR_URL = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/User%20Profile%20Backup%20Restore/UserProfileBackupRestore.exe"
+
+######################################################################################
+####    USER PROFILE BACKUP AND RESTORE EXTRACTION LOCATION                    #######
+######################################################################################
+$UPBRFilePath = "C:\OSDCloud\GitHub\downloads\UserProfileBackupRestore.exe"
+
 
