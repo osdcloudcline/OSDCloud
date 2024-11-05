c8c0487f2c2302de86872583bca5d745114d5080
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 12:45:19 -0500

    Update Business.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/Business.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/Business.ps1
@@ -41,3 +41,33 @@ New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Business Motherboard Ethernet Drivers..." -Verbose 
+$BusinessEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\Ethernet1"
+$BusinessEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\Ethernet2"
+$BusinessEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\Ethernet3"
+$BusinessEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\Ethernet4"
+
+Edit-OSDCloudWinPE -DriverPath $BusinessEthernet1
+Edit-OSDCloudWinPE -DriverPath $BusinessEthernet2
+Edit-OSDCloudWinPE -DriverPath $BusinessEthernet3
+Edit-OSDCloudWinPE -DriverPath $BusinessEthernet4
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Business Motherboard WiFi Drivers..." -Verbose 
+$BusinessWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\WiFi1"
+$BusinessWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\WiFi2"
+$BusinessWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\WiFi3"
+
+Edit-OSDCloudWinPE -DriverPath $BusinessWiFi1
+Edit-OSDCloudWinPE -DriverPath $BusinessWiFi2
+Edit-OSDCloudWinPE -DriverPath $BusinessWiFi3
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Business Motherboard Storage Drivers..." -Verbose 
+$BusinessStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Business\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $BusinessStorage
+
+Write-Host
