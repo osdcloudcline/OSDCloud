ed6f584096bb126d850a3409ad4b386ee6251575
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 20:25:40 -0500

    Update PortableBrowsers.ps1

--- Extra Files/Browser/PortableBrowsers.ps1
+++ Extra Files/Browser/PortableBrowsers.ps1
@@ -16,6 +16,25 @@ $OSDBrowser = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/
 ######################################################################################
 $ChromePath = "C:\OSDCloud\GitHub\downloads\Chrome.exe"
 
+Function Get-OSDCloudMount(){
+$OSDCloudMountQues = Read-Host -Prompt 'Is the OSD Cloud boot.wim already mounted?'
+If(($OSDCloudMountQues -eq "Y") -or ($OSDCloudMountQues -eq "y") -or ($OSDCloudMountQues -eq "Yes") -or ($OSDCloudMountQues -eq "yes")){
+$OSDCloudMountDir = Read-Host -Prompt 'Please provide current mount directory'
+}
+ElseIf(($OSDCloudMountQues -eq "N") -or ($OSDCloudMountQues -eq "n") -or ($OSDCloudMountQues -eq "No") -or ($OSDCloudMountQues -eq "no")){
+$OSDCloudWIM = Read-Host -Prompt 'Please Specify the location of your OSDCloud boot.wim file'
+$mountdir = Read-Host 'Please specify the mount point'
+Write-Host "Mounting OSDCloud boot.wim..." 
+Mount-WindowsImage -ImagePath "$OSDCloudWIM\boot.wim" -Index 1 -Path $mountdir
+Write-Host "Mounting completed..."
+}
+
+
+Function Get-OSDCloudDismount(){
+$mountdir = Read-Host -Prompt 'Please provide mount directory'
+Dismount-WindowsImage -Path $mountdir -Save
+}
+
 Write-Verbose "Acquiring Google Chrome Portable Web Browser from GitHub repository..." -Verbose
 Save-WebFile -SourceUrl $OSDBrowser DestinationDirectory $OSDCloudGHdownloads
 
