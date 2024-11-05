fda47414f04e87bbe8a0e40511fc2c850f5f2fec
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 08:05:45 -0500

    Update TUF Gaming.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/TUF Gaming.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/TUF Gaming.ps1
@@ -43,3 +43,58 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASUS LGA 1851 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 TUF Gaming Motherboard Ethernet Drivers..." -Verbose 
+$TUFEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\TUF\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 TUF Gaming Motherboard WiFi Drivers..." -Verbose 
+$TUFWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\TUF\WiFi"
+
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 TUF Gaming Motherboard Storage Drivers..." -Verbose 
+$TUFStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\TUF\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $TUFStorage
+
+# Cloud Drivers
+
+Write-Host
+Write-Verbose "Processing: OSDCloud Cloud Drivers..." -Verbose 
+Edit-OSDCloudWinPE -CloudDriver USB, VMware, WiFi
+Write-Host
+Write-Verbose "Completed: Integration of Cloud Drivers..." -Verbose 
+
+Write-Host
+Write-Verbose "Processing: Copying OSD PowerShell Module..." -Verbose
+Edit-OSDCloudWinPE -PSModuleCopy OSD
+
+Write-Host
+Write-Verbose "Processing: Extra OSD PowerShell Module..." -Verbose
+Edit-OSDCloudWinPE -PSModuleInstall Azure
+Edit-OSDCloudWinPE -PSModuleInstall AzureAD
+Edit-OSDCloudWinPE -PSModuleInstall Az.Accounts
+Edit-OSDCloudWinPE -PSModuleInstall Az.Storage
+Edit-OSDCloudWinPE -PSModuleInstall Az.Resources
+Edit-OSDCloudWinPE -PSModuleInstall Az.KeyVault
+Edit-OSDCloudWinPE -PSModuleInstall Az.Compute
+Edit-OSDCloudWinPE -PSModuleInstall Az.Automation
+Edit-OSDCloudWinPE -PSModuleInstall Az.Network
+Edit-OSDCloudWinPE -PSModuleInstall Az.ApiManagement
+Edit-OSDCloudWinPE -PSModuleInstall OSDCloudGUI
+Edit-OSDCloudWinPE -PSModuleInstall OSDCloudAzure
+Edit-OSDCloudWinPE -PSModuleInstall OSDUpdate
+Edit-OSDCloudWinPE -PSModuleInstall AutopilotOOBE
+Edit-OSDCloudWinPE -PSModuleInstall OSDDrivers
+Edit-OSDCloudWinPE -PSModuleInstall OSDDeploy
+Edit-OSDCloudWinPE -PSModuleInstall OSDSoftware
+Edit-OSDCloudWinPE -PSModuleInstall OSDCatalog
+Edit-OSDCloudWinPE -PSModuleInstall OSDProgress
+Edit-OSDCloudWinPE -PSModuleInstall PSCloudPC
+Edit-OSDCloudWinPE -PSModuleInstall PSWindowsUpdate
+Edit-OSDCloudWinPE -PSModuleInstall wifiprofilemanagement
