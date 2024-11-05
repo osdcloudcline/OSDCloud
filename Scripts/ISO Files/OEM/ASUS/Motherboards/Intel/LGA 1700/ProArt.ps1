2fbfd40299c8f5d2c665026c2802bcac59628829
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 14:12:45 -0500

    Update ProArt.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/ProArt.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/ProArt.ps1
@@ -41,3 +41,42 @@ New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 ProArt Motherboard Ethernet Drivers..." -Verbose 
+$ProArtEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Ethernet1"
+$ProArtEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Ethernet2"
+$ProArtEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Ethernet3"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet1
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet2
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet3
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 ProArt Motherboard WiFi Drivers..." -Verbose 
+$ProArtWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi1"
+$ProArtWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi2"
+$ProArtWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi3"
+$ProArtWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi4"
+$ProArtWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi5"
+$ProArtWiFi6 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi6"
+$ProArtWiFi7 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi7"
+$ProArtWiFi8 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi8"
+$ProArtWiFi9 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi9"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi1
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi2
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi3
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi4
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi5
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi6
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi7
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi8
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi9
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 ProArt Motherboard Storage Drivers..." -Verbose 
+$ProArtStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtStorage
+
