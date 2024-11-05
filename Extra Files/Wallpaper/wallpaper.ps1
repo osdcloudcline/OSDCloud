9c41b44ad211ea03c88c36c31e2835aa165ff406
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 20:51:50 -0500

    Update wallpaper.ps1

--- Extra Files/Wallpaper/wallpaper.ps1
+++ Extra Files/Wallpaper/wallpaper.ps1
@@ -1,1 +1,7 @@
-
+Write-Host '                                                                              '
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' The following extra files will be integrated into OSDCloud boot.wim:         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Custom OSD Cloud Wallpaper                                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+Write-Host '                                                                              '
+pause
