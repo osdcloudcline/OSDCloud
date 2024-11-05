34bfaca66f234b16736f7fc925a3cfd74e501cd3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-04 20:46:37 -0500

    Update UPBR.ps1

--- Extra Files/User Profile Backup Restore/UPBR.ps1
+++ Extra Files/User Profile Backup Restore/UPBR.ps1
@@ -18,3 +18,21 @@ $UPBR_URL = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/Us
 $UPBRFilePath = "C:\OSDCloud\GitHub\downloads\UserProfileBackupRestore.exe"
 
 
+Function Get-OSDCloudMount(){
+$OSDCloudMountQues = Read-Host -Prompt 'Is the OSD Cloud boot.wim already mounted?'
+If(($OSDCloudMountQues -eq "Y") -or ($OSDCloudMountQues -eq "y") -or ($OSDCloudMountQues -eq "Yes") -or ($OSDCloudMountQues -eq "yes")){
+$OSDCloudMountDir = Read-Host -Prompt 'Please provide current mount directory'
+}
+ElseIf(($OSDCloudMountQues -eq "N") -or ($OSDCloudMountQues -eq "n") -or ($OSDCloudMountQues -eq "No") -or ($OSDCloudMountQues -eq "no")){
+$OSDCloudWIM = Read-Host -Prompt 'Please Specify the location of your OSDCloud boot.wim file'
+$mountdir = Read-Host 'Please specify the mount point'
+Write-Host "Mounting OSDCloud boot.wim..." 
+Mount-WindowsImage -ImagePath "$OSDCloudWIM\boot.wim" -Index 1 -Path $mountdir
+Write-Host "Mounting completed..."
+}
+
+
+Function Get-OSDCloudDismount(){
+$mountdir = Read-Host -Prompt 'Please provide mount directory'
+Dismount-WindowsImage -Path $mountdir -Save
+}
