ad55ed3b9e3038e3c132719fb1fe85a9c8ebf794
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 08:02:10 -0500

    Update ProArt.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/ProArt.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/ProArt.ps1
@@ -43,3 +43,71 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASUS LGA 1851 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 ProArt Motherboard Ethernet Drivers..." -Verbose 
+$ProArtEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 ProArt Motherboard WiFi Drivers..." -Verbose 
+$ProArtWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi1"
+$ProArtWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi2"
+$ProArtWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi3"
+$ProArtWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi4"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi1
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi2
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi3
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi4
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 ProArt Motherboard Storage Drivers..." -Verbose 
+$ProArtStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtStorage
+
+# VM Drivers
+
+
+
+Write-Host
+Write-Verbose "Completed: Integration of Virtual Machine Drivers..." -Verbose 
+
+# Cloud Drivers
+
+Write-Host
+Write-Verbose "Processing: OSDCloud Cloud Drivers..." -Verbose 
+Edit-OSDCloudWinPE -CloudDriver USB, VMware, WiFi
+Write-Host
+Write-Verbose "Completed: Integration of Cloud Drivers..." -Verbose 
+
+Write-Host
+Write-Verbose "Processing: Copying OSD PowerShell Module..." -Verbose
+Edit-OSDCloudWinPE -PSModuleCopy OSD
+
+Write-Host
+Write-Verbose "Processing: Extra OSD PowerShell Module..." -Verbose
+Edit-OSDCloudWinPE -PSModuleInstall Azure
+Edit-OSDCloudWinPE -PSModuleInstall AzureAD
+Edit-OSDCloudWinPE -PSModuleInstall Az.Accounts
+Edit-OSDCloudWinPE -PSModuleInstall Az.Storage
+Edit-OSDCloudWinPE -PSModuleInstall Az.Resources
+Edit-OSDCloudWinPE -PSModuleInstall Az.KeyVault
+Edit-OSDCloudWinPE -PSModuleInstall Az.Compute
+Edit-OSDCloudWinPE -PSModuleInstall Az.Automation
+Edit-OSDCloudWinPE -PSModuleInstall Az.Network
+Edit-OSDCloudWinPE -PSModuleInstall Az.ApiManagement
+Edit-OSDCloudWinPE -PSModuleInstall OSDCloudGUI
+Edit-OSDCloudWinPE -PSModuleInstall OSDCloudAzure
+Edit-OSDCloudWinPE -PSModuleInstall OSDUpdate
+Edit-OSDCloudWinPE -PSModuleInstall AutopilotOOBE
+Edit-OSDCloudWinPE -PSModuleInstall OSDDrivers
+Edit-OSDCloudWinPE -PSModuleInstall OSDDeploy
+Edit-OSDCloudWinPE -PSModuleInstall OSDSoftware
+Edit-OSDCloudWinPE -PSModuleInstall OSDCatalog
+Edit-OSDCloudWinPE -PSModuleInstall OSDProgress
+Edit-OSDCloudWinPE -PSModuleInstall PSCloudPC
+Edit-OSDCloudWinPE -PSModuleInstall PSWindowsUpdate
+Edit-OSDCloudWinPE -PSModuleInstall wifiprofilemanagement
