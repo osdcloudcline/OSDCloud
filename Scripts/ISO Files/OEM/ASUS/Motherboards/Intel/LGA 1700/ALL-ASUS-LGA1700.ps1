8442360202aedabca4c99ff7aa820ac744bda428
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 12:44:39 -0500

    Update ALL-ASUS-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/ALL-ASUS-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/ALL-ASUS-LGA1700.ps1
@@ -123,97 +123,186 @@ $OtherStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Other\Storage"
 Edit-OSDCloudWinPE -DriverPath $OtherStorage
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 PRIME Motherboard Ethernet Drivers..." -Verbose 
-$PRIMEEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Ethernet"
+Write-Verbose "Processing: ASUS LGA 1700 PRIME Motherboard Ethernet Drivers..." -Verbose 
+$PRIMEEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\Ethernet1"
+$PRIMEEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\Ethernet2"
+$PRIMEEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\Ethernet3"
+$PRIMEEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\Ethernet4"
 
-Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet1
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet2
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet3
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet4
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 PRIME Motherboard WiFi Drivers..." -Verbose 
-$PRIMEWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\WiFi"
+Write-Verbose "Processing: ASUS LGA 1700 PRIME Motherboard WiFi Drivers..." -Verbose 
+$PRIMEWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\WiFi1"
+$PRIMEWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\WiFi2"
 
-Edit-OSDCloudWinPE -DriverPath $PRIMEWiFi
+Edit-OSDCloudWinPE -DriverPath $PRIMEWiFi1
+Edit-OSDCloudWinPE -DriverPath $PRIMEWiFi2
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 PRIME Motherboard Storage Drivers..." -Verbose 
-$PRIMEStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage"
+Write-Verbose "Processing: ASUS LGA 1700 PRIME Motherboard Storage Drivers..." -Verbose 
+$PRIMEStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\PRIME\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $PRIMEStorage
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 ProArt Motherboard Ethernet Drivers..." -Verbose 
-$ProArtEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\Ethernet"
+Write-Verbose "Processing: ASUS LGA 1700 ProArt Motherboard Ethernet Drivers..." -Verbose 
+$ProArtEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Ethernet1"
+$ProArtEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Ethernet2"
+$ProArtEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Ethernet3"
 
-Edit-OSDCloudWinPE -DriverPath $ProArtEthernet
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet1
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet2
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet3
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 ProArt Motherboard WiFi Drivers..." -Verbose 
-$ProArtWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi1"
-$ProArtWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi2"
-$ProArtWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi3"
-$ProArtWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\WiFi4"
+Write-Verbose "Processing: ASUS LGA 1700 ProArt Motherboard WiFi Drivers..." -Verbose 
+$ProArtWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi1"
+$ProArtWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi2"
+$ProArtWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi3"
+$ProArtWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi4"
+$ProArtWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi5"
+$ProArtWiFi6 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi6"
+$ProArtWiFi7 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi7"
+$ProArtWiFi8 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi8"
+$ProArtWiFi9 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\WiFi9"
 
 Edit-OSDCloudWinPE -DriverPath $ProArtWiFi1
 Edit-OSDCloudWinPE -DriverPath $ProArtWiFi2
 Edit-OSDCloudWinPE -DriverPath $ProArtWiFi3
 Edit-OSDCloudWinPE -DriverPath $ProArtWiFi4
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi5
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi6
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi7
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi8
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi9
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 ProArt Motherboard Storage Drivers..." -Verbose 
-$ProArtStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ProArt\Storage"
+Write-Verbose "Processing: ASUS LGA 1700 ProArt Motherboard Storage Drivers..." -Verbose 
+$ProArtStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ProArt\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $ProArtStorage
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 ROG - Republic of Gamers Motherboard Ethernet Drivers..." -Verbose 
-$ROGEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Ethernet1"
-$ROGEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Ethernet2"
-$ROGEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Ethernet3"
+Write-Verbose "Processing: ASUS LGA 1700 ROG - Republic of Gamers Motherboard Ethernet Drivers..." -Verbose 
+$ROGEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\Ethernet1"
+$ROGEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\Ethernet2"
+
 
 Edit-OSDCloudWinPE -DriverPath $ROGEthernet1
 Edit-OSDCloudWinPE -DriverPath $ROGEthernet2
-Edit-OSDCloudWinPE -DriverPath $ROGEthernet3
+
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 ROG - Republic of Gamers Motherboard WiFi Drivers..." -Verbose 
-$ROGWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi1"
-$ROGWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi2"
-$ROGWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi3"
-$ROGWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi4"
-$ROGWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\WiFi5"
+Write-Verbose "Processing: ASUS LGA 1700 ROG - Republic of Gamers Motherboard WiFi Drivers..." -Verbose 
+$ROGWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi1"
+$ROGWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi2"
+$ROGWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi3"
+$ROGWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi4"
+$ROGWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi5"
+$ROGWiFi6 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi6"
+$ROGWiFi7 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi7"
+$ROGWiFi8 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi8"
+$ROGWiFi9 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi9"
+$ROGWiFi10 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi10"
+$ROGWiFi11 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi11"
+$ROGWiFi12 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi12"
+$ROGWiFi13 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi13"
+$ROGWiFi14 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi14"
+$ROGWiFi15 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi15"
+$ROGWiFi16 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi16"
+$ROGWiFi17 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\WiFi17"
 
 Edit-OSDCloudWinPE -DriverPath $ROGWiFi1
 Edit-OSDCloudWinPE -DriverPath $ROGWiFi2
 Edit-OSDCloudWinPE -DriverPath $ROGWiFi3
 Edit-OSDCloudWinPE -DriverPath $ROGWiFi4
 Edit-OSDCloudWinPE -DriverPath $ROGWiFi5
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi6
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi7
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi8
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi9
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi10
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi11
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi12
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi13
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi14
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi15
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi16
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi17
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 ROG - Republic of Gamers Motherboard Storage Drivers..." -Verbose 
-$ROGStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\ROG\Storage"
+Write-Verbose "Processing: ASUS LGA 1700 ROG - Republic of Gamers Motherboard Storage Drivers..." -Verbose 
+$ROGStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\ROG\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $ROGStorage
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 TUF Gaming Motherboard Ethernet Drivers..." -Verbose 
-$TUFEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\TUF\Ethernet"
+Write-Verbose "Processing: ASUS LGA 1700 TUF Gaming Motherboard Ethernet Drivers..." -Verbose 
+$TUFEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\Ethernet1"
+$TUFEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\Ethernet2"
+$TUFEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\Ethernet3"
 
-Edit-OSDCloudWinPE -DriverPath $TUFEthernet
-
-Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 TUF Gaming Motherboard WiFi Drivers..." -Verbose 
-$TUFWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\TUF\WiFi"
-
-Edit-OSDCloudWinPE -DriverPath $TUFWiFi
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet1
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet2
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet3
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 TUF Gaming Motherboard Storage Drivers..." -Verbose 
-$TUFStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\TUF\Storage"
+Write-Verbose "Processing: ASUS LGA 1700 TUF Gaming Motherboard WiFi Drivers..." -Verbose 
+$TUFWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\WiFi1"
+$TUFWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\WiFi2"
+$TUFWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\WiFi3"
+$TUFWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\WiFi4"
+$TUFWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\WiFi5"
+
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi1
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi2
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi3
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi4
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi5
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 TUF Gaming Motherboard Storage Drivers..." -Verbose 
+$TUFStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\TUF\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $TUFStorage
 
+
 Write-Host
-Write-Verbose "Completed: Integration of ASUS Intel LGA 1851 Motherboard Drivers..." -Verbose 
+Write-Verbose "Processing: ASUS LGA 1700 Expedition Motherboard Ethernet Drivers..." -Verbose 
+$ExpeditionEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Expedition\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $ExpeditionEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Expedition Motherboard Storage Drivers..." -Verbose 
+$ExpeditionStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Expedition\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ExpeditionStorage
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Workstation Motherboard Ethernet Drivers..." -Verbose 
+$WorkstationEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Workstation\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $WorkstationEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Workstation Motherboard WiFi Drivers..." -Verbose 
+$WorkstationWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Workstation\WiFi1"
+
+Edit-OSDCloudWinPE -DriverPath $WorkstationWiFi
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1700 Workstation Motherboard Storage Drivers..." -Verbose 
+$WorkstationStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\Workstation\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $WorkstationStorage
+
+Write-Host
+Write-Verbose "Completed: Integration of ASUS Intel LGA 1700 Motherboard Drivers..." -Verbose 
 
 # VM Drivers
 
