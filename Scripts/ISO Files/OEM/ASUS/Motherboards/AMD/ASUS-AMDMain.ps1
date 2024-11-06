7667a34ea23aee85c1a6b5fd90e5de609e0657cc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 19:17:35 -0500

    Update ASUS-AMDMain.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/ASUS-AMDMain.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/ASUS-AMDMain.ps1
@@ -1,1 +1,33 @@
+#################################################################
+# Main Menu
+#################################################################
+
+Function Show-MainMenu{
+   [CmdletBinding()]
+   param(
+   [string]$Title = 'OSD Cloud - ASUS AMD Main Menu',
+   [string]$Question = 'What type of action do you need to do?' 
+   )
+cls
+
+$Date = Get-Date
+Write-Host "Today is:" "$Date"
+Write-Host
+Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
+Write-Host
+Write-Host 
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host '            This OSDCloud section allows the user to perform:                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '          - Creating ISO images for Desktop motherboards                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                 for the following manufacturers:                             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '          - ASUS: AMD AM4/AM5 socket types                                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+pause
+Clear-Host
+
+Write-Host "======= $Title ======"
+Write-Host " 1. OSDCloud: Create ISO - ASUS AMD AM 4"
+Write-Host " 2. OSDCloud: Create ISO - ASUS AMD AM 5"
+Write-Host " 3. OSDCloud: Get Help"
+Write-Host " 4. Return to Main Menu"
 
