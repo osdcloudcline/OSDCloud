e8998587a0bf0d99324fae1a494892bd8d42916d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-05 23:36:17 -0500

    Update ASRockMain.ps1

--- Scripts/ISO Files/Desktop Motherboards/ASRockMain.ps1
+++ Scripts/ISO Files/Desktop Motherboards/ASRockMain.ps1
@@ -118,7 +118,7 @@ do
     Invoke-Expression $($AM5.Content)
     }
 '4'{cls
-    $AM4 = Invoke-WebRequest("")
+    $AM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/AM4-Main.ps1")
     Invoke-Expression $($AM4.Content)
     }
 '5'{cls
