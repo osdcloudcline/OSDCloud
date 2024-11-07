048cded9bd8233fe145373f58ab613c6fe6c7b84
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-07 07:12:17 -0500

    Update CPUSocketMain.ps1

--- Scripts/ISO Files/CPU and Socket Type/CPUSocketMain.ps1
+++ Scripts/ISO Files/CPU and Socket Type/CPUSocketMain.ps1
@@ -113,7 +113,7 @@ do
     Invoke-Expression $($LGA1700.Content)
     }
 '3'{cls
-    $AM4 = Invoke-WebRequest("")
+    $AM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/CPU%20and%20Socket%20Type/AMD/AM4/OSDCloud-AM4.ps1")
     Invoke-Expression $($AM4.Content)
     }
 '4'{cls
