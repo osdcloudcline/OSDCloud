8edebec0ab94ddad7921f491c0c9826dcfa034b1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-16 18:17:22 -0500

    Update OSDCloud-AM4.ps1

--- Scripts/ISO Files/CPU and Socket Type/AMD/AM4/OSDCloud-AM4.ps1
+++ Scripts/ISO Files/CPU and Socket Type/AMD/AM4/OSDCloud-AM4.ps1
@@ -44,3 +44,198 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASUS AM 4 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Business Motherboard Ethernet Drivers..." -Verbose 
+$BusinessEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Business\Ethernet1"
+$BusinessEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Business\Ethernet2"
+
+Edit-OSDCloudWinPE -DriverPath $BusinessEthernet1
+
+Edit-OSDCloudWinPE -DriverPath $BusinessEthernet2
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Business Motherboard Storage Drivers..." -Verbose 
+$BusinessStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Business\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $BusinessStorage
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 CSM Motherboard Ethernet Drivers..." -Verbose 
+$CSMEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\CSM\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $CSMEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 CSM Motherboard Storage Drivers..." -Verbose 
+$CSMStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\CSM\Storage1"
+$CSMStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\CSM\Storage2"
+
+Edit-OSDCloudWinPE -DriverPath $CSMStorage1
+
+Edit-OSDCloudWinPE -DriverPath $CSMStorage2
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Other Motherboard Ethernet Drivers..." -Verbose 
+$OtherEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Other\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $OtherEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 Other Motherboard Storage Drivers..." -Verbose 
+$OtherStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Other\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $OtherStorage
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 PRIME Motherboard Ethernet Drivers..." -Verbose 
+$PRIMEEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\Ethernet1"
+$PRIMEEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\Ethernet2"
+$PRIMEEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\Ethernet3"
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet1
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet2
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet3
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 PRIME Motherboard WiFi Drivers..." -Verbose 
+$PRIMEWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\WiFi"
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEWiFi
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 PRIME Motherboard Storage Drivers..." -Verbose 
+$PRIMEStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage1"
+$PRIMEStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage2"
+$PRIMEStorage3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage3"
+$PRIMEStorage4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage4"
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage1
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage2
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage3
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage4
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ProArt Motherboard Ethernet Drivers..." -Verbose 
+$ProArtEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ProArt\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ProArt Motherboard WiFi Drivers..." -Verbose 
+$ProArtWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ProArt\WiFi1"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtWiFi
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ProArt Motherboard Storage Drivers..." -Verbose 
+$ProArtStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ProArt\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ProArtStorage
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard Bluetooth Drivers..." -Verbose 
+$ROGBT = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Bluetooth"
+
+$ROGBluetooth = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Bluetooth"
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard Ethernet Drivers..." -Verbose 
+$ROGEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet1"
+$ROGEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet2"
+$ROGEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet3"
+$ROGEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet4"
+$ROGEthernet5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet5"
+
+
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet1
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet2
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet3
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet4
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet5
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard WiFi Drivers..." -Verbose 
+$ROGWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi1"
+$ROGWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi2"
+$ROGWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi3"
+$ROGWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi4"
+
+
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi1
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi2
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi3
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi4
+
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard Storage Drivers..." -Verbose 
+$ROGStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Storage"
+$ROGStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ROGStorage1
+Edit-OSDCloudWinPE -DriverPath $ROGStorage2
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 TUF Gaming Motherboard Bluetooth Drivers..." -Verbose 
+$TUFBluetooth = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Bluetooth"
+
+Edit-OSDCloudWinPE -DriverPath $TUFBluetooth
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 TUF Gaming Motherboard Ethernet Drivers..." -Verbose 
+$TUFEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Ethernet1"
+$TUFEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Ethernet2"
+$TUFEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Ethernet3"
+$TUFEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Ethernet4"
+
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet1
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet2
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet3
+Edit-OSDCloudWinPE -DriverPath $TUFEthernet4
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 TUF Gaming Motherboard WiFi Drivers..." -Verbose 
+$TUFWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\WiFi1"
+$TUFWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\WiFi2"
+$TUFWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\WiFi3"
+$TUFWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\WiFi4"
+$TUFWiFi5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\WiFi5"
+$TUFWiFi6 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\WiFi6"
+
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi1
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi2
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi3
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi4
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi5
+Edit-OSDCloudWinPE -DriverPath $TUFWiFi6
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 TUF Gaming Motherboard Storage Drivers..." -Verbose 
+$TUFStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Storage1"
+$TUFStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\TUF\Storage2"
+
+Edit-OSDCloudWinPE -DriverPath $TUFStorage1
+Edit-OSDCloudWinPE -DriverPath $TUFStorage2
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Workstation Motherboard Ethernet Drivers..." -Verbose 
+$WorkstationEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $WorkstationEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Workstation Motherboard Storage Drivers..." -Verbose 
+$WorkstationStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation\Storage1"
+$WorkstationStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation\Storage2"
+
+Edit-OSDCloudWinPE -DriverPath $WorkstationStorage1
+Edit-OSDCloudWinPE -DriverPath $WorkstationStorage2
+
+Write-Host
+Write-Verbose "Completed: Integration of ASUS Intel AM 4 Motherboard Drivers..." -Verbose 
