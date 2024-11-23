1451ebef66fdc13ea6716b4f8ff55c4575818231
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-11-23 00:43:17 -0500

    Update OSDCloud-AM4.ps1

--- Scripts/ISO Files/CPU and Socket Type/AMD/AM4/OSDCloud-AM4.ps1
+++ Scripts/ISO Files/CPU and Socket Type/AMD/AM4/OSDCloud-AM4.ps1
@@ -349,13 +349,25 @@ Write-Verbose "Completed: Integration of ASRock AMD AM 4 Motherboard Drivers..."
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 4 AERO Motherboard Ethernet Drivers..." -Verbose 
+$AEROEthernet = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\Ethernet"
 
+Edit-OSDCloudWinPE -DriversPath $AEROEthernet
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 4 AERO Motherboard WiFi Drivers..." -Verbose 
+$AEROWiFi1 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\WiFi1"
+$AEROWiFi2 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\WiFi2"
+
+Edit-OSDCloudWinPE -DriversPath $AEROWiFi1
+Edit-OSDCloudWinPE -DriversPath $AEROWiFi2
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 4 AERO Motherboard Storage Drivers..." -Verbose 
+$AEROStorage1 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\Storage1"
+$AEROStorage2 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\Storage2"
+
+Edit-OSDCloudWinPE -DriversPath  $AEROStorage1
+Edit-OSDCloudWinPE -DriversPath  $AEROStorage2
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 4 AORUS Motherboard Ethernet Drivers..." -Verbose 
