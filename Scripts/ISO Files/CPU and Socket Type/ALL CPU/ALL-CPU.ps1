00d689263c49db9b23ec8627e61da2099f7638bf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-12 09:22:59 -0500

    Update ALL-CPU.ps1

--- Scripts/ISO Files/CPU and Socket Type/ALL CPU/ALL-CPU.ps1
+++ Scripts/ISO Files/CPU and Socket Type/ALL CPU/ALL-CPU.ps1
@@ -1,1 +1,48 @@
+Write-Host
+Write-Host "Hello, $env:username..." -ForegroundColor Cyan 
+Write-Host
+$Date = Get-Date
+Write-Host "Today is:" "$Date"
+Write-Host
+Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
+Write-Host
+Write-Host 
+Write-Host '                                                                                              ' -BackgroundColor White                                                              
+Write-Host '    This scripts creates an OSCloud Deployment ISO with the following:                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: ASUS AM4/AM5/LGA 1700/LGA 1851 motherboard drivers             ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: MSI AM4/AM5/LGA 1700/LGA 1851 Motherboard drivers              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: Gigabyte AM4/AM5/LGA 1700/LGA 1851 Motherboard drivers         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: ASRock AM4/AM5/LGA 1700/LGA 1851 Motherboard drivers           ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: VM drivers for ESXI, Hyper-V, VMWare Workstation and Proxmox   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - VBS Scripting Support                                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Wireless networking support                                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - FOR SYSTEM REFRESH: User Profile Backup and Restore for data backup                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Google Chrome portable browser                                                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                                              ' -BackgroundColor White
+pause
+Clear-Host
+
+Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
+New-OSDCloudTemplate -Name WinRE -WinRE
+
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = Read-Host -Prompt 'Please enter custom path for new OSDCloud Workspace'
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
+
+
+# AM4 CPU Socket
 
