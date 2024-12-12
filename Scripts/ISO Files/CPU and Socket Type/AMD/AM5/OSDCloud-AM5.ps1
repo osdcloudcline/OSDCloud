42de163e8e100e1a0f0aed5e36d8e8c9a0580532
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-12 09:06:46 -0500

    Update OSDCloud-AM5.ps1

--- Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
+++ Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
@@ -43,6 +43,78 @@ Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
+# ASRock Am5 motherboards
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 5 PRO Motherboard Ethernet Drivers..." -Verbose 
+$PROEthernet = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\PRO\Ethernet"
+
+Edit-OSDCloudWinPE -DriversPath $PROEthernet
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 PRO Motherboard WiFi Drivers..." -Verbose 
+$PROWiFi = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\PRO\WiFi"
+
+Edit-OSDCloudWinPE -DriversPath $PROWiFi
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 PRO Motherboard Bluetooth Drivers..." -Verbose 
+$PROBluetooth = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\PRO\Bluetooth"
+
+Edit-OSDCloudWinPE -DriversPath $PROBluetooth
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 PRO Motherboard Storage Drivers..." -Verbose 
+$PROStorage = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\PRO\Storage"
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Steel Legend Motherboard Ethernet Drivers..." -Verbose 
+$SteelLegendEthernet = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\SteelLegend\Ethernet"
+
+Edit-OSDCloudWinPE -DriversPath $SteelLegendEthernet
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Steel Legend Motherboard WiFi Drivers..." -Verbose 
+$SteelLegendWiFi = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\SteelLegend\WiFi"
+
+Edit-OSDCloudWinPE -DriversPath $SteelLegendWiFi
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Steel Legend Motherboard Bluetooth Drivers..." -Verbose 
+$SteelLegendBluetooth = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\SteelLegend\Bluetooth"
+
+Edit-OSDCloudWinPE -DriversPath $SteelLegendBluetooth
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Steel Legend Motherboard Storage Drivers..." -Verbose 
+$SteelLegendStorage = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\SteelLegend\Storage"
+
+Edit-OSDCloudWinPE -DriversPath $SteelLegendStorage
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Taichi Motherboard Ethernet Drivers..." -Verbose 
+$TaichiEthernet = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Ethernet"
+
+Edit-OSDCloudWinPE -DriversPath $TaichiEthernet
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Taichi Motherboard WiFi Drivers..." -Verbose 
+$TaichiWiFi = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\WiFi"
+
+Edit-OSDCloudWinPE -DriversPath $TaichiWiFi
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Taichi Motherboard Bluetooth Drivers..." -Verbose 
+$TaichiBluetooth = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Bluetooth"
+$TaichiBluetooth1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Bluetooth1"
+
+Edit-OSDCloudWinPE -DriversPath $TaichiBluetooth
+Edit-OSDCloudWinPE -DriversPath $TaichiBluetooth1
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 5 Taichi Motherboard Storage Drivers..." -Verbose 
+$TaichiStorage = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Storage"
+
+Edit-OSDCloudWinPE -DriversPath $TaichiStorage
+
+# ASUS AM5 motherboards
