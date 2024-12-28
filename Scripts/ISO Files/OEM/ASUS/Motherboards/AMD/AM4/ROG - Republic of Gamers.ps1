909773463a7cb3440e4573493dda5acf0de6ad3d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 17:58:11 -0500

    Update ROG - Republic of Gamers.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/ROG - Republic of Gamers.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/ROG - Republic of Gamers.ps1
@@ -43,3 +43,40 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASUS AM 4 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard Ethernet Drivers..." -Verbose 
+$ROGEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet1"
+$ROGEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet2"
+$ROGEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet3"
+$ROGEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet4"
+$ROGEthernet5 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Ethernet5"
+
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet1
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet2
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet3
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet4
+Edit-OSDCloudWinPE -DriverPath $ROGEthernet5
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard WiFi Drivers..." -Verbose 
+$ROGWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi1"
+$ROGWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi2"
+$ROGWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi3"
+$ROGWiFi4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\WiFi4"
+
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi1
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi2
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi3
+Edit-OSDCloudWinPE -DriverPath $ROGWiFi4
+
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 ROG - Republic of Gamers Motherboard Storage Drivers..." -Verbose 
+$ROGStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Storage"
+$ROGStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\ROG\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ROGStorage1
+Edit-OSDCloudWinPE -DriverPath $ROGStorage2
+
+Write-Host
