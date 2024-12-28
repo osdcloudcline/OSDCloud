3624baffd00e986c72b8883085d01218b8248ea2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 17:59:38 -0500

    Update Workstation.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/Workstation.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/Workstation.ps1
@@ -43,4 +43,18 @@ Get-OSDCloudWorkspace
 
 # ASUS AM 4 Motherboards
 
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Workstation Motherboard Ethernet Drivers..." -Verbose 
+$WorkstationEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation\Ethernet"
 
+Edit-OSDCloudWinPE -DriverPath $WorkstationEthernet
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 Workstation Motherboard Storage Drivers..." -Verbose 
+$WorkstationStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation\Storage1"
+$WorkstationStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\Workstation\Storage2"
+
+Edit-OSDCloudWinPE -DriverPath $WorkstationStorage1
+Edit-OSDCloudWinPE -DriverPath $WorkstationStorage2
+
+Write-Host
