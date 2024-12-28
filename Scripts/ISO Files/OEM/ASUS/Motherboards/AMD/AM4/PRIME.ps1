174a1ebbb575d85b7036c61ac8014e3d7ad90487
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 17:56:51 -0500

    Update PRIME.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/PRIME.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/PRIME.ps1
@@ -41,3 +41,33 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASUS AM 4 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 PRIME Motherboard Ethernet Drivers..." -Verbose 
+$PRIMEEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\Ethernet1"
+$PRIMEEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\Ethernet2"
+$PRIMEEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\Ethernet3"
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet1
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet2
+Edit-OSDCloudWinPE -DriverPath $PRIMEEthernet3
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 PRIME Motherboard WiFi Drivers..." -Verbose 
+$PRIMEWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\AM4\PRIME\WiFi"
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEWiFi
+
+Write-Host
+Write-Verbose "Processing: ASUS AM 4 PRIME Motherboard Storage Drivers..." -Verbose 
+$PRIMEStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage1"
+$PRIMEStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage2"
+$PRIMEStorage3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage3"
+$PRIMEStorage4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\PRIME\Storage4"
+
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage1
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage2
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage3
+Edit-OSDCloudWinPE -DriverPath $PRIMEStorage4
+
+Write-Host
