f0db7e07ac65b8a44ca19ec995b6e3aa8945c7f5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 23:27:12 -0500

    Update CSM.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/CSM.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/CSM.ps1
@@ -19,6 +19,13 @@ Write-Host '                                                                    
 pause
 Clear-Host
 
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
+
+Write-Host
+Write-Verbose "Processing: ASUS AM4 CSM Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
+Write-Host
+
 $CSMAM4DriversDownload = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/CSM%20Motherboards/CSM.ps1")
 Invoke-Expression $($CSMAM4DriversDownload.Content)
 
