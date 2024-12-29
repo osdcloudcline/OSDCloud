db7bb2acd8b0906141d7a96f895c54cb7297e890
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 23:27:59 -0500

    Update PRIME.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/PRIME.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/PRIME.ps1
@@ -19,6 +19,12 @@ Write-Host '                                                                    
 pause
 Clear-Host
 
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
+
+Write-Host
+Write-Verbose "Processing: ASUS AM4 PRIME Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
+Write-Host
 
 $PRIME = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/PRIME.ps1")
 Invoke-Expression $($PRIME.Content)
