ee0112df670e3feefc2eae220982d7d6bf86bfc9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 23:16:50 -0500

    Update OSDCloudWallpaper-Download.ps1

--- Extra Files/Wallpaper/OSDCloudWallpaper-Download.ps1
+++ Extra Files/Wallpaper/OSDCloudWallpaper-Download.ps1
@@ -1,1 +1,13 @@
+Write-Host '                                                                                       '
+Write-Host '                                                                                       ' -BackgroundColor White                                                              
+Write-Host ' The following extra files will be downloaded and integrated into OSDCloud boot.wim:   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Custom OSD Cloud Wallpaper                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                                       ' -BackgroundColor White
+Write-Host '                                                                                       '
+pause
 
+
+######################################################################################
+####    OSDCloud Wallpaper URL                                                 #######
+######################################################################################
+$OSDCloudWallpaperURL = "https://github.com/osdcloudcline/OSDCloud/blob/main/Extra%20Files/Wallpaper/winpe.jpg"
