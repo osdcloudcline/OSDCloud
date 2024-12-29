6eb43b796eb754a6979c4361f42fff16b46a8534
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-29 01:13:37 -0500

    Update PhantomGaming.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PhantomGaming.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PhantomGaming.ps1
@@ -79,3 +79,34 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASRock AM 4 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 4 Phantom Gaming Motherboard Ethernet Drivers..." -Verbose 
+$PhantomGamingEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet1"
+$PhantomGamingEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet2"
+$PhantomGamingEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet3"
+$PhantomGamingEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet4"
+$PhantomGamingEthernet5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet5"
+
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet1
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet2
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet3
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet4
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet5
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 4 Phantom Gaming Motherboard WiFi Drivers..." -Verbose 
+$PhantomGamingWLAN = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\WiFi"
+
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingWiFi
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 4 Phantom Gaming Motherboard Storage Drivers..." -Verbose 
+$PhantomGamingStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Storage1"
+$PhantomGamingStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Storage2"
+
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingStorage1
+Edit-OSDCloudWinPE -DriversPath $PhantomGamingStorage2
+
+Write-Host
+Write-Verbose "Completed: Integration of ASRock AMD AM 4 Motherboard Drivers..." -Verbose 
