9661860c3bad01ffa0cef4e19e62c2d06dc01a15
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-29 08:22:29 -0500

    Update GigabyteMain.ps1

--- Scripts/ISO Files/Desktop Motherboards/GigabyteMain.ps1
+++ Scripts/ISO Files/Desktop Motherboards/GigabyteMain.ps1
@@ -106,7 +106,7 @@ do
   switch($selection)
   {
 '1'{cls
-    $LGA1851 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/Intel/LGA%201851/LGA1851-Main.ps1")
+    $LGA1851 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/Gigabyte/Motherboards/Intel/LGA%201851/LGA1851-Main.ps1")
     Invoke-Expression $($LGA1851.Content)
     }
 '2'{cls
