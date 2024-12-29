3b779dd1111352a1c29eabd46940b64b67cc5f15
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-29 10:02:29 -0500

    Update UD-Ultra Durable.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/UD-Ultra Durable.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/UD-Ultra Durable.ps1
@@ -1,1 +1,28 @@
+Write-Host
+Write-Host "Hello, $env:username..." -ForegroundColor Cyan 
+Write-Host
+$Date = Get-Date
+Write-Host "Today is:" "$Date"
+Write-Host
+Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
+Write-Host
+Write-Host 
+Write-Host '                                                                                              ' -BackgroundColor White                                                              
+Write-Host '    This scripts creates an OSCloud Deployment ISO with the following:                        ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: Gigabyte AM4 motherboard drivers                               ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: VM drivers for ESXI, Hyper-V, VMWare Workstation and Proxmox   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - VBS Scripting Support                                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Wireless networking support                                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - FOR SYSTEM REFRESH: User Profile Backup and Restore for data backup                    ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Google Chrome portable browser                                                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                                              ' -BackgroundColor White
+pause
+Clear-Host
 
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
+
+
+Write-Host
+Write-Verbose "Processing: Gigabyte AM4 UD - Ultra Durable Motherboard drivers, Virtualization drivers, scripting support and other utilities file downloads" -Verbose
+Write-Host 
