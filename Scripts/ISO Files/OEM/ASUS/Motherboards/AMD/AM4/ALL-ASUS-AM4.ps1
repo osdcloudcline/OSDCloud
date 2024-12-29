92d6581746123a965642d111c580aaa8f9e25293
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 23:20:44 -0500

    Update ALL-ASUS-AM4.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/ALL-ASUS-AM4.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/ALL-ASUS-AM4.ps1
@@ -19,6 +19,13 @@ Write-Host '                                                                    
 pause
 Clear-Host
 
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
+
+Write-Host
+Write-Verbose "Processing: ASUS AM4 Motherboard driver, Virtualization driver, scripting support and other utilities files" -Verbose
+Write-Host 
+
 $Business = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Business%20Motherboards/Business.ps1")
 Invoke-Expression $($Business.Content)
 
@@ -40,6 +47,9 @@ Invoke-Expression $($ROG.Content)
 $TUFGaming = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/TUF%20Gaming%20Motherboards/TUF%20Gaming.ps1")
 Invoke-Expression $($TUFGaming.Content)
 
+$Workstation = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Workstation%20Motherboards/Workstation.ps1")
+Invoke-Expression $($Workstation.Content)
+
 $HyperV = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/Hyper-V-NetworkDrivers.ps1")
 Invoke-Expression $($HyperV.Content)
 
@@ -52,9 +62,20 @@ Invoke-Expression $($ESXI.Content)
 $VMWareWSPro = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20Workstation/VMWare-WorkstationPRO-Drivers.ps1")
 Invoke-Expression $($VMWareWSPro.Content)
 
+$VBS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Scripting%20Support/VBS%20Scripting/VBSScriptingSupport-Download.ps1")
+Invoke-Expression $($VBS.Content)
+
+$PortableBrowser = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Browser/PortableBrowser-Download.ps1")
+Invoke-WebRequest $($PortableBrowser.Content)
+
+$UPBR = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/User%20Profile%20Backup%20Restore/UPBR-Download.ps1")
+Invoke-Expression $($UPBR.Content)
+
+$OSDCloudWallpaper = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Wallpaper/OSDCloudWallpaper-Download.ps1")
+Invoke-Expression $($OSDCloudWallpaper.Content) 
+
 Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
 New-OSDCloudTemplate -Name WinRE -WinRE
-
 
 Write-Host
 Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
