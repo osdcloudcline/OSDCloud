673cfdf41bc1a835c2256229d92cee028d79ae31
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-29 01:12:41 -0500

    Update PRO.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PRO.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PRO.ps1
@@ -79,3 +79,26 @@ Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
 # ASRock AM 4 Motherboards
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 4 PRO Motherboard Ethernet Drivers..." -Verbose 
+$PROEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PRO\Ethernet1"
+$PROEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PRO\Ethernet2"
+$PROEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PRO\Ethernet3"
+$PROEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PRO\Ethernet4"
+$PROEthernet5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PRO\Ethernet5"
+
+Edit-OSDCloudWinPE -DriversPath $PROEthernet1
+Edit-OSDCloudWinPE -DriversPath $PROEthernet2
+Edit-OSDCloudWinPE -DriversPath $PROEthernet3
+Edit-OSDCloudWinPE -DriversPath $PROEthernet4
+Edit-OSDCloudWinPE -DriversPath $PROEthernet5
+
+Write-Host
+Write-Verbose "Processing: ASRock AM 4 PRO Motherboard Storage Drivers..." -Verbose 
+$PROStorage = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\PRO\Storage"
+
+Edit-OSDCloudWinPE -DriversPath $PROStorage
+
+Write-Host
+Write-Verbose "Completed: Integration of ASRock AMD AM 4 Motherboard Drivers..." -Verbose 
