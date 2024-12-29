6fc51ff0c884f4e828c3d44e76e8bc6431c17099
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 22:58:33 -0500

    Update VBSScriptingSupport-Download.ps1

--- Extra Files/Scripting Support/VBS Scripting/VBSScriptingSupport-Download.ps1
+++ Extra Files/Scripting Support/VBS Scripting/VBSScriptingSupport-Download.ps1
@@ -1,1 +1,23 @@
+Write-Host '                                                                              '
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' The following scripting support will be integrated into OSDCloud boot.wim:   ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - VBS Scripting Support                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+Write-Host '                                                                              '
+pause
+
+
+#######################################################################################
+###########     OSDCloud VBS Script DOWNLOAD/EXTRACTION LOCATION       ################
+#######################################################################################
+
+$OSDCloudVBS_Extract = "C:\OSDCloud\GitHub\downloads\VBSScript"
+$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+
+#######################################################################################
+####               VBS SCRIPT SUPPORT URL                             #################
+#######################################################################################
+
+$VBSScriptSupport_URL = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/Scripting%20Support/VBS%20Scripting/VBScriptCABFiles.zip"
+
 
