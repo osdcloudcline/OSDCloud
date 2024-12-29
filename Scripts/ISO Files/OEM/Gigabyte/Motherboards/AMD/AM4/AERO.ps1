1ee29e299c3c04bb7f1cf4bed45092a8b046e5ce
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-29 10:28:59 -0500

    Update AERO.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/AERO.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/AERO.ps1
@@ -24,7 +24,7 @@ Import-Module -Name OSD -Force
 
 
 Write-Host
-Write-Verbose "Processing: Gigabyte AM4 Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
+Write-Verbose "Processing: Gigabyte AM4 AERO Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
 Write-Host 
 
 $AERO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/AERO/AERO.ps1")
@@ -54,3 +54,50 @@ Invoke-Expression $($UPBR.Content)
 $OSDCloudWallpaper = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Wallpaper/OSDCloudWallpaper-Download.ps1")
 Invoke-Expression $($OSDCloudWallpaper.Content) 
 
+Write-Host
+Write-Verbose "Completed: ALL Gigabyte AM4 AERO Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
+Write-Host
+
+Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
+New-OSDCloudTemplate -Name WinRE -WinRE
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = Read-Host -Prompt 'Please enter custom path for new OSDCloud Workspace'
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
+
+# Gigabyte AM 4 Motherboards
+
+Write-Host
+Write-Verbose "Processing: Gigabyte AM 4 AERO Motherboard Ethernet Drivers..." -Verbose 
+$AEROEthernet = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\Ethernet"
+
+Edit-OSDCloudWinPE -DriversPath $AEROEthernet
+
+Write-Host
+Write-Verbose "Processing: Gigabyte AM 4 AERO Motherboard WiFi Drivers..." -Verbose 
+$AEROWiFi1 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\WiFi1"
+$AEROWiFi2 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\WiFi2"
+
+Edit-OSDCloudWinPE -DriversPath $AEROWiFi1
+Edit-OSDCloudWinPE -DriversPath $AEROWiFi2
+
+Write-Host
+Write-Verbose "Processing: Gigabyte AM 4 AERO Motherboard Storage Drivers..." -Verbose 
+$AEROStorage1 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\Storage1"
+$AEROStorage2 = "C:\OSDCloud\Drivers\Motherboards\Gigabyte\AM4\AERO\Storage2"
+
+Edit-OSDCloudWinPE -DriversPath  $AEROStorage1
+Edit-OSDCloudWinPE -DriversPath  $AEROStorage2
