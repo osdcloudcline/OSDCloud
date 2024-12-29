59a27cc53788bc5ba9fd9b1174d1214c07fee8a5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-28 23:05:45 -0500

    Create PortableBrowser-Download.ps1

---
+++ Extra Files/Browser/PortableBrowser-Download.ps1
@@ -0,0 +1,17 @@
+Write-Host '                                                                              '
+Write-Host '                                                                              ' -BackgroundColor White                                                              
+Write-Host ' The following extras will be integrated into OSDCloud boot.wim:              ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Google Chrome Portable Web Browser                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                              ' -BackgroundColor White
+Write-Host '                                                                              '
+pause
+
+######################################################################################
+####    OSDCloud Web Browser URL                                                 #######
+######################################################################################
+$OSDBrowser = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/Browser/Chrome.exe"
+
+######################################################################################
+####    OSDCloud Chrome Browser EXTRACTION LOCATION                            #######
+######################################################################################
+$ChromePath = "C:\OSDCloud\GitHub\downloads\Chrome.exe"
