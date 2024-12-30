c9756ff776e0162ebd82d9a1e1a95368db84d492
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-30 00:14:24 -0500

    Update AddModules.ps1

--- Scripts/ISO Files/PowerShell Modules/7.x/AddModules.ps1
+++ Scripts/ISO Files/PowerShell Modules/7.x/AddModules.ps1
@@ -2,3 +2,7 @@ Write-Verbose "Processing: Adding Cloud-related PowerShell 7.x modules to OSDClo
 Write-Host
 
 Edit-OSDCloudWinPE -PSModuleInstall Azure
+
+Write-Host
+Write-Verbose "Completed: Adding Cloud-related PowerShell 7.x modules to OSDCloud ISO..." -Verbose
+Write-Host
