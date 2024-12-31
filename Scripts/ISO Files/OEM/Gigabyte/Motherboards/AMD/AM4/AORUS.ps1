e52629ce925a716e37faa80f0cb315afa8b4f8a1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-30 22:34:56 -0500

    Update AORUS.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/AORUS.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/AORUS.ps1
@@ -28,6 +28,9 @@ Write-Host
 Write-Verbose "Processing: Gigabyte AM4 AORUS Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
 Write-Host 
 
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
 $AORUS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/AORUS/AORUS.ps1")
 Invoke-Expression $($AORUS.Content)
 
