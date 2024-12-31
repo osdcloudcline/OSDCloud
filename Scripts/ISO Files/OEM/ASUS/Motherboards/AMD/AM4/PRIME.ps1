2397d065ccfd3980d434fed522b50878fb260117
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-30 22:32:26 -0500

    Update PRIME.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/PRIME.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/PRIME.ps1
@@ -27,6 +27,9 @@ Write-Host
 Write-Verbose "Processing: ASUS AM4 PRIME Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
 Write-Host
 
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
 $PRIME = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/PRIME%20Motherboards/PRIME.ps1")
 Invoke-Expression $($PRIME.Content)
 
