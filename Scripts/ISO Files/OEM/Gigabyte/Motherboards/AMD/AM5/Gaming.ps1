9b2265341143b3b1969aebc7ff6ad6526f9173ad
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-31 01:17:21 -0500

    Update Gaming.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Gaming.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Gaming.ps1
@@ -87,3 +87,8 @@ Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
+
+# Adding PowerShell 7 to OSDCloud 
+
+$AddPS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/AddPS7-OSDCloudISO.ps1")
+Invoke-Expression $($AddPS7.Content)
