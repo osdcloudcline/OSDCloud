11494f7afffd11daf5caf18c6f51741927b622df
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-30 22:27:04 -0500

    Update PS7Download.ps1

--- Scripts/ISO Files/PowerShell Modules/PS 7 Support to OSDCloud/PS7Download.ps1
+++ Scripts/ISO Files/PowerShell Modules/PS 7 Support to OSDCloud/PS7Download.ps1
@@ -1,1 +1,22 @@
+$PS7ToOSDCloudISOLog = "C:\Logs\OSDCloud\PowerShell 7 Support\PS7-Download.log"
 
+Start-Transcript -Path $PS7ToOSDCloudISOLog
+
+
+Import-Module -Name OSD -Force
+
+
+# File Paths
+$PS7Source = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/PowerShell-7.4.6-win-x64.zip"
+$PS7Destination = "C:\downloads\OSDCloud"
+
+Write-Verbose "Processing: Windows PowerShell 7 Download" -Verbose
+Write-Host
+
+Save-WebFile -SourceUrl $PS7Source -DestinationDirectory $PS7Destination 
+
+Write-Host
+Write-Verbose "Completed: PowerShell 7, Version 7.4.6 64-bit has downloaded" -Verbose
+Write-Host
+
+Stop-Transcript
