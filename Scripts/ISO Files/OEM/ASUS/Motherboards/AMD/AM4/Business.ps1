186f52462760bac875330dc4eb6924a6d5e775f0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-30 22:31:37 -0500

    Update Business.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/Business.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM4/Business.ps1
@@ -27,6 +27,9 @@ Write-Host
 Write-Verbose "Processing: ASUS AM4 Business Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
 Write-Host 
 
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
 $Business = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/AMD/AM%204/Business%20Motherboards/Business.ps1")
 Invoke-Expression $($Business.Content)
 
