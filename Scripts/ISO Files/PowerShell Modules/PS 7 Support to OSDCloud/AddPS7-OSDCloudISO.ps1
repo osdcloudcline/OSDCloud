a4b9dc85d48e66ea920924880ed23ce2dd92b5b5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2024-12-31 01:25:12 -0500

    Update AddPS7-OSDCloudISO.ps1

--- Scripts/ISO Files/PowerShell Modules/PS 7 Support to OSDCloud/AddPS7-OSDCloudISO.ps1
+++ Scripts/ISO Files/PowerShell Modules/PS 7 Support to OSDCloud/AddPS7-OSDCloudISO.ps1
@@ -13,10 +13,12 @@ $PS7Extract = "C:\downloads\OSDCloud\PS7"
 $mountdir = "C:\Mount"
 
 
-
+Write-Verbose "Processing: Creating mount directory" -Verbose
 # Create Mount Directory
 New-Item -Path $mountdir -ItemType Directory -Force
 
+
+Write-Verbose "Processing: Mounting OSDCloud boot.wim" -Verbose
 # Mount the image
 $sourceWIMDir = "\Media\sources"
 $WorkspacePath = Get-OSDCloudWorkspace
@@ -26,11 +28,13 @@ Mount-WindowsImage -ImagePath "$WimFileh\boot.wim" -Path $mountdir -Index 1
 Write-Verbose "Processing: Expanding Windows PowerShell 7 ZIP File" -Verbose
 Expand-Archive -Path "$PS7Destination\PowerShell-7.4.6-win-x64.zip" -DestinationPath "$mountdir\Program Files\PowerShell\7" -Force
 
+Write-Verbose "Processing: Updating environment PATHS" -Verbose
 # Update the offline environment PATH for PowerShell 7
 $HivePath = "$mountdir\Windows\System32\config\SYSTEM"
 reg load "HKLM\OfflineWinPE" $HivePath 
 Start-Sleep -Seconds 5
 
+Write-Verbose "Processing: Adding PowerShell 7 Paths to PATH and PSModulePath" -Verbose
 # Add PowerShell 7 Paths to Path and PSModulePath
 $RegistryKey = "HKLM:\OfflineWinPE\ControlSet001\Control\Session Manager\Environment"
 $CurrentPath = (Get-Item -path $RegistryKey ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
@@ -41,6 +45,7 @@ $CurrentPSModulePath = (Get-Item -path $RegistryKey ).GetValue('PSModulePath', '
 $NewPSModulePath = $CurrentPSModulePath + ";%ProgramFiles%\PowerShell\;%ProgramFiles%\PowerShell\7\;%SystemRoot%\system32\config\systemprofile\Documents\PowerShell\Modules\"
 $Result = New-ItemProperty -Path $RegistryKey -Name "PSModulePath" -PropertyType ExpandString -Value $NewPSModulePath -Force 
 
+Write-Verbose "Processing: Adding additional variables for PowerShell Gallery Support" -Verbose
 # Add additional environment variables for PowerShell Gallery Support
 $APPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Roaming"
 $Result = New-ItemProperty -Path $RegistryKey -Name "APPDATA" -PropertyType String -Value $APPDATA -Force 
@@ -54,11 +59,17 @@ $Result = New-ItemProperty -Path $RegistryKey -Name "HOMEPATH" -PropertyType Str
 $LOCALAPPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Local"
 $Result = New-ItemProperty -Path $RegistryKey -Name "LOCALAPPDATA" -PropertyType String -Value $LOCALAPPDATA -Force 
 
+Write-Verbose "Processing: Cleaning up" -Verbose
 # Cleanup (to prevent access denied issue unloading the registry hive)
 Get-Variable Result | Remove-Variable
 Get-Variable RegistryKey | Remove-Variable
 [gc]::collect()
 Start-Sleep -Seconds 5
 
+Write-Verbose "Processing: Unloading the registry hive" -Verbose
 # Unload the registry hive
 reg unload "HKLM\OfflineWinPE"
+
+Write-Verbose "Processing: Dismounting and saving the OSDCloud boot.wim" -Verbose
+# dismounting and saving the image
+Dismount-WindowsImage -Path $mountdir -Save
