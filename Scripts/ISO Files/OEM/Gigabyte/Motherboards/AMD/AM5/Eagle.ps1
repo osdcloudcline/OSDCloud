4ed69d6a47f6eecd95dc508b904a867081935697
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-01 08:14:41 -0500

    Update Eagle.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Eagle.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Eagle.ps1
@@ -28,6 +28,9 @@ Write-Host
 Write-Verbose "Processing: Gigabyte AM5 Eagle Motherboard drivers, Virtualization drivers, scripting support and other utilities file downloads" -Verbose
 Write-Host
 
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
 $Eagle = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Eagle/Eagle.ps1")
 Invoke-Expression $($Eagle.Content)
 
