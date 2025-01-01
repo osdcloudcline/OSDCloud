a7dded9be917c08da62a9d6e8aa92ab6c31575ff
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-01 08:15:02 -0500

    Update Gaming.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Gaming.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Gaming.ps1
@@ -28,6 +28,9 @@ Write-Host
 Write-Verbose "Processing: Gigabyte AM5 Gaming Motherboard drivers, Virtualization drivers, scripting support and other utilities file downloads" -Verbose
 Write-Host
 
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
 $Gaming = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM5/Gaming/Gaming.ps1")
 Invoke-Expression $($Gaming.Content)
 
