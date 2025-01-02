91f6e05f6454f5d0edcbbcc8170d603a7d1369a4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-02 16:39:34 -0500

    Update Ghosting.ps1

--- Extra Files/Imaging/Ghost/Ghosting.ps1
+++ Extra Files/Imaging/Ghost/Ghosting.ps1
@@ -1,1 +1,15 @@
+$GhostLog = ""
 
+Start-Transcript -Path $GhostLog
+
+Import-Module -Name OSD -Force
+
+#################################################
+## Ghost EXE download URL
+################################################
+
+$Ghost64URL = ""
+
+$GhostExplorerURL = ""
+
+$GhostServURL = ""
