fb4ef42c4ff5521c449ad10452a2d6adeae423fc
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-02 16:39:53 -0500

    Update Ghosting.ps1

--- Extra Files/Imaging/Ghost/Ghosting.ps1
+++ Extra Files/Imaging/Ghost/Ghosting.ps1
@@ -8,7 +8,7 @@ Import-Module -Name OSD -Force
 ## Ghost EXE download URL
 ################################################
 
-$Ghost64URL = ""
+$Ghost64URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghost64.exe"
 
 $GhostExplorerURL = ""
 
