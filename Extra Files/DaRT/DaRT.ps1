591969a3bbbb7a608c32f441c12b4bb1bb52f46d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-02 22:53:03 -0500

    Update DaRT.ps1

--- Extra Files/DaRT/DaRT.ps1
+++ Extra Files/DaRT/DaRT.ps1
@@ -16,13 +16,17 @@ $DARTConfig8URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main
 ################################################################
 
 $DARTDestination = "C:\OSDCloud\GitHub\downloads\DART"
-$DART64CABDestination = ""
-$DARTConfig8Destination = ""
+$DART64CABDestination = "C:\Program Files\Microsoft DaRT\v10"
+$DARTConfig8Destination = "C:\Program Files\Microsoft Deployment Toolkit\Templates"
 
 Import-Module -Name OSD -Force
 
+Write-Host
 Write-Verbose "Processing: Acquiring Microsoft DART Files..." -Verbose
 Save-WebFile -SourceUrl $DART64CABURL -DestinationDirectory $DARTDestination
 Save-WebFile -SourceUrl $DARTConfig8URL -DestinationDirectory $DARTDestination
 
+Write-Host "Processing: Copying Microsoft DART Files..." -Verbose
+Copy-Item -Path "$DARTDestination\Toolsx64.cab" -Destination $DART64CABDestination -Force
+Copy-Item -Path "$DARTDestination\DartConfig8.dat" -Destination $DARTConfig8Destination -Force
 Stop-Transcript 
