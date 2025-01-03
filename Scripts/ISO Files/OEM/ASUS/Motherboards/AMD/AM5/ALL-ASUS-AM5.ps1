62f8b371c541702b28059acb15fc86126e0a7f62
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-03 00:19:45 -0500

    Update ALL-ASUS-AM5.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM5/ALL-ASUS-AM5.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM5/ALL-ASUS-AM5.ps1
@@ -32,3 +32,105 @@ Write-Host
 
 $PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
 Invoke-Expression $($PS7.Content)
+
+$HyperV = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/Hyper-V-NetworkDrivers.ps1")
+Invoke-Expression $($HyperV.Content)
+
+$Proxmox = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox.ps1")
+Invoke-Expression $($Proxmox.Content)
+
+$ESXI = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXI-Drivers.ps1")
+Invoke-Expression $($ESXI.Content)
+
+$VMWareWSPro = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20Workstation/VMWare-WorkstationPRO-Drivers.ps1")
+Invoke-Expression $($VMWareWSPro.Content)
+
+$VBS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Scripting%20Support/VBS%20Scripting/VBSScriptingSupport-Download.ps1")
+Invoke-Expression $($VBS.Content)
+
+$PortableBrowser = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Browser/PortableBrowser-Download.ps1")
+Invoke-WebRequest $($PortableBrowser.Content)
+
+$UPBR = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/User%20Profile%20Backup%20Restore/UPBR-Download.ps1")
+Invoke-Expression $($UPBR.Content)
+
+$OSDCloudWallpaper = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Wallpaper/OSDCloudWallpaper-Download.ps1")
+Invoke-Expression $($OSDCloudWallpaper.Content) 
+
+$Ghost = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghosting.ps1")
+Invoke-Expression $($Ghost.Content)
+
+$DaRT = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/DaRT/DaRT.ps1")
+Invoke-WebRequest $($DaRT.Content)
+
+Write-Host
+Write-Verbose "Completed: ALL ASUS AM4 Motherboard driver, Virtualization driver, scripting support and other utilities file downloads" -Verbose
+Write-Host
+
+# OSDCloud Template and Workspace configuration
+
+Write-Host
+Write-Verbose "Confirming OSDCloud Templates......" -Verbose
+Get-OSDCloudTemplate
+
+Write-Host
+Write-Verbose "Creating New OSDCloud WinRE Template specific for ALL ASUS AM4 motherboards to enable wireless networking support..." -Verbose
+New-OSDCloudTemplate -Name ASUS-AM4 -WinRE
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = Read-Host -Prompt 'Please enter custom path for new OSDCloud Workspace'
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
+
+# Adding PowerShell 7 to OSDCloud 
+
+$AddPS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/AddPS7-OSDCloudISO.ps1")
+Invoke-Expression $($AddPS7.Content)
+
+# 3rd party software and utilities
+
+# VBS Scripting Support
+
+# Portable Browsers
+
+# MS DaRT Remote Connections and Troubleshooting
+
+# ServiceUI
+
+# CMTrace 
+
+# Ghost Imaging
+
+# User Profile Backup and Restore 
+
+# ASUS AM 5 Motherboards
+
+# Business motherboards
+
+# CSM motherboards
+
+# Expedition motherboards
+
+# Others motherboards
+
+# PRIME motherboards
+
+# ProArt motherboards
+
+# ROG - Republic of Gamers motherboards
+
+# TUF Gaming motherboards
+
