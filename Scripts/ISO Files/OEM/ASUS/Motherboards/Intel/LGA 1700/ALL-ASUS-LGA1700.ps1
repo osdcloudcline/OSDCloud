1118017b97bb2e0a4a8144116fa58b993dd17a7d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-03 08:14:28 -0500

    Update ALL-ASUS-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/ALL-ASUS-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/ALL-ASUS-LGA1700.ps1
@@ -103,7 +103,7 @@ Get-OSDCloudTemplate
 
 Write-Host
 Write-Verbose "Creating New OSDCloud WinRE Template specific for ALL ASUS AM4 motherboards to enable wireless networking support..." -Verbose
-New-OSDCloudTemplate -Name ASUS-AM5 -WinRE
+New-OSDCloudTemplate -Name ASUS-LGA1700 -WinRE
 
 Write-Host
 Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
