09557e637929e2dc93e5e25b3311763f837262e7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-03 09:28:42 -0500

    Update MSTSC.ps1

--- Extra Files/Remote Desktop Control/MSTSC.ps1
+++ Extra Files/Remote Desktop Control/MSTSC.ps1
@@ -1,1 +1,30 @@
+$MSTSCLog = ""
 
+Start-Process -Path $MSTSCLog
+
+Get-Date
+
+############################################
+# MSTSC RDP File URLs
+############################################
+
+$MSTSCFile1URL = ""
+$MSTSCFile2URL = ""
+$MSTSCFile3URL = ""
+$MSTSCFile4URL = ""
+$MSTSCFile5URL = ""
+$MSTSCFile6URL = ""
+$MSTSCFile7URL = ""
+$MSTSCFile8URL = ""
+$MSTSCFile9URL = ""
+$MSTSCFile10URL = ""
+$MSTSCFile11URL = ""
+$MSTSCFile12URL = ""
+$MSTSCFile13URL = ""
+$MSTSCFile14URL = ""
+$MSTSCFile15URL = ""
+$MSTSCFile16URL = ""
+
+Import-Module -Name OSD -Force
+
+Write-Verbose "Processing: Acquiring files for Microsoft Remote Desktop integration to OSDCloud..." -Verbose
