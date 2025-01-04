23129479d2e06f99e42dfec45677ef040f47130a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-04 08:05:24 -0500

    Update OSDCloud-AM5.ps1

--- Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
+++ Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
@@ -7,20 +7,23 @@ Write-Host
 Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
 Write-Host
 Write-Host 
-Write-Host '                                                                                              ' -BackgroundColor White  
+Write-Host '                                                                                              ' -BackgroundColor White                                                              
 Write-Host '    This scripts creates an OSCloud Deployment ISO with the following:                        ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host '     - Networking and Storage: ASRock AM5 Motherboard drivers                                 ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host '     - Networking and Storage: ASUS AM5 motherboard drivers                                   ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host '     - Networking and Storage: MSI AM5 Motherboard drivers                                    ' -ForegroundColor DarkBlue -BackgroundColor White
-Write-Host '     - Networking and Storage: Gigabyte AM5 Motherboard drivers                               ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Networking and Storage: ASRock AM5 motherboard drivers                                 ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '     - Networking and Storage: VM drivers for ESXI, Hyper-V, VMWare Workstation and Proxmox   ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '     - VBS Scripting Support                                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '     - Wireless networking support                                                            ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '     - FOR SYSTEM REFRESH: User Profile Backup and Restore for data backup                    ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '     - Google Chrome portable browser                                                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - PowerShell Modules: Version 5.x and 7.x                                                ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Imaging Tools: Symantec Ghost                                                          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - System Rescue/Troubleshooting: Microsoft DaRT Support                                  ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                                              ' -BackgroundColor White
 pause
 Clear-Host
+
+Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name OSD -Force
 
 Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
 New-OSDCloudTemplate -Name WinRE -WinRE
