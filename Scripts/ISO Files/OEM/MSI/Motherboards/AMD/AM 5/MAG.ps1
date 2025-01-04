7899d636f4773b5fc75b5737dbb260b0cd1a7ad0
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-03 20:50:28 -0500

    Update MAG.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MAG.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MAG.ps1
@@ -79,7 +79,7 @@ Write-Verbose "Confirming OSDCloud Templates......" -Verbose
 Get-OSDCloudTemplate
 
 Write-Host
-Write-Verbose "Creating New OSDCloud WinRE Template specific for ALL MSI AM5 motherboards to enable wireless networking support..." -Verbose
+Write-Verbose "Creating New OSDCloud WinRE Template specific for ALL MSI MAG AM5 motherboards to enable wireless networking support..." -Verbose
 New-OSDCloudTemplate -Name MSI-MAG-AM5 -WinRE
 
 Write-Host
