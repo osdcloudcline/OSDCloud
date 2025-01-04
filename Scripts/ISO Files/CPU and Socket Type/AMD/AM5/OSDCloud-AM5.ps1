618a43c5af9c8d98a99171858c9f35e60d94f660
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-04 08:26:14 -0500

    Update OSDCloud-AM5.ps1

--- Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
+++ Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
@@ -128,6 +128,90 @@ Write-Host
 Write-Verbose "Completed: MSI AM5 Motherboard driver downloads" -Verbose
 Write-Host
 
+Write-Host
+Write-Verbose "Processing: Microsoft Hyper-V virtualization driver downloads" -Verbose
+Write-Host 
+
+$HyperV = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Hyper-V/Hyper-V-NetworkDrivers.ps1")
+Invoke-Expression $($HyperV.Content)
+
+Write-Host
+Write-Verbose "Processing: Proxmox virtualization driver downloads" -Verbose
+Write-Host
+
+$Proxmox = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Proxmox/Proxmox.ps1")
+Invoke-Expression $($Proxmox.Content)
+
+Write-Host
+Write-Verbose "Processing: VMWare ESXI vSphere virtualization driver downloads" -Verbose
+Write-Host
+
+$ESXI = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20ESXI/ESXI-Drivers.ps1")
+Invoke-Expression $($ESXI.Content)
+
+Write-Host
+Write-Verbose "Processing: VMWare Workstation Pro virtualization driver downloads" -Verbose
+Write-Host
+
+$VMWareWSPro = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/VMWare%20Workstation/VMWare-WorkstationPRO-Drivers.ps1")
+Invoke-Expression $($VMWareWSPro.Content)
+
+Write-Host
+Write-Verbose "Completed: ALL Virtualization driver downloads" -Verbose
+Write-Host
+
+Write-Host
+Write-Verbose "Processing: Additional OSDCloud scripting support downloads" -Verbose
+Write-Host
+
+$VBS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Scripting%20Support/VBS%20Scripting/VBSScriptingSupport-Download.ps1")
+Invoke-Expression $($VBS.Content)
+
+Write-Host
+Write-Verbose "Processing: Google Chrome - portable browser" -Verbose
+Write-Host
+
+$PortableBrowser = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Browser/PortableBrowser-Download.ps1")
+Invoke-WebRequest $($PortableBrowser.Content)
+
+Write-Host
+Write-Verbose "Processing: User Profile Backup and Restore" -Verbose
+Write-Host
+
+$UPBR = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/User%20Profile%20Backup%20Restore/UPBR-Download.ps1")
+Invoke-Expression $($UPBR.Content)
+
+Write-Host
+Write-Verbose "Processing: System Imaging utilities" -Verbose
+Write-Host
+
+$Ghost = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghosting.ps1")
+Invoke-Expression $($Ghost.Content)
+
+Write-Host
+Write-Verbose "Processing: Microsoft DaRT and Remote Desktop Connections" -Verbose
+Write-Host
+
+$DaRT = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/DaRT/DaRT.ps1")
+Invoke-WebRequest $($DaRT.Content)
+
+$MSTSC = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Remote%20Desktop%20Control/MSTSC.ps1")
+Invoke-Expression $($MSTSC.Content)
+
+Write-Host
+Write-Verbose "Completed: Download of Extra OSDCloud ISO Utilities" -Verbose
+Write-Host
+
+Write-Host
+Write-Verbose "Processing: NEW OSDCloud wallpaper" -Verbose
+Write-Host
+
+$OSDCloudWallpaper = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Wallpaper/OSDCloudWallpaper-Download.ps1")
+Invoke-Expression $($OSDCloudWallpaper.Content) 
+
+Write-Host
+Write-Verbose "Completed: OSDCloud wallpaper download" -Verbose
+Write-Host
 
 Write-Host
 Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
