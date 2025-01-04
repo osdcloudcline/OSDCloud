f9a0e14f2996e56e661a00156e2550d2cc64f5d3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-04 08:06:02 -0500

    Update OSDCloud-AM5.ps1

--- Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
+++ Scripts/ISO Files/CPU and Socket Type/AMD/AM5/OSDCloud-AM5.ps1
@@ -28,6 +28,18 @@ Import-Module -Name OSD -Force
 Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
 New-OSDCloudTemplate -Name WinRE -WinRE
 
+Write-Host
+Write-Verbose "Processing: PowerShell 7.x support downloads" -Verbose
+Write-Host 
+
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
+Write-Host
+Write-Verbose "Completed: PowerShell 7.x support downloads" -Verbose
+Write-Host
+
+
 
 Write-Host
 Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
