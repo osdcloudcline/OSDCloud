627281737e4cd91c68e6e2d10fa67783ec41ca75
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-05 13:44:53 -0500

    Update PRIME.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM5/PRIME.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM5/PRIME.ps1
@@ -44,7 +44,7 @@ $PRIME = Invoke-WebRequest("")
 Invoke-Expression $($PRIME.Content)
 
 Write-Host
-Write-Verbose "Completed: ASRUS AMD AM5 motherboard driver downloads" -Verbose
+Write-Verbose "Completed: ASUS AMD AM5 motherboard driver downloads" -Verbose
 Write-Host 
 
 Write-Host
