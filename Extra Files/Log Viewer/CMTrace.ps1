9e7842df4690ef0d36b1227efb1e77950b8c2cc1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-05 15:36:32 -0500

    Update CMTrace.ps1

--- Extra Files/Log Viewer/CMTrace.ps1
+++ Extra Files/Log Viewer/CMTrace.ps1
@@ -1,1 +1,11 @@
+$CMTraceLog = "C:\Logs\OSDCloud\LogViewer\CMTrace.log"
 
+Start-Transcript -Path $MSTSCLog
+
+Get-Date
+
+############################################
+# CM Trace File URLs
+############################################
+
+$CMTraceFile1URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Remote%20Desktop%20Control/d3d10.dll"
