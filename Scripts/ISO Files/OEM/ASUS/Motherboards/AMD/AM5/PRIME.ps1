f214aaa8f474b22d75cfca3bcc14489080dbc3ee
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-05 13:48:19 -0500

    Update PRIME.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM5/PRIME.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/AMD/AM5/PRIME.ps1
@@ -37,14 +37,14 @@ Write-Verbose "Completed: PowerShell 7.x support downloads" -Verbose
 Write-Host
 
 Write-Host
-Write-Verbose "Processing: ASUS AMD AM5 motherboard driver downloads" -Verbose
+Write-Verbose "Processing: ASUS AMD AM5 PRIME motherboard driver downloads" -Verbose
 Write-Host  
 
 $PRIME = Invoke-WebRequest("")
 Invoke-Expression $($PRIME.Content)
 
 Write-Host
-Write-Verbose "Completed: ASUS AMD AM5 motherboard driver downloads" -Verbose
+Write-Verbose "Completed: ASUS AMD AM5 PRIME motherboard driver downloads" -Verbose
 Write-Host 
 
 Write-Host
