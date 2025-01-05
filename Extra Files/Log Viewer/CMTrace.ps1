81d42d908581e061b49e9276152e2bdc88623d8c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-05 15:39:03 -0500

    Update CMTrace.ps1

--- Extra Files/Log Viewer/CMTrace.ps1
+++ Extra Files/Log Viewer/CMTrace.ps1
@@ -9,3 +9,18 @@ Get-Date
 ############################################
 
 $CMTraceFile1URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Log%20Viewer/CMTrace.exe"
+
+Import-Module -Name OSD -Force
+
+
+####################################################################
+# Log Viewer - CM Trace DL File Destination ##############
+####################################################################
+
+$CMTraceDestination = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer"
+
+
+
+Write-Verbose "Processing: Acquiring CM Trace for integration to OSDCloud..." -Verbose
+Write-Host
+Save-WebFile -SourceUrl $CMTraceFile1URL -DestinationDirectory $CMTraceDestination
