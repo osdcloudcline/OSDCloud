9cfead0bb3af7e9c27892080b46f109a0f01dbf3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-05 22:18:57 -0500

    Update ExplorerPP.ps1

--- Extra Files/File Explorer/ExplorerPP.ps1
+++ Extra Files/File Explorer/ExplorerPP.ps1
@@ -1,1 +1,31 @@
+$ExplorerPPLog = "C:\Logs\OSDCloud\File Explorer\ExplorerPP.log"
 
+Start-Transcript -Path $ExplorerPPLog
+
+Get-Date
+
+############################################
+# Explorer + +  File URLs
+############################################
+
+$ExplorerPPFile1URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Log%20Viewer/CMTrace.exe"
+
+Import-Module -Name OSD -Force
+
+
+####################################################################
+# Explorer + + DL File Destination ##############
+####################################################################
+
+$ExplorerPPDestination = "C:\OSDCloud\downloads\GitHub\FileExplorer"
+
+
+
+Write-Verbose "Processing: Acquiring Explorer + + for integration to OSDCloud..." -Verbose
+Write-Host
+Save-WebFile -SourceUrl $ExplorerPPFile1URL -DestinationDirectory $ExplorerPPDestination
+
+Write-Host
+Write-Verbose "Completed: Download of Explorer + +for integration to OSDCloud..." -Verbose
+
+Stop-Transcript
