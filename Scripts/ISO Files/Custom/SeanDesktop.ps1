80e3076bd13e3034dfdfde10b53655b8d5cce0e3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-06 17:43:39 -0500

    Update SeanDesktop.ps1

--- Scripts/ISO Files/Custom/SeanDesktop.ps1
+++ Scripts/ISO Files/Custom/SeanDesktop.ps1
@@ -344,11 +344,38 @@ $OSDCloudPS7xMods = Invoke-WebRequest("https://raw.githubusercontent.com/osdclou
 Invoke-Expression $($OSDCloudPS7xMods.Content)
 
 #############################################
-# INTEL LGA 1851 Driver Integration - ASUS
+# Alienware M18 R2 - Sean Laptop
 #############################################
 
 Write-Host
-Write-Verbose "Processing: ASUS LGA 1851 drivers" -Verbose
+Write-Verbose "Processing: Alienware M18 R2 drivers" -Verbose
+Write-Host
+
+Write-Verbose "Processing: Alienware M18 R2 Ethernet Driver..." -Verbose
+$EthernetDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\Ethernet"
+
+Edit-OSDCloudWinPE -DriverPath $EthernetDriver
+
+Write-Host
+Write-Verbose "Processing: Alienware M18 R2 Bluetooth Driver..." -Verbose
+$BluetoothDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\Bluetooth"
+
+Edit-OSDCloudWinPE -DriverPath $BluetoothDriver
+
+Write-Host
+Write-Verbose "Processing: Alienware M18 R2 WiFi Driver..." -Verbose
+$WiFiDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\WiFi"
+
+Edit-OSDCloudWinPE -DriverPath $WiFiDriver 
+
+Write-Host
+Write-Verbose "Processing: Alienware M18 R2 Storage Driver..." -Verbose
+$StorageDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $StorageDriver 
+
+Write-Host
+Write-Verbose "Completed: Integration of Alienware M18 R2 drivers into OSDCloud" -Verbose
 Write-Host
 
 
