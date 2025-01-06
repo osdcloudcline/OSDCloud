fb6ef07733d69c8aa673538f4c807490cc28d0d3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-05 19:37:07 -0500

    Update MPG.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MPG.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MPG.ps1
@@ -287,9 +287,25 @@ Write-Host
 
 # MS DaRT Remote Connections and Troubleshooting
 
-# ServiceUI
+# CloudPC
+
+Write-Host
+Write-Verbose "Processing: Microsoft Remote Desktop - Cloud PC for OSDCloud..." -Verbose 
+$CloudPCPath1 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files"
+$CloudPCPath2 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files\en-us"
+$CloudPCDestination1 = "$mountdir\Windows\System32"
+$CloudPCDestination2 = "$mountdir\Windows\System32\en-US"
+
+Copy-Item -Path "$CloudPCPath1\*" -Destination $CloudPCDestination1  -Force
+Copy-Item -Path "$CloudPCPath2\*" -Destination $CloudPCDestination2  -Force
 
 # CMTrace 
+
+Write-Verbose "Processing: Microsoft Endpoint Configuration Manager Log Viewer CM Trace for OSDCloud..." -Verbose 
+$CMTracePath = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer"
+$CMTraceDestination = "$mountdir\Windows\System32"
+
+Copy-Item -Path $CMTracePath -Destination $CMTraceDestination -Force 
 
 # Ghost Imaging
 
