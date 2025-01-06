fe193ddba83c409cc344487ffabc5cb34eaa7936
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-06 15:59:02 -0500

    Update Other.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/Other.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/Other.ps1
@@ -250,12 +250,12 @@ Dismount-WindowsImage -Path $mountdir -Save
 
 # PowerShell 5.1 Modules - OSDCloud
 
-$OSDCloudPS5xMods = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/5.x/AddModules.ps1")
+$OSDCloudPS5xMods = Invoke-WebRequest("https://raw.githubusercontent.com/osdcloudcline/OSDCloud/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/5.x/AddModules.ps1")
 Invoke-Expression $($OSDCloudPS5xMods.Content)
 
 # PowerShell 7.x Modules - OSDCloud
 
-$OSDCloudPS7xMods = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/7.x/AddModules.ps1")
+$OSDCloudPS7xMods = Invoke-WebRequest("https://raw.githubusercontent.com/osdcloudcline/OSDCloud/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/7.x/AddModules.ps1")
 Invoke-Expression $($OSDCloudPS7xMods.Content)
 
 ###############################
