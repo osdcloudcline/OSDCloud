6e21dee9a38be0844cd04057e93c1a7b776a6eb1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-07 14:08:59 -0500

    Update NotepadPP.ps1

--- Extra Files/Text Editor/NotepadPP.ps1
+++ Extra Files/Text Editor/NotepadPP.ps1
@@ -1,1 +1,31 @@
+$NotepadPPLog = "C:\Logs\OSDCloud\Text Editor\NotepadPP.log"
 
+Start-Transcript -Path $NotepadPPLog
+
+Get-Date
+
+############################################
+# Notepad + +  File URLs
+############################################
+
+$NotepadPPFile1URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/File%20Explorer/Explorer++.exe"
+
+Import-Module -Name OSD -Force
+
+
+####################################################################
+# Explorer + + DL File Destination ##############
+####################################################################
+
+$NotepadPPDestination = "C:\OSDCloud\downloads\GitHub\TextEditor"
+
+
+
+Write-Verbose "Processing: Acquiring Notepad + + for integration to OSDCloud..." -Verbose
+Write-Host
+Save-WebFile -SourceUrl $NotepadPPFile1URL -DestinationDirectory $NotepadPPDestination
+
+Write-Host
+Write-Verbose "Completed: Download of Notepad ++for integration to OSDCloud..." -Verbose
+
+Stop-Transcript
