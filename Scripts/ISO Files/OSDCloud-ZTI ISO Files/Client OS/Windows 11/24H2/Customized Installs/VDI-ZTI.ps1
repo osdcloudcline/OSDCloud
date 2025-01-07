f9c8cb6636047510329b09127dba29895e237ea6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-07 18:11:19 -0500

    Update VDI-ZTI.ps1

--- Scripts/ISO Files/OSDCloud-ZTI ISO Files/Client OS/Windows 11/24H2/Customized Installs/VDI-ZTI.ps1
+++ Scripts/ISO Files/OSDCloud-ZTI ISO Files/Client OS/Windows 11/24H2/Customized Installs/VDI-ZTI.ps1
@@ -457,6 +457,46 @@ Write-Verbose "Completed: Integration of NEW OSDCloud Wallpaper..." -Verbose
 Write-Host
 
 ##########################################
+# Add Custom WIM Image
+##########################################
+Function Show-CustomImage{
+$CustomImageLog = "C:\Logs\OSDCloud\Images\AddCustom.log"
+Start-Transcript -Path $CustomImageLog
+$Question = Read-Host -Prompt 'Do you want or need to add an additional Windows Installation WIM file?'
+$ExtractWIM = Read-Host 'Do you need to extract a WIM Image before copying to OSDCloud?'
+If(($Question -and $ExtractWIM -eq "yes") -or ($Question -and $ExtractWIM -eq "Yes") -or ($Question -and $ExtractWIM -eq "y") -or ($Question -and $ExtractWIM -eq "Y")){
+$WindowsImage = Read-Host -Prompt 'Please specify path to the Windows image you want to add to OSDCloud (EG: D:\OS\Windows11)'
+$sourceWIM = "\sources\install.wim"
+$WIMFile = Join-Path -Path $WindowsImage '\Sources\install.wim'
+$Destination = "$(Get-OSDCloudWorkspace)\Media\OSDCloud\OS"
+Write-Verbose "Creating Custom WIM OS folder..." -Verbose
+New-Item -Path $Destination -ItemType Directory -Force
+Write-Host
+Write-Verbose "Exporting Client WIM File and copying Windows Image to OSDCloud..." -Verbose
+Get-WindowsImage -ImagePath $WIMFile | Format-Table ImageIndex, ImageName
+$Index = Read-Host -Prompt ' Select edition'
+$ExportWIMFileName = Read-Host -Prompt 'Please specify a file name for the exported WIM file, including the file extension (EG: Windows11ProWorkstations.wim)'
+$DescriptionName = Read-Host -Prompt 'Please enter a descriptive name for the image'
+Export-WindowsImage -SourceImagePath "$WIMFile" -SourceIndex $Index -DestinationImagePath "$Destination\$ExportWIMFileName" -DescriptionName "$DescriptionName" 
+Stop-Transcript
+}
+ElseIf (($Question -eq "yes" -and $ExtractWIM -eq "no") -or ($Question -eq "Yes"-and $ExtractWIM -eq "No") -or ($Question -eq "y" -and $ExtractWIM -eq "n") -or ($Question -eq "Y"-and $ExtractWIM -eq "N")){
+$CustomImageLog = "C:\Logs\OSDCloud\Images\AddCustom.log"
+Start-Transcript -Path $CustomImageLog
+$WindowsImage = Read-Host -Prompt 'Please specify path to the Windows image you want to add to OSDCloud (EG: D:\OS\Windows11)'
+$sourceWIM = "\sources\install.wim"
+$WIMFile = Join-Path -Path $WindowsImage -ChildPath $sourceWIM
+$Destination = "$(Get-OSDCloudWorkspace)\Media\OSDCloud\OS"
+Write-Verbose "Creating Custom WIM OS folder..." -Verbose
+New-Item -Path $Destination -ItemType Directory -Force
+Write-Verbose "Copying Windows Image to OSDCloud..." -Verbose
+Copy-Item -Path "$WIMFile" -Destination "$Destination" -Force
+Stop-Transcript
+}
+}
+Show-CustomImage
+
+##########################################
 # Remove Built In Apps
 ##########################################
 $mountdir = C:\Mount
