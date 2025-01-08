57447e08259f041a82d10ab48550394b29ddb1f9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-08 07:43:11 -0500

    Update PhantomGaming.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PhantomGaming.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PhantomGaming.ps1
@@ -35,7 +35,7 @@ Write-Host
 Write-Verbose "Processing: PowerShell 7.x support downloads" -Verbose
 Write-Host 
 
-$PS7 = Invoke-WebRequest("https://raw.githubusercontent.com/osdcloudcline/OSDCloud/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
 Invoke-Expression $($PS7.Content)
 
 Write-Host
