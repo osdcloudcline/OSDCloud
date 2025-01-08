0e0a88fcf10fce1b1bb86150ef0e610467580282
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-08 14:18:55 -0500

    Update Ghosting.ps1

--- Extra Files/Imaging/Ghost/Ghosting.ps1
+++ Extra Files/Imaging/Ghost/Ghosting.ps1
@@ -8,23 +8,13 @@ Import-Module -Name OSD -Force
 ## Ghost EXE download URL
 ################################################
 
-$Ghost64URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghost64.exe"
-
-$GhostExplorerURL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghostexp.exe"
-
-$GhostServURL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/GhostSrv.exe"
+$GhostURL = "https://github.com/osdcloudcline/OSDCloud/blob/main/Extra%20Files/Imaging/Ghost/Ghost.zip"
 
 $GhostDestination = "C:\OSDCloud\downloads\GitHub\Ghost"
 
-Write-Verbose "Processing: Ghost 64-bit" -Verbose
-Save-WebFile -SourceUrl $Ghost64URL -DestinationDirectory $GhostDestination
-
-Write-Verbose "Processing: Ghost Explorer" -Verbose
-Save-WebFile -SourceUrl $GhostExplorerURL -DestinationDirectory $GhostDestination
-
-Write-Verbose "Processing: Ghost Server" -Verbose
-Save-WebFile -SourceUrl $GhostServURL -DestinationDirectory $GhostDestination
-
+Write-Verbose "Processing: Symantec Ghost " -Verbose
+Save-WebFile -SourceUrl $GhostURL -DestinationDirectory $GhostDestination
+Expand-7Zip -ArchiveFileName "$GhostDestination\Ghost.zip" -Destination $GhostDestination
 Write-Host
 Write-Verbose "Completed: Symantec Ghost file downloads" -Verbose
 Write-Host
