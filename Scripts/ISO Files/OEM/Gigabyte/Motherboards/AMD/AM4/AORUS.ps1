7eafe3d4a9484fddffd550260adeb69e0aa3529d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-07 23:27:42 -0500

    Update AORUS.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/AORUS.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM4/AORUS.ps1
@@ -25,6 +25,8 @@ Clear-Host
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
 Import-Module -Name OSD -Force
 
+Install-Module -Name 7Zip4Powershell -Force -AllowClobber -SkipPublisherCheck -Verbose
+Import-Module -Name 7Zip4Powershell -Force
 
 Write-Host
 Write-Verbose "Processing: PowerShell 7.x support downloads" -Verbose
