b968d630699657e0ee45d5c3a8ec875d60f4f275
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-09 10:43:34 -0500

    Update Ghosting.ps1

--- Extra Files/Imaging/Ghost/Ghosting.ps1
+++ Extra Files/Imaging/Ghost/Ghosting.ps1
@@ -11,6 +11,7 @@ Import-Module -Name OSD -Force
 $GhostURL1 = "https://github.com/osdcloudcline/OSDCloud/blob/main/Extra%20Files/Imaging/Ghost/Ghost.zip"
 $GhostURL2 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghost64.exe"
 $GhostURL3 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/GhostSrv.exe"
+$GhostURL4 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/GhostSrv.exe"
 
 $GhostDestination = "C:\OSDCloud\downloads\GitHub\Ghost"
 
