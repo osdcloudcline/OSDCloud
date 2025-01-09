5c42a6ebf5b7cc3502205f5a99beb2c07302f42c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-09 01:22:27 -0500

    Update UPBR-Download.ps1

--- Extra Files/User Profile Backup Restore/UPBR-Download.ps1
+++ Extra Files/User Profile Backup Restore/UPBR-Download.ps1
@@ -1,6 +1,7 @@
 Write-Host '                                                                                      '
-Write-Host '                                                                                      ' -BackgroundColor White                                                              
+Write-Host '                                                                                      ' -BackgroundColor White     
 Write-Host ' The following extra files will be downloaded and integrated into OSDCloud boot.wim:  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                                      ' -BackgroundColor White 
 Write-Host '     - Operating System User Profile Backup and Restore                               ' -ForegroundColor DarkBlue -BackgroundColor White
 Write-Host '                                                                                      ' -BackgroundColor White
 Write-Host '                                                                                      '
@@ -16,10 +17,11 @@ $UPBR_URL = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/Us
 ####    USER PROFILE BACKUP AND RESTORE EXTRACTION LOCATION                    #######
 ######################################################################################
 $UPBRFilePath = "C:\OSDCloud\downloads\GitHub\ProfileBackupRestore"
+$OSDCloudUPBRdownload = "C:\OSDCloud\downloads\GitHub\ProfileBackupRestore"
 
 Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
 Import-Module -Name OSD -Force
 
 Write-Host "Acquiring User Profile Backup and Restore from GitHub repository..." -ForegroundColor Green
-Save-WebFile -SourceUrl $UPBR_URL -DestinationDirectory $OSDCloudGHdownloads
-Rename-Item -Path "$UPBRFilePath\UserProfileBackupRestore.exe" -NewName "C:\OSDCloud\downloads\GitHub\ProfileBackupRestore\UPBR.exe" -Force
+Save-WebFile -SourceUrl $UPBR_URL -DestinationDirectory $OSDCloudUPBRdownload
+Rename-Item -Path "$UPBRFilePath\UserProfileBackupRestore.exe" -NewName "$UPBRFilePath\UPBR.exe" -Force
