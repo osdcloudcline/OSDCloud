3700d760be5f480af5352f33c3f520578c4ca254
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-09 10:34:03 -0500

    Update Ghosting.ps1

--- Extra Files/Imaging/Ghost/Ghosting.ps1
+++ Extra Files/Imaging/Ghost/Ghosting.ps1
@@ -14,9 +14,26 @@ $GhostDestination = "C:\OSDCloud\downloads\GitHub\Ghost"
 
 Write-Verbose "Processing: Symantec Ghost " -Verbose
 Save-WebFile -SourceUrl $GhostURL -DestinationDirectory $GhostDestination
-Expand-7Zip -ArchiveFileName "$GhostDestination\Ghost.zip" -TargetPath $GhostDestination
 Write-Host
 Write-Verbose "Completed: Symantec Ghost file downloads" -Verbose
 Write-Host
 
+Write-Verbose "Confirming successful download of Symantec Ghost..." -Verbose
+$Ghost = Test-Path -Path "$GhostDestination\Ghost.zip"
+
+
+If($Ghost -eq $true ){
+Write-Host "Symantec Ghost successfully downloaded" -ForegroundColor Cyan
+Expand-7Zip -ArchiveFileName "$GhostDestination\Ghost.zip" -TargetPath $GhostDestination
+}
+ElseIf($Ghost -eq $false){
+Write-Verbose "Acquiring Symantec Ghost from GitHub repository..." -Verbose
+
+Save-WebFile -SourceUrl $GhostURL -DestinationDirectory $GhostDestination
+Expand-7Zip -ArchiveFileName "$GhostDestination\Ghost.zip" -TargetPath $GhostDestination
+
+Write-Host
+Write-Verbose "Completed: Symantec Ghost has been downloaded..." -Verbose
+Write-Host
+}
 Stop-Transcript
