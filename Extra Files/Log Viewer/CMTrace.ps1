ba87a3540982d5b1359f33c6c599004bd74b509e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-09 10:51:42 -0500

    Update CMTrace.ps1

--- Extra Files/Log Viewer/CMTrace.ps1
+++ Extra Files/Log Viewer/CMTrace.ps1
@@ -28,4 +28,21 @@ Save-WebFile -SourceUrl $CMTraceFile1URL -DestinationDirectory $CMTraceDestinati
 Write-Host
 Write-Verbose "Completed: Download of CM Trace for integration to OSDCloud..." -Verbose
 
+Write-Verbose "Confirming successful download of CM Trace..." -Verbose
+$CMTrace = Test-Path -Path "$CMTraceDestination\CMTrace.exe"
+
+
+If($CMTrace -eq $true ){
+Write-Host "CM Trace successfully downloaded" -ForegroundColor Cyan
+}
+ElseIf($CMTrace -eq $false){
+Write-Verbose "Acquiring CM Trace from GitHub repository..." -Verbose
+
+Save-WebFile -SourceUrl $CMTraceFile1URL -DestinationDirectory $CMTraceDestination
+
+Write-Host
+Write-Verbose "Completed: CM Trace has been downloaded..." -Verbose
+Write-Host
+}
+
 Stop-Transcript
