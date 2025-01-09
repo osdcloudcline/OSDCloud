2098c52eb6d8cb450e6dbe83205fb5510cf8d136
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-09 10:46:18 -0500

    Update Ghosting.ps1

--- Extra Files/Imaging/Ghost/Ghosting.ps1
+++ Extra Files/Imaging/Ghost/Ghosting.ps1
@@ -11,29 +11,35 @@ Import-Module -Name OSD -Force
 $GhostURL1 = "https://github.com/osdcloudcline/OSDCloud/blob/main/Extra%20Files/Imaging/Ghost/Ghost.zip"
 $GhostURL2 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghost64.exe"
 $GhostURL3 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/GhostSrv.exe"
-$GhostURL4 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/GhostSrv.exe"
+$GhostURL4 = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Imaging/Ghost/Ghostexp.exe"
 
 $GhostDestination = "C:\OSDCloud\downloads\GitHub\Ghost"
 
 Write-Verbose "Processing: Symantec Ghost " -Verbose
-Save-WebFile -SourceUrl $GhostURL -DestinationDirectory $GhostDestination
+Save-WebFile -SourceUrl $GhostURL1 -DestinationDirectory $GhostDestination
+Save-WebFile -SourceUrl $GhostURL2 -DestinationDirectory $GhostDestination
+Save-WebFile -SourceUrl $GhostURL3 -DestinationDirectory $GhostDestination
+Save-WebFile -SourceUrl $GhostURL4 -DestinationDirectory $GhostDestination
 Write-Host
 Write-Verbose "Completed: Symantec Ghost file downloads" -Verbose
 Write-Host
 
 Write-Verbose "Confirming successful download of Symantec Ghost..." -Verbose
 $Ghost = Test-Path -Path "$GhostDestination\Ghost.zip"
-
+$Ghost2 = Test-Path -Path "$GhostDestination\Ghost64.exe"
+$Ghost3 = Test-Path -Path "$GhostDestination\GhostSrv.exe"
+$Ghost4 = Test-Path -Path "$GhostDestination\Ghostexp.exe"
 
 If($Ghost -eq $true ){
 Write-Host "Symantec Ghost successfully downloaded" -ForegroundColor Cyan
-Expand-7Zip -ArchiveFileName "$GhostDestination\Ghost.zip" -TargetPath $GhostDestination
+
 }
 ElseIf($Ghost -eq $false){
 Write-Verbose "Acquiring Symantec Ghost from GitHub repository..." -Verbose
 
-Save-WebFile -SourceUrl $GhostURL -DestinationDirectory $GhostDestination
-Expand-7Zip -ArchiveFileName "$GhostDestination\Ghost.zip" -TargetPath $GhostDestination
+Save-WebFile -SourceUrl $GhostURL2 -DestinationDirectory $GhostDestination
+Save-WebFile -SourceUrl $GhostURL3 -DestinationDirectory $GhostDestination
+Save-WebFile -SourceUrl $GhostURL4 -DestinationDirectory $GhostDestination
 
 Write-Host
 Write-Verbose "Completed: Symantec Ghost has been downloaded..." -Verbose
