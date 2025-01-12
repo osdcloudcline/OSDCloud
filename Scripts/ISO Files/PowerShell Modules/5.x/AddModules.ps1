1de7a35cf7436e105460716969b67a78622e3d21
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-01-12 08:27:16 -0500

    Update AddModules.ps1

--- Scripts/ISO Files/PowerShell Modules/5.x/AddModules.ps1
+++ Scripts/ISO Files/PowerShell Modules/5.x/AddModules.ps1
@@ -29,17 +29,6 @@ Edit-OSDCloudWinPE -PSModuleInstall Az.Accounts
 Edit-OSDCloudWinPE -PSModuleInstall Az.Storage
 Edit-OSDCloudWinPE -PSModuleInstall Az.Resources
 Edit-OSDCloudWinPE -PSModuleInstall Az.KeyVault
-Edit-OSDCloudWinPE -PSModuleInstall Az.Compute
-Edit-OSDCloudWinPE -PSModuleInstall Az.Automation
-Edit-OSDCloudWinPE -PSModuleInstall Az.Network
-Edit-OSDCloudWinPE -PSModuleInstall Az.ApiManagement
-Edit-OSDCloudWinPE -PSModuleInstall Az.Advisor
-Edit-OSDCloudWinPE -PSModuleInstall Az.AnalysisServices
-Edit-OSDCloudWinPE -PSModuleInstall Az.Billing
-Edit-OSDCloudWinPE -PSModuleInstall Az.ApplicationInsights
-Edit-OSDCloudWinPE -PSModuleInstall Az.Cdn
-Edit-OSDCloudWinPE -PSModuleInstall Az.Batch
-Edit-OSDCloudWinPE -PSModuleInstall Az.Monitor
 Edit-OSDCloudWinPE -PSModuleInstall Microsoft.Graph
 
 Write-Host
