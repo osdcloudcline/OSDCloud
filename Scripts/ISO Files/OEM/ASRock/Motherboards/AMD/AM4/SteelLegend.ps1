6a5a608857147841c6c8853865ef63b0f0e77f96
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-08 23:53:21 -0400

    Update SteelLegend.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/SteelLegend.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/SteelLegend.ps1
@@ -196,7 +196,7 @@ Get-OSDCloudWorkspace
 
 Write-Host
 Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
-$WorkspacePath = Read-Host -Prompt 'Please enter custom path for new OSDCloud Workspace'
+$WorkspacePath = "C:\OSDCloud\ASRock-SteelLegendMotherboards-AM4"
 New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 
@@ -599,5 +599,11 @@ Edit-OSDCloudWinPE -WebPSScript https://raw.githubusercontent.com/osdcloudcline/
 Write-Host
 
 ###########################################
-# Create OSDCloud ISO
-############################################
+# Rename OSDCloud ISO
+#########################################
+
+$OSDCloudISOPath = "C:\OSDCloud\ASRock-SteelLegendMotherboards-AM4"
+
+Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ASRock-SteelLegendMotherboards-AM4\OSDCloud-SteelLegendMotherboards-AM4.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ASRock-SteelLegendMotherboards-AM4\OSDCloud-SteelLegendMotherboards-AM4_NoPrompt.iso" -Force
