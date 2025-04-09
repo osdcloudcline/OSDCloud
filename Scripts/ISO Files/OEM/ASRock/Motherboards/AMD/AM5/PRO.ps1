bc1fcaa6faf570cb34a3b74527a468eecfe9595b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 00:00:01 -0400

    Update PRO.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM5/PRO.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM5/PRO.ps1
@@ -192,7 +192,7 @@ Get-OSDCloudWorkspace
 
 Write-Host
 Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
-$WorkspacePath = Read-Host -Prompt 'Please enter custom path for new OSDCloud Workspace'
+$WorkspacePath = "C:\OSDCloud\ASRock-PROMotherboards-AM5"
 New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 
@@ -569,5 +569,11 @@ Edit-OSDCloudWinPE -WebPSScript https://raw.githubusercontent.com/osdcloudcline/
 Write-Host
 
 ###########################################
-# Create OSDCloud ISO
-############################################
+# Rename OSDCloud ISO
+#########################################
+
+$OSDCloudISOPath = "C:\OSDCloud\ASRock-PROMotherboards-AM5"
+
+Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ASRock-PROMotherboardsk-AM5\OSDCloud-ASRock-PROMotherboards-AM5.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ASRock-PROMotherboardsk-AM5\OSDCloud-ASRock-PROMotherboards-AM5_NoPrompt.iso" -Force
