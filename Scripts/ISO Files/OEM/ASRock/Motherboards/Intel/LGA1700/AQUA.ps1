b7bd7dc62d7e2d023cc450370cae606ab49be6db
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 15:19:09 -0400

    Update AQUA.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/AQUA.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/AQUA.ps1
@@ -107,6 +107,28 @@ Get-OSDCloudWorkspace
 $AddPS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/AddPS7-OSDCloudISO.ps1")
 Invoke-Expression $($AddPS7.Content)
 
+############################################
+# ASRock LGA 1700 AQUA motherboards
+############################################
+
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 AQUA Motherboard drivers..." -Verbose
+Write-Host
+
+$AQUABluetoothPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Bluetooth"
+$AQUAEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Ethernet1"
+$AQUAEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Ethernet2"
+$AQUAStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Storage"
+$AQUAWLANPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\WLAN1"
+$AQUAWLANPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\WLAN2"
+
+Edit-OSDCloudWinPe -DriverPath $AQUABluetoothPath
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath1
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath2
+Edit-OSDCloudWinPe -DriverPath $AQUAStoragePath
+Edit-OSDCloudWinPe -DriverPath $AQUAWLANPath1
+Edit-OSDCloudWinPe -DriverPath $AQUAWLANPath2
+
 #################################
 # OSDCloud - Cloud Drivers
 #################################
@@ -347,13 +369,6 @@ Invoke-Expression $($OSDCloudPS5xMods.Content)
 
 $OSDCloudPS7xMods = Invoke-WebRequest("https://raw.githubusercontent.com/osdcloudcline/OSDCloud/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/7.x/AddModules.ps1")
 Invoke-Expression $($OSDCloudPS7xMods.Content)
-
-
-############################################
-# ASRock LGA 1700 AQUA motherboards
-############################################
-
-
 
 
 ############################################
