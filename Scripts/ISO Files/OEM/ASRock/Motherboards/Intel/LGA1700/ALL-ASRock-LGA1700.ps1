500e370237955feb191c6ed9ac626aec51ebe85a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 23:20:06 -0400

    Update ALL-ASRock-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
@@ -155,6 +155,31 @@ Write-Host
 
 # Extreme motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 Extreme Motherboard drivers..." -Verbose
+Write-Host
+
+$ExtremeEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\Ethernet1"
+$ExtremeEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\Ethernet2"
+$ExtremeEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\Ethernet3"
+$ExtremeEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\Ethernet4"
+$ExtremeEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\Ethernet5"
+$ExtremeStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\Storage"
+$ExtremeWLANPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Extreme\WLAN1"
+
+
+Edit-OSDCloudWinPe -DriverPath $ExtremeEthernetPath1
+Edit-OSDCloudWinPe -DriverPath $ExtremeEthernetPath2
+Edit-OSDCloudWinPe -DriverPath $ExtremeEthernetPath3
+Edit-OSDCloudWinPe -DriverPath $ExtremeEthernetPath4
+Edit-OSDCloudWinPe -DriverPath $ExtremeEthernetPath5
+Edit-OSDCloudWinPe -DriverPath $ExtremeStoragePath
+Edit-OSDCloudWinPe -DriverPath $ExtremeWLANPath1
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA 1700 Extreme Motherboard drivers..." -Verbose
+Write-Host
+
 # PRO motherboards
 
 # Phantom Gaming motherboards
