c83c55e1478ca3fb647abfc0163e850f6b957f0f
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-10 13:27:54 -0400

    Update ALL-ASRock-AM4.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/ALL-ASRock-AM4.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/ALL-ASRock-AM4.ps1
@@ -638,7 +638,18 @@ Write-Host
 Write-Verbose "Completed: Integration of Google Chrome Portable Browser for OSDCloud..." -Verbose
 Write-Host
 
-# MS DaRT Remote Connections and Troubleshooting
+# MSTSC
+
+Write-Host
+Write-Verbose "Processing: Microsoft Remote Desktop for OSDCloud..." -Verbose 
+$MSTSCPath1 = "C:\OSDCloud\downloads\GitHub\MSTSC\Sys32Files"
+$MSTSCPath2 = "C:\OSDCloud\downloads\GitHub\MSTSC\Sys32Files\en-us"
+$MSTSCDestination1 = "$mountdir\Windows\System32"
+$MSTSCDestination2 = "$mountdir\Windows\System32\en-US"
+
+Copy-Item -Path "$MSTSCPath1\*" -Destination $MSTSCDestination1 -Recurse  -Force
+Copy-Item -Path "$MSTSCPath2\*" -Destination $MSTSCDestination2  -Recurse -Force
+
 
 # CloudPC
 
@@ -649,13 +660,13 @@ $CloudPCPath2 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files\en-us"
 $CloudPCDestination1 = "$mountdir\Windows\System32"
 $CloudPCDestination2 = "$mountdir\Windows\System32\en-US"
 
-Copy-Item -Path "$CloudPCPath1\*" -Destination $CloudPCDestination1  -Force
-Copy-Item -Path "$CloudPCPath2\*" -Destination $CloudPCDestination2  -Force
+Copy-Item -Path "$CloudPCPath1\*" -Destination $CloudPCDestination1  -Recurse -Force
+Copy-Item -Path "$CloudPCPath2\*" -Destination $CloudPCDestination2  -Recurse -Force
 
 # CMTrace 
 
 Write-Verbose "Processing: Microsoft Endpoint Configuration Manager Log Viewer CM Trace for OSDCloud..." -Verbose 
-$CMTracePath = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer"
+$CMTracePath = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer\CMTrace.exe"
 $CMTraceDestination = "$mountdir\Windows\System32"
 
 Copy-Item -Path $CMTracePath -Destination $CMTraceDestination -Force
