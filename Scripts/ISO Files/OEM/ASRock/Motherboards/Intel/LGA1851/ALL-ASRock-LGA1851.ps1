73ecb30f2c7d247b44c6d6447f85dfb4e0fdda93
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-10 08:11:37 -0400

    Update ALL-ASRock-LGA1851.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/ALL-ASRock-LGA1851.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/ALL-ASRock-LGA1851.ps1
@@ -208,6 +208,27 @@ Write-Host
 
 # Steel Legend motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA1851 Steel Legend Motherboard drivers..." -Verbose
+Write-Host
+
+$SteelLegendBluetoothPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\SteelLegend\Bluetooth"
+
+$SteelLegendEthernetPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\SteelLegend\Ethernet"
+
+$SteelLegendStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\SteelLegend\Storage"
+
+$SteelLegendWLANPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\SteelLegend\WLAN"
+
+Edit-OSDCloudWinPe -DriverPath $SteelLegendBluetoothPath
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath
+Edit-OSDCloudWinPe -DriverPath $SteelLegendStoragePath
+Edit-OSDCloudWinPe -DriverPath $SteelLegendWLANPath
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA1851 Steel Legend Motherboard drivers..." -Verbose
+Write-Host
+
 # Taichi motherboards
 
 #################################
