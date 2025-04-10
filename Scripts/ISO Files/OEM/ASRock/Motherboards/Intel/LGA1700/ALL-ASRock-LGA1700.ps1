4d2ad10d9148ad6582d62cbfe13755c2905fbd78
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 23:21:48 -0400

    Update ALL-ASRock-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
@@ -180,6 +180,22 @@ Write-Host
 Write-Verbose "Completed: ASRock Intel LGA 1700 Extreme Motherboard drivers..." -Verbose
 Write-Host
 
+# Live Mixer motherboards
+
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 Live Mixer Motherboard drivers..." -Verbose
+Write-Host
+
+$LiveMixerEthernetPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\LiveMixer\Ethernet"
+$LiveMixerStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\LiveMixer\Storage"
+
+Edit-OSDCloudWinPe -DriverPath $LiveMixerEthernetPath
+Edit-OSDCloudWinPe -DriverPath $LiveMixerStoragePath
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA 1700 Live Mixer Motherboard drivers..." -Verbose
+Write-Host
+
 # PRO motherboards
 
 # Phantom Gaming motherboards
