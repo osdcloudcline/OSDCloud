6bfdcabedbeaec501edccea8d4029bb1383c8625
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-10 00:03:19 -0400

    Update ALL-ASRock-AM5.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM5/ALL-ASRock-AM5.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM5/ALL-ASRock-AM5.ps1
@@ -253,6 +253,26 @@ Write-Host
 
 # Taichi motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock AMD AM5 Taichi Motherboard drivers..." -Verbose
+Write-Host
+
+$TaichiBluetoothPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Bluetooth"
+$TaichiBluetoothPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Bluetooth"
+$TaichiEthernetPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Ethernet"
+$TaichiStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\Storage"
+$TaichiWLANPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM5\Taichi\WLAN"
+
+Edit-OSDCloudWinPe -DriverPath $TaichiBluetoothPath1
+Edit-OSDCloudWinPe -DriverPath $TaichiBluetoothPath2
+Edit-OSDCloudWinPe -DriverPath $TaichiEthernetPath
+Edit-OSDCloudWinPe -DriverPath $TaichiStoragePath
+Edit-OSDCloudWinPe -DriverPath $TaichiWLANPath
+
+Write-Host
+Write-Verbose "Completed: ASRock AMD AM5 Taichi Motherboard drivers..." -Verbose
+Write-Host
+
 #################################
 # OSDCloud - Cloud Drivers
 #################################
