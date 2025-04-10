4be19b22e24842f5e7ae5b4a9d92ba4092480ceb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 23:25:48 -0400

    Update ALL-ASRock-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
@@ -282,6 +282,24 @@ Write-Host
 
 # Taichi motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 Taichi Motherboard drivers..." -Verbose
+Write-Host
+
+$TaichiBluetoothPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Taichi\Bluetooth"
+$TaichiEthernetPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Taichi\Ethernet"
+$TaichiStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Taichi\Storage"
+$TaichiWLANPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\Taichi\WLAN"
+
+
+Edit-OSDCloudWinPe -DriverPath $TaichiBluetoothPath
+Edit-OSDCloudWinPe -DriverPath $TaichiEthernetPath
+Edit-OSDCloudWinPe -DriverPath $TaichiStoragePath
+Edit-OSDCloudWinPe -DriverPath $TaichiWLANPath
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA 1700 Taichi Motherboard drivers..." -Verbose
+Write-Host
 
 #################################
 # OSDCloud - Cloud Drivers
