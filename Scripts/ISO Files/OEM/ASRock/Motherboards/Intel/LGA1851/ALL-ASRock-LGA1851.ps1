ba55d5b926944c7d53987c49797db7c006eab683
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-10 07:53:26 -0400

    Update ALL-ASRock-LGA1851.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/ALL-ASRock-LGA1851.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/ALL-ASRock-LGA1851.ps1
@@ -125,6 +125,39 @@ Invoke-Expression $($AddPS7.Content)
 
 # AQUA motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA1851 AQUA Motherboard drivers..." -Verbose
+Write-Host
+
+$AQUABluetoothPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Bluetooth"
+
+$AQUAEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet1"
+$AQUAEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet2"
+$AQUAEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet3"
+$AQUAEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet4"
+$AQUAEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet5"
+$AQUAEthernetPath6 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet6"
+$AQUAEthernetPath7 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet7"
+
+$AQUAStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Storage"
+
+$AQUAWLANPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\WLAN"
+
+Edit-OSDCloudWinPe -DriverPath $AQUABluetoothPath
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath1
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath2
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath3
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath4
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath5
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath6
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath7
+Edit-OSDCloudWinPe -DriverPath $AQUAStoragePath
+Edit-OSDCloudWinPe -DriverPath $AQUAWLANPath
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA1851 AQUA Motherboard drivers..." -Verbose
+Write-Host
+
 # PRO motherboards
 
 # Live Mixer motherboards
