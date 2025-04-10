83725713fdc26470dcbecdeda8be53052afe2c8b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 23:24:16 -0400

    Update ALL-ASRock-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
@@ -224,6 +224,34 @@ Write-Host
 
 # Phantom Gaming motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 Phantom Gaming Motherboard drivers..." -Verbose
+Write-Host
+
+$PhantomGamingBluetoothPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Bluetooth"
+$PhantomGamingEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Ethernet1"
+$PhantomGamingEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Ethernet2"
+$PhantomGamingEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Ethernet3"
+$PhantomGamingEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Ethernet4"
+$PhantomGamingEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Ethernet5"
+$PhantomGamingStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\Storage"
+$PhantomGamingWLANPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\WLAN1"
+$PhantomGamingWLANPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PhantomGaming\WLAN2"
+
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingBluetoothPath
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingEthernetPath1
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingEthernetPath2
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingEthernetPath3
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingEthernetPath4
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingEthernetPath5
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingStoragePath
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingWLANPath1
+Edit-OSDCloudWinPe -DriverPath $PhantomGamingWLANPath2
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA 1700 Phantom Gaming Motherboard drivers..." -Verbose
+Write-Host
+
 # Steel Legend motherboards
 
 # Taichi motherboards
