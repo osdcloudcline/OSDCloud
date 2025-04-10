092d52e377cf09595f20b37e79b281a47aa37d41
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 23:25:09 -0400

    Update ALL-ASRock-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
@@ -254,6 +254,32 @@ Write-Host
 
 # Steel Legend motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 Steel Legend Motherboard drivers..." -Verbose
+Write-Host
+
+$SteelLegendEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet1"
+$SteelLegendEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet2"
+$SteelLegendEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet3"
+$SteelLegendEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet4"
+$SteelLegendEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet5"
+$SteelLegendEthernetPath6 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet6"
+$SteelLegendStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Storage"
+$SteelLegendWLANPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\WLAN1"
+
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath1
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath2
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath3
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath4
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath5
+Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath6
+Edit-OSDCloudWinPe -DriverPath $SteelLegendStoragePath
+Edit-OSDCloudWinPe -DriverPath $SteelLegendWLANPath1
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA 1700 Steel Legend Motherboard drivers..." -Verbose
+Write-Host
+
 # Taichi motherboards
 
 
