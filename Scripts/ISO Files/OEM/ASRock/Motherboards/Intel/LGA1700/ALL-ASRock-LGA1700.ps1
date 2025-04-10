51df37978fe51504583748e483d06606e89c2fae
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-09 23:18:53 -0400

    Update ALL-ASRock-LGA1700.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/ALL-ASRock-LGA1700.ps1
@@ -131,6 +131,28 @@ Invoke-Expression $($AddPS7.Content)
 
 # AQUA motherboards
 
+Write-Host
+Write-Verbose "Processing: ASRock Intel LGA 1700 AQUA Motherboard drivers..." -Verbose
+Write-Host
+
+$AQUABluetoothPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Bluetooth"
+$AQUAEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Ethernet1"
+$AQUAEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Ethernet2"
+$AQUAStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\Storage"
+$AQUAWLANPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\WLAN1"
+$AQUAWLANPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\AQUA\WLAN2"
+
+Edit-OSDCloudWinPe -DriverPath $AQUABluetoothPath
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath1
+Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath2
+Edit-OSDCloudWinPe -DriverPath $AQUAStoragePath
+Edit-OSDCloudWinPe -DriverPath $AQUAWLANPath1
+Edit-OSDCloudWinPe -DriverPath $AQUAWLANPath2
+
+Write-Host
+Write-Verbose "Completed: ASRock Intel LGA 1700 AQUA Motherboard drivers..." -Verbose
+Write-Host
+
 # Extreme motherboards
 
 # PRO motherboards
