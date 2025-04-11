5b6b4bf5bf7b35fa10bf69dc0baccfbd82971c0d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-11 00:07:46 -0400

    Update HWInfo.ps1

--- Extra Files/System Information/HWInfo.ps1
+++ Extra Files/System Information/HWInfo.ps1
@@ -11,23 +11,25 @@ Get-Date
 $HWInfoFile1URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/System%20Information/HWiNFO.zip"
 
 Import-Module -Name OSD -Force
-
+Import-Module -Name 7Zip4Powershell -Force
 
 ####################################################################
 # HWInfo DL File Destination ##############
 ####################################################################
 
 $HWInfoDestination = "C:\OSDCloud\downloads\GitHub\HWInfo"
+$HWInfoZIPDestination = "C:\OSDCloud\downloads\GitHub\HWInfo\Extract"
 
 Write-Verbose "Processing: Acquiring HWiNFO Portable for integration to OSDCloud..." -Verbose
 Write-Host
 Save-WebFile -SourceUrl $HWInfoFile1URL -DestinationDirectory $HWInfoDestination
+Expand-7Zip -ArchiveFileName "$HWInfoDestination\HWiNFO.zip" -TargetPath $HWInfoZIPDestination -ErrorAction SilentlyContinue 
 
 Write-Host
 Write-Verbose "Completed: Download of HWiNFO Portable for integration to OSDCloud..." -Verbose
 
 Write-Verbose "Confirming successful download of HWiNFO Portable..." -Verbose
-$HWInfo = Test-Path -Path "$HWInfoDestination\HWiNFOPortable.exe"
+$HWInfo = Test-Path -Path "$HWInfoZIPDestination\HWiNFOPortable.exe" 
 
 
 If($HWInfo -eq $true ){
