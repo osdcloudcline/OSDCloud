5e30a8c0c830d7609ce366b10bb531409e258331
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-10 23:58:02 -0400

    Update HWInfo.ps1

--- Extra Files/System Information/HWInfo.ps1
+++ Extra Files/System Information/HWInfo.ps1
@@ -1,1 +1,20 @@
+$HWInfoLog = "C:\Logs\OSDCloud\System Information\HWInfo.log"
 
+Start-Transcript -Path $HWInfoLog
+
+Get-Date
+
+############################################
+# HWInfo  File URLs
+############################################
+
+$HWInfoFile1URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/File%20Explorer/Explorer++.exe"
+
+Import-Module -Name OSD -Force
+
+
+####################################################################
+# HWInfo DL File Destination ##############
+####################################################################
+
+$HWInfoDestination = "C:\OSDCloud\downloads\GitHub\HWInfo"
