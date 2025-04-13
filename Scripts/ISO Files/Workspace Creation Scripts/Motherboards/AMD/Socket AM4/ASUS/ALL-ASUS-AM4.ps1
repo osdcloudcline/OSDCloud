69717961c0d855d9dec7821c090c841e8833537b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 14:36:23 -0400

    Create ALL-ASUS-AM4.ps1

---
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASUS/ALL-ASUS-AM4.ps1
@@ -0,0 +1,29 @@
+################################################
+# OSDCloud Template and Workspace configuration
+################################################
+
+Write-Host
+Write-Verbose "Confirming OSDCloud Templates......" -Verbose
+Get-OSDCloudTemplate
+
+Write-Host
+Write-Verbose "Creating New OSDCloud WinRE Template specific for ALL ASUS AM4 motherboards to enable wireless networking support..." -Verbose
+New-OSDCloudTemplate -Name ASUS-AM4 -WinRE
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = "C:\OSDCloud\ALL-ASUS-AM4"
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
