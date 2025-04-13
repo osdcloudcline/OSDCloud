94cd0e49b7b24eb023b2b7739857cb8b56795e14
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 15:37:50 -0400

    Update CSM.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/CSM.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/CSM.ps1
@@ -7,7 +7,7 @@ Write-Verbose "Confirming OSDCloud Templates......" -Verbose
 Get-OSDCloudTemplate
 
 Write-Host
-Write-Verbose "Creating New OSDCloud WinRE Template specific for ASUS CSM AM4 motherboards to enable wireless networking support..." -Verbose
+Write-Verbose "Creating New OSDCloud WinRE Template specific for ASUS CSM AM5 motherboards to enable wireless networking support..." -Verbose
 New-OSDCloudTemplate -Name ASUS-CSM-AM5 -WinRE
 
 Write-Host
