acce0662ffdb2ae117f6010d760584d5fa3f9143
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 12:10:57 -0400

    Update CommonDownloads.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Virtualization/CommonDownloads.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Virtualization/CommonDownloads.ps1
@@ -1,1 +1,12 @@
+Write-Host
+Write-Verbose "Processing: PowerShell 7.x support downloads" -Verbose
+Write-Host 
+
+$PS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/PS7Download.ps1")
+Invoke-Expression $($PS7.Content)
+
+Write-Host
+Write-Verbose "Completed: PowerShell 7.x support downloads" -Verbose
+Write-Host
+
 
