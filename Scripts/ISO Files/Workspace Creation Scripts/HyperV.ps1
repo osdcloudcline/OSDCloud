1e29e5a743edb6e70514996b47d088c38ad41177
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 11:52:38 -0400

    Update HyperV.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/HyperV.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/HyperV.ps1
@@ -16,7 +16,7 @@ Get-OSDCloudWorkspace
 
 Write-Host
 Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
-$WorkspacePath = Read-Host -Prompt 'Please enter custom path for new OSDCloud Workspace'
+$WorkspacePath = "C:\OSDCloud\Hyper-V"
 New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 
