021e4b5dd3ecd5a7087f27a2bd04decfa6a73ecf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-12 21:31:17 -0400

    Update DaRT.ps1

--- Extra Files/DaRT/DaRT.ps1
+++ Extra Files/DaRT/DaRT.ps1
@@ -8,8 +8,8 @@ Get-Date
 ## DART File URL Paths
 ##############################################################
 
-$DART64CAB_URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/DaRT/Toolsx64.cab"
-$DARTConfig8_URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/DaRT/DartConfig8.dat"
+$DART64CABURL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/DaRT/Toolsx64.cab"
+$DARTConfig8URL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/DaRT/DartConfig8.dat"
 
 ################################################################
 ## DART File Destination
@@ -25,8 +25,8 @@ Write-Host
 Write-Verbose "Processing: Acquiring Microsoft DART files..." -Verbose
 Write-Host
 
-Save-WebFile -SourceUrl $DART64CAB_URL -DestinationDirectory $DARTDestination
-Save-WebFile -SourceUrl $DARTConfig8_URL -DestinationDirectory $DARTDestination
+Save-WebFile -SourceUrl $DART64CABURL -DestinationDirectory $DARTDestination
+Save-WebFile -SourceUrl $DARTConfig8URL -DestinationDirectory $DARTDestination
 
 Write-Host
 Write-Verbose "Completed: Microsoft DART files have been downloaded..." -Verbose
@@ -36,14 +36,20 @@ Write-Verbose "Confirming successful download of Microsoft Diagnostics and Recov
 $DARTTP1 = Test-Path -Path "$DARTDestination\Toolsx64.cab"
 $DARTTP2 = Test-Path -Path "$DARTDestination\DartConfig8.dat"
 
-If($DARTTP1 -and $DARTTP2 -eq $true ){
+If(($DARTTP1 -eq $true) -and ($DARTTP2 -eq $true)){
 Write-Host "Microsoft Diagnostics and Recovery Tools successfully downloaded" -ForegroundColor Cyan
+
+Copy-Item -Path "$DARTDestination\Toolsx64.cab" -Destination $DART64CABDestination -Force
+Copy-Item -Path "$DARTDestination\DartConfig8.dat" -Destination $DARTConfig8Destination -Force
 }
-ElseIf($DARTTP1 -and $DARTTP2 -eq $false){
+ElseIf(($DARTTP1 -eq $false) -and ($DARTTP2 -eq $false)){
 Write-Verbose "Acquiring Microsoft Diagnostics and Recovery Tools from GitHub repository..." -Verbose
 
-Save-WebFile -SourceUrl $DART64CAB_URL -DestinationDirectory $DARTDestination
-Save-WebFile -SourceUrl $DARTConfig8_URL -DestinationDirectory $DARTDestination
+Save-WebFile -SourceUrl $DART64CABURL -DestinationDirectory $DARTDestination
+Save-WebFile -SourceUrl $DARTConfig8URL -DestinationDirectory $DARTDestination
+
+Copy-Item -Path "$DARTDestination\Toolsx64.cab" -Destination $DART64CABDestination -Force
+Copy-Item -Path "$DARTDestination\DartConfig8.dat" -Destination $DARTConfig8Destination -Force
 
 Write-Host
 Write-Verbose "Completed: Microsoft DART files have been downloaded..." -Verbose
