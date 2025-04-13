b084675b4e2ade9ff36182fb8c7627a2b60dfebe
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 11:56:40 -0400

    Create HyperV.ps1

---
+++ Scripts/ISO Files/Workspace Creation Scripts/Virtualization/HyperV.ps1
@@ -0,0 +1,25 @@
+################################################
+# OSDCloud Template and Workspace configuration
+################################################
+
+Write-Host
+Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
+New-OSDCloudTemplate -Name HyperV-Virtual -WinRE
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = "C:\OSDCloud\Hyper-V"
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
