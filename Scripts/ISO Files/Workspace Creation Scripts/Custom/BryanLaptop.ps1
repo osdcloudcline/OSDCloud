44a7b2ef54d5233ac2acb050a3602c6ac143b290
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 13:58:05 -0400

    Create BryanLaptop.ps1

---
+++ Scripts/ISO Files/Workspace Creation Scripts/Custom/BryanLaptop.ps1
@@ -0,0 +1,24 @@
+################################################
+# OSDCloud Template and Workspace configuration
+################################################
+
+Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support for Bryan's laptop..." -Verbose
+New-OSDCloudTemplate -Name OSDCloud-BryanLaptops -WinRE
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = "C:\OSDCloud\BryanLaptop"
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
