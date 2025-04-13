c28a236cd9cccf2ef26059df922ac4fbd4a4401c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 11:16:56 -0400

    Update ALL-VT.ps1

--- Scripts/ISO Files/Virtualization/ALL-VT.ps1
+++ Scripts/ISO Files/Virtualization/ALL-VT.ps1
@@ -261,6 +261,78 @@ Write-Host
 Write-Verbose "Completed: OSDCloud wallpaper download" -Verbose
 Write-Host
 
+########################################
+# 3rd party software and utilities
+########################################
+
+Mount-WindowsImage -ImagePath "$WimFile\boot.wim" -Path $mountdir -Index 1
+
+# Portable Browsers
+
+Write-Host
+Write-Verbose "Processing: Google Chrome Portable Browser for OSDCloud..." -Verbose 
+$ChromePath = "C:\OSDCloud\GitHub\downloads\Chrome\Chrome.exe"
+$ChromeDestination = "$mountdir\Windows\System32"
+
+Copy-Item -Path $ChromePath -Destination $ChromeDestination -Force
+
+Write-Host
+Write-Verbose "Completed: Integration of Google Chrome Portable Browser for OSDCloud..." -Verbose
+Write-Host
+
+# MS DaRT Remote Connections and Troubleshooting
+
+# CloudPC
+
+Write-Host
+Write-Verbose "Processing: Microsoft Remote Desktop - Cloud PC for OSDCloud..." -Verbose 
+$CloudPCPath1 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files"
+$CloudPCPath2 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files\en-us"
+$CloudPCDestination1 = "$mountdir\Windows\System32"
+$CloudPCDestination2 = "$mountdir\Windows\System32\en-US"
+
+Copy-Item -Path "$CloudPCPath1\*" -Destination $CloudPCDestination1  -Force
+Copy-Item -Path "$CloudPCPath2\*" -Destination $CloudPCDestination2  -Force
+
+# CMTrace 
+
+Write-Verbose "Processing: Microsoft Endpoint Configuration Manager Log Viewer CM Trace for OSDCloud..." -Verbose 
+$CMTracePath = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer"
+$CMTraceDestination = "$mountdir\Windows\System32"
+
+Copy-Item -Path $CMTracePath -Destination $CMTraceDestination -Force
+
+# Ghost Imaging
+Write-Host
+Write-Verbose "Processing: Ghost Imaging for OSDCloud..." -Verbose 
+$GhostPath = "C:\OSDCloud\downloads\GitHub\Ghost"
+$GhostDestination = "$mountdir\Windows\System32"
+
+Copy-Item -Path "$Ghost64Path\*" -Recurse -Destination $GhostDestination -Force
+
+Write-Host
+Write-Verbose "Completed: Integration of Ghost Imaging for OSDCloud..." -Verbose
+Write-Host
+
+# User Profile Backup and Restore 
+
+Write-Host
+Write-Verbose "Processing: User Profile Backup/Restore for OSDCloud..." -Verbose 
+$UPBRFilePath = "C:\OSDCloud\GitHub\downloads\UPBR.exe"
+$UBPRDestination = "$mountdir\Windows\System32"
+
+Copy-Item -Path $UPBRFilePath -Destination $UBPRDestination -Force
+
+Write-Host
+Write-Verbose "Completed: Integration of User Profile Backup/Restore for OSDCloud..." -Verbose
+Write-Host
+
+# Disount the image
+Write-Host
+Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
+
+Dismount-WindowsImage -Path $mountdir -Save
+
 #################################
 # OSDCloud - Cloud Drivers
 #################################
@@ -339,9 +411,9 @@ Invoke-Expression $($USBCloudDrivers.Content)
 $USBCloudDriverPath1 = "C:\Drivers\Cloud Drivers\USB\Extract\1"
 $USBCloudDriverPath2 = "C:\Drivers\Cloud Drivers\USB\Extract\2"
 $USBCloudDriverPath3 = "C:\Drivers\Cloud Drivers\USB\Extract\3"
-$USBCloudDriverPath1 = "C:\Drivers\Cloud Drivers\USB\Extract\4"
-$USBCloudDriverPath2 = "C:\Drivers\Cloud Drivers\USB\Extract\5"
-$USBCloudDriverPath3 = "C:\Drivers\Cloud Drivers\USB\Extract\6"
+$USBCloudDriverPath4 = "C:\Drivers\Cloud Drivers\USB\Extract\4"
+$USBCloudDriverPath5 = "C:\Drivers\Cloud Drivers\USB\Extract\5"
+$USBCloudDriverPath6 = "C:\Drivers\Cloud Drivers\USB\Extract\6"
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud USB Cloud Drivers..." --Verbose
@@ -503,75 +575,7 @@ Write-Host
 Write-Verbose "Completed: Integration of VBS Scripting Support for OSDCloud..." -Verbose
 Write-Host
 
-########################################
-# 3rd party software and utilities
-########################################
 
-# Portable Browsers
-
-Write-Host
-Write-Verbose "Processing: Google Chrome Portable Browser for OSDCloud..." -Verbose 
-$ChromePath = "C:\OSDCloud\GitHub\downloads\Chrome\Chrome.exe"
-$ChromeDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $ChromePath -Destination $ChromeDestination -Force
-
-Write-Host
-Write-Verbose "Completed: Integration of Google Chrome Portable Browser for OSDCloud..." -Verbose
-Write-Host
-
-# MS DaRT Remote Connections and Troubleshooting
-
-# CloudPC
-
-Write-Host
-Write-Verbose "Processing: Microsoft Remote Desktop - Cloud PC for OSDCloud..." -Verbose 
-$CloudPCPath1 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files"
-$CloudPCPath2 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files\en-us"
-$CloudPCDestination1 = "$mountdir\Windows\System32"
-$CloudPCDestination2 = "$mountdir\Windows\System32\en-US"
-
-Copy-Item -Path "$CloudPCPath1\*" -Destination $CloudPCDestination1  -Force
-Copy-Item -Path "$CloudPCPath2\*" -Destination $CloudPCDestination2  -Force
-
-# CMTrace 
-
-Write-Verbose "Processing: Microsoft Endpoint Configuration Manager Log Viewer CM Trace for OSDCloud..." -Verbose 
-$CMTracePath = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer"
-$CMTraceDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $CMTracePath -Destination $CMTraceDestination -Force
-
-# Ghost Imaging
-Write-Host
-Write-Verbose "Processing: Ghost Imaging for OSDCloud..." -Verbose 
-$GhostPath = "C:\OSDCloud\downloads\GitHub\Ghost"
-$GhostDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path "$Ghost64Path\*" -Recurse -Destination $GhostDestination -Force
-
-Write-Host
-Write-Verbose "Completed: Integration of Ghost Imaging for OSDCloud..." -Verbose
-Write-Host
-
-# User Profile Backup and Restore 
-
-Write-Host
-Write-Verbose "Processing: User Profile Backup/Restore for OSDCloud..." -Verbose 
-$UPBRFilePath = "C:\OSDCloud\GitHub\downloads\UPBR.exe"
-$UBPRDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $UPBRFilePath -Destination $UBPRDestination -Force
-
-Write-Host
-Write-Verbose "Completed: Integration of User Profile Backup/Restore for OSDCloud..." -Verbose
-Write-Host
-
-# Disount the image
-Write-Host
-Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
-
-Dismount-WindowsImage -Path $mountdir -Save
 
 ######################################
 ## Extra PowerShell Modules - OSDCloud
