a12b192f9ce19928d7ff7c9e31046a9b9fc272dd
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 00:31:39 -0400

    Update ALL-VT.ps1

--- Scripts/ISO Files/Virtualization/ALL-VT.ps1
+++ Scripts/ISO Files/Virtualization/ALL-VT.ps1
@@ -27,10 +27,6 @@ Install-Module -Name 7Zip4Powershell -Force -AllowClobber -SkipPublisherCheck -V
 Import-Module -Name 7Zip4Powershell -Force
 
 Write-Host
-Write-Verbose "Processing: Beginning file downloads" -Verbose
-Write-Host 
-
-Write-Host
 Write-Verbose "Processing: PowerShell 7.x support downloads" -Verbose
 Write-Host 
 
@@ -40,6 +36,113 @@ Invoke-Expression $($PS7.Content)
 Write-Host
 Write-Verbose "Completed: PowerShell 7.x support downloads" -Verbose
 Write-Host
+
+################################################
+# OSDCloud Template and Workspace configuration
+################################################
+
+Write-Host
+Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
+New-OSDCloudTemplate -Name ALL-Virtual -WinRE
+
+Write-Host
+Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
+Get-OSDCloudTemplateNames
+
+Write-Host
+Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
+Get-OSDCloudWorkspace
+
+Write-Host
+Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
+$WorkspacePath = "C:\OSDCloud\All-VT"
+New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
+
+Write-Host
+Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
+Get-OSDCloudWorkspace
+
+#################################
+# Adding PowerShell 7 to OSDCloud 
+#################################
+
+$PS7ToOSDCloudISOLog = "C:\Logs\OSDCloud\PowerShell 7 Support\AddToISO.log"
+
+Start-Transcript -Path $PS7ToOSDCloudISOLog
+
+# Settings and File Paths
+$PS7Source = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.zip"
+$PS7Destination = "C:\OSDCloud\GitHub\downloads\PowerShell\7"
+$PS7Extract = "C:\downloads\OSDCloud\PS7"
+$mountdir = "C:\Mount"
+
+
+Write-Verbose "Processing: Creating mount directory" -Verbose
+# Create Mount Directory
+New-Item -Path $mountdir -ItemType Directory -Force
+
+
+Write-Verbose "Processing: Mounting OSDCloud boot.wim" -Verbose
+# Mount the image
+$sourceWIMDir = "\Media\sources"
+$WorkspacePath = Get-OSDCloudWorkspace
+$WimFile = Join-Path -Path $WorkspacePath -ChildPath $sourceWIMDir
+Mount-WindowsImage -ImagePath "$WimFile\boot.wim" -Path $mountdir -Index 1
+
+Write-Verbose "Processing: Expanding Windows PowerShell 7 ZIP File" -Verbose
+Expand-Archive -Path "$PS7Destination\PowerShell-7.5.0-win-x64.zip" -DestinationPath "$mountdir\Program Files\PowerShell\7" -Force
+
+Write-Verbose "Processing: Updating environment PATHS" -Verbose
+# Update the offline environment PATH for PowerShell 7
+$HivePath = "$mountdir\Windows\System32\config\SYSTEM"
+reg load "HKLM\OfflineWinPE" $HivePath 
+Start-Sleep -Seconds 5
+
+Write-Verbose "Processing: Adding PowerShell 7 Paths to PATH and PSModulePath" -Verbose
+# Add PowerShell 7 Paths to Path and PSModulePath
+$RegistryKey = "HKLM:\OfflineWinPE\ControlSet001\Control\Session Manager\Environment"
+$CurrentPath = (Get-Item -path $RegistryKey ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
+$NewPath = $CurrentPath + ";%ProgramFiles%\PowerShell\7\"
+$Result = New-ItemProperty -Path $RegistryKey -Name "Path" -PropertyType ExpandString -Value $NewPath -Force 
+
+$CurrentPSModulePath = (Get-Item -path $RegistryKey ).GetValue('PSModulePath', '', 'DoNotExpandEnvironmentNames')
+$NewPSModulePath = $CurrentPSModulePath + ";%ProgramFiles%\PowerShell\;%ProgramFiles%\PowerShell\7\;%SystemRoot%\system32\config\systemprofile\Documents\PowerShell\Modules\"
+$Result = New-ItemProperty -Path $RegistryKey -Name "PSModulePath" -PropertyType ExpandString -Value $NewPSModulePath -Force 
+
+Write-Verbose "Processing: Adding additional variables for PowerShell Gallery Support" -Verbose
+# Add additional environment variables for PowerShell Gallery Support
+$APPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Roaming"
+$Result = New-ItemProperty -Path $RegistryKey -Name "APPDATA" -PropertyType String -Value $APPDATA -Force 
+
+$HOMEDRIVE = "%SystemDrive%"
+$Result = New-ItemProperty -Path $RegistryKey -Name "HOMEDRIVE" -PropertyType String -Value $HOMEDRIVE -Force 
+
+$HOMEPATH = "%SystemRoot%\System32\Config\SystemProfile"
+$Result = New-ItemProperty -Path $RegistryKey -Name "HOMEPATH" -PropertyType String -Value $HOMEPATH -Force 
+
+$LOCALAPPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Local"
+$Result = New-ItemProperty -Path $RegistryKey -Name "LOCALAPPDATA" -PropertyType String -Value $LOCALAPPDATA -Force 
+
+Write-Verbose "Processing: Cleaning up" -Verbose
+# Cleanup (to prevent access denied issue unloading the registry hive)
+Get-Variable Result | Remove-Variable
+Get-Variable RegistryKey | Remove-Variable
+[gc]::collect()
+Start-Sleep -Seconds 5
+
+Write-Verbose "Processing: Unloading the registry hive" -Verbose
+# Unload the registry hive
+reg unload "HKLM\OfflineWinPE"
+
+Write-Verbose "Processing: Dismounting and saving the OSDCloud boot.wim" -Verbose
+# dismounting and saving the image
+Dismount-WindowsImage -Path $mountdir -Save
+
+
+######################################################################
+###  Remaining downloads
+######################################################################
 
 Write-Host
 Write-Verbose "Processing: ALL Virtualization driver downloads" -Verbose
@@ -157,39 +260,6 @@ Invoke-Expression $($OSDCloudWallpaper.Content)
 Write-Host
 Write-Verbose "Completed: OSDCloud wallpaper download" -Verbose
 Write-Host
-
-################################################
-# OSDCloud Template and Workspace configuration
-################################################
-
-Write-Host
-Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
-New-OSDCloudTemplate -Name ALL-Virtual -WinRE
-
-Write-Host
-Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
-Get-OSDCloudTemplateNames
-
-Write-Host
-Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
-Get-OSDCloudWorkspace
-
-Write-Host
-Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
-$WorkspacePath = "C:\OSDCloud\All-VT"
-New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
-Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
-
-Write-Host
-Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
-Get-OSDCloudWorkspace
-
-#################################
-# Adding PowerShell 7 to OSDCloud 
-#################################
-
-$AddPS7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/PowerShell%20Modules/PS%207%20Support%20to%20OSDCloud/AddPS7-OSDCloudISO.ps1")
-Invoke-Expression $($AddPS7.Content)
 
 #################################
 # OSDCloud - Cloud Drivers
