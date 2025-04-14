d3c63609a14d3a2ac9fa2754a49188526f6a2a69
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 21:40:37 -0400

    Update ALL-VT.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Virtualization/ALL-VT.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Virtualization/ALL-VT.ps1
@@ -24,6 +24,95 @@ Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
 
+#########################################
+# ADD PowerShell 7
+##########################################
+Import-Module -Name OSD -Force
+
+$sourcedownload = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.zip"
+$DLDestination = "C:\OSDCloud\downloads\PowerShell\7"
+
+Write-Verbose "Processing: PowerShell 7.5.0 download..." -Verbose
+Write-Host
+Save-WebFile -SourceUrl $sourcedownload -DestinationDirectory $DLDestination
+
+Write-Verbose "Completed: PowerShell 7.5.0 download" -Verbose
+Write-Host
+
+# Settings and File Paths
+
+$sourcedownload = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.zip"
+$DLDestination = "C:\OSDCloud\downloads\PowerShell\7"
+$mountdir = "C:\Mount"
+$sourceWIMDir = "\Media\sources"
+$WorkspacePath = Get-OSDCloudWorkspacePath
+$WimFile = Join-Path -Path $WorkspacePath -ChildPath $sourceWIMDir
+
+Write-Verbose "Processing: Mounting OSDCloud boot.wim" -Verbose
+# Mount the image
+Mount-WindowsImage -ImagePath "$WimFile\boot.wim" -Path $mountdir -Index 1
+
+Write-Host
+Write-Verbose "Processing: Expanding Windows PowerShell 7 ZIP File" -Verbose
+Write-Host
+
+Expand-Archive -Path "$DLDestination\PowerShell-7.5.0-win-x64.zip" -DestinationPath "$mountdir\Program Files\PowerShell\7" -Force
+
+Write-Verbose "Processing: Updating environment PATHS" -Verbose
+Write-Host
+
+# Update the offline environment PATH for PowerShell 7
+$HivePath = "$mountdir\Windows\System32\config\SYSTEM"
+reg load "HKLM\OfflineWinPE" $HivePath 
+Start-Sleep -Seconds 5
+
+Write-Verbose "Processing: Adding PowerShell 7 Paths to PATH and PSModulePath" -Verbose
+Write-Host
+# Add PowerShell 7 Paths to Path and PSModulePath
+$RegistryKey = "HKLM:\OfflineWinPE\ControlSet001\Control\Session Manager\Environment"
+$CurrentPath = (Get-Item -path $RegistryKey ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
+$NewPath = $CurrentPath + ";%ProgramFiles%\PowerShell\7\"
+$Result = New-ItemProperty -Path $RegistryKey -Name "Path" -PropertyType ExpandString -Value $NewPath -Force 
+
+$CurrentPSModulePath = (Get-Item -path $RegistryKey ).GetValue('PSModulePath', '', 'DoNotExpandEnvironmentNames')
+$NewPSModulePath = $CurrentPSModulePath + ";%ProgramFiles%\PowerShell\;%ProgramFiles%\PowerShell\7\;%SystemRoot%\system32\config\systemprofile\Documents\PowerShell\Modules\"
+$Result = New-ItemProperty -Path $RegistryKey -Name "PSModulePath" -PropertyType ExpandString -Value $NewPSModulePath -Force 
+
+Write-Verbose "Processing: Adding additional variables for PowerShell Gallery Support" -Verbose
+Write-Host
+
+$APPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Roaming"
+$Result = New-ItemProperty -Path $RegistryKey -Name "APPDATA" -PropertyType String -Value $APPDATA -Force 
+
+$HOMEDRIVE = "%SystemDrive%"
+$Result = New-ItemProperty -Path $RegistryKey -Name "HOMEDRIVE" -PropertyType String -Value $HOMEDRIVE -Force 
+
+$HOMEPATH = "%SystemRoot%\System32\Config\SystemProfile"
+$Result = New-ItemProperty -Path $RegistryKey -Name "HOMEPATH" -PropertyType String -Value $HOMEPATH -Force 
+
+$LOCALAPPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Local"
+$Result = New-ItemProperty -Path $RegistryKey -Name "LOCALAPPDATA" -PropertyType String -Value $LOCALAPPDATA -Force 
+
+Write-Verbose "Processing: Cleaning up" -Verbose
+Write-Host
+
+# Cleanup (to prevent access denied issue unloading the registry hive)
+Get-Variable Result | Remove-Variable
+Get-Variable RegistryKey | Remove-Variable
+[gc]::collect()
+Start-Sleep -Seconds 5
+
+Write-Verbose "Processing: Unloading the registry hive" -Verbose
+Write-Host
+
+# Unload the registry hive
+reg unload "HKLM\OfflineWinPE"
+
+Write-Verbose "Processing: Dismounting and saving the OSDCloud boot.wim" -Verbose
+Write-Host
+# dismounting and saving the image
+Dismount-WindowsImage -Path $mountdir -Save
+
 ##########################################
 # OSDCloud WebScript for Startnet.cmd
 ##########################################
