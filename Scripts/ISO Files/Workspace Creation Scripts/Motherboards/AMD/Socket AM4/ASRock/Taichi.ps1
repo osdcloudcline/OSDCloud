3e6c7c840526ab6eb2dd038c3b563694087c0001
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-14 00:46:13 -0400

    Update Taichi.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/Taichi.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/Taichi.ps1
@@ -216,6 +216,11 @@ $ISO2 = "$OSDCloudISOPath\OSDCloud-ASRock-Taichi-AM4_NoPrompt.iso"
 
 $OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM4\ASRock"
 
+Write-Verbose "Processing: Creating ISO directory" -Verbose
+Write-Host
+# Create ISO Directory
+New-Item -Path $OSDISODestination -ItemType Directory -Force
+
 Write-Verbose "Copying OSDCloud ISO Files..." -Verbose
 Write-Host
 
