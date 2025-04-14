2a6e89ae1ca8b625406841a210863d04588e55d8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-13 21:22:24 -0400

    Update ALL-VT.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Virtualization/ALL-VT.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Virtualization/ALL-VT.ps1
@@ -23,3 +23,37 @@ Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
 Write-Host
 Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
 Get-OSDCloudWorkspace
+
+##########################################
+# OSDCloud WebScript for Startnet.cmd
+##########################################
+
+Write-Verbose "Adding a customized PowerShell based Startnet script and configuring OSDCloud to execute it on startup..." -Verbose
+Edit-OSDCloudWinPE -WebPSScript https://raw.githubusercontent.com/osdcloudcline/OSDCloud/refs/heads/main/Scripts/ISO%20Files/OSDCloud%20Startup%20Scripts/OSDCloudStartnet.ps1
+Write-Host
+
+###########################################
+# Rename OSDCloud ISO
+#########################################
+
+$OSDCloudISOPath = "C:\OSDCloud\ALL-VT"
+
+Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
+Write-Host
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ALL-VT\OSDCloud-ALL-VT.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ALL-VT\OSDCloud-ALL-VT_NoPrompt.iso" -Force
+
+###########################################
+# Copy OSDCloud ISO Files
+#########################################
+
+$ISO1 = "C:\OSDCloud\ALL-VT\OSDCloud-ALL-VT.iso"
+$ISO2 = "C:\OSDCloud\ALL-VT\OSDCloud-ALL-VT_NoPrompt.iso"
+
+$OSDISODestination = "C:\ISOs\OSDCloud\Vitualization"
+
+Write-Verbose "Copying OSDCloud ISO Files..." -Verbose
+Write-Host
+
+Copy-Item -Path $ISO1 -Destination $OSDISODestination -Force
+Copy-Item -Path $ISO2 -Destination $OSDISODestination -Force
