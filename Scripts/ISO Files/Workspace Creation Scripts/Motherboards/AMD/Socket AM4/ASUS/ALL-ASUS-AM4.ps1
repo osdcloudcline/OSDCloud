d8fb3231f61dd72a85a3b229e478c1f34ddbe0d8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-14 06:36:40 -0400

    Update ALL-ASUS-AM4.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASUS/ALL-ASUS-AM4.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASUS/ALL-ASUS-AM4.ps1
@@ -15,10 +15,6 @@ Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
 Get-OSDCloudTemplateNames
 
 Write-Host
-Write-Verbose "Retriving OSDCloud Workspaces..." -Verbose
-Get-OSDCloudWorkspace
-
-Write-Host
 Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
 $WorkspacePath = "C:\OSDCloud\ALL-ASUS-AM4"
 New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
