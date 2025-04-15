608abf47230eeedd6ee71db270db242e22259d3b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-14 23:44:21 -0400

    Update MASTER-ASRock-AM4.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/MASTER-ASRock-AM4.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/MASTER-ASRock-AM4.ps1
@@ -34,35 +34,54 @@ Write-Verbose "Processing: $ASRock1" -Verbose
 $ASRock1Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/ALL-ASRock-AM4.ps1")
 Invoke-Expression $($ASRock1Script.Content)
 
+$Cleanup1 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup1.Content)
+
 Write-Verbose "Processing: $ASRock2" -Verbose
 
 $ASRock2Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Extreme.ps1")
 Invoke-Expression $($ASRock2Script.Content)
+
+$Cleanup2 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup2.Content)
 
 Write-Verbose "Processing: $ASRock3" -Verbose
 
 $ASRock3Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Fatal1tyGaming.ps1")
 Invoke-Expression $($ASRock3Script.Content)
 
+$Cleanup3 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup3.Content)
+
 Write-Verbose "Processing: $ASRock4" -Verbose
 
 $ASRock4Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/PRO.ps1")
 Invoke-Expression $($ASRock4Script.Content)
+
+$Cleanup4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup4.Content)
 
 Write-Verbose "Processing: $ASRock5" -Verbose
 
 $ASRock5Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/PhantomGaming.ps1")
 Invoke-Expression $($ASRock5Script.Content)
 
+$Cleanup5 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup5.Content)
+
 Write-Verbose "Processing: $ASRock6" -Verbose
 
 $ASRock6Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/SteelLegend.ps1")
 Invoke-Expression $($ASRock6Script.Content)
+
+$Cleanup6 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup6.Content)
 
 Write-Verbose "Processing: $ASRock7" -Verbose
 
 $ASRock7Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Taichi.ps1")
 Invoke-Expression $($ASRock7Script.Content)
 
-
+$Cleanup7 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Cleanup.ps1")
+Invoke-Expression $($Cleanup7.Content)
 
