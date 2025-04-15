e8838b05df0deffbe1bdfd12ff3172ab836c0def
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-15 18:45:11 -0400

    Update AQUA.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/AQUA.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/AQUA.ps1
@@ -4,7 +4,7 @@
 
 Write-Host
 Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
-New-OSDCloudTemplate -Name ASRock-LGA1700 -WinRE
+New-OSDCloudTemplate -Name ASRock-AQUA-LGA1700 -WinRE
 
 Write-Host
 Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
