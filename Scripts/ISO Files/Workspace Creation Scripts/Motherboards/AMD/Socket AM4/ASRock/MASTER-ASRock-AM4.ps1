ddee7538084a9cdbc0a7398213a3be56b700f5e9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-14 22:53:32 -0400

    Update MASTER-ASRock-AM4.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/MASTER-ASRock-AM4.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/MASTER-ASRock-AM4.ps1
@@ -1,227 +1,68 @@
-
-################################################
-# OSDCloud Template and Workspace configuration
-################################################
-
 Write-Host
-Write-Verbose "Creating New OSDCloud WinRE Template to enable wireless networking support..." -Verbose
-New-OSDCloudTemplate -Name ASRock-AM4 -WinRE
-
+Write-Host "Hello, $env:username..." -ForegroundColor Cyan 
 Write-Host
-Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
-Get-OSDCloudTemplateNames
-
+$Date = Get-Date
+Write-Host "Today is:" "$Date"
 Write-Host
-Write-Verbose "Configuring and setting new OSDCloud Workspace Path..." -Verbose
-$WorkspacePath = "C:\OSDCloud\ALL-ASRock-AM4"
-New-OSDCloudWorkspace -WorkspacePath $WorkspacePath
-Set-OSDCloudWorkspace -WorkspacePath $WorkspacePath
-
+Write-Verbose "Your user profile is located at $env:userprofile" -Verbose
 Write-Host
-Write-Verbose "Confirming new OSDCloud Workspace Path..." -Verbose
-Get-OSDCloudWorkspace
-
-#########################################
-# ADD PowerShell 7
-##########################################
-Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
-Import-Module -Name OSD -Force
-
-Install-Module -Name 7Zip4Powershell -Force -AllowClobber -SkipPublisherCheck -Verbose
-Import-Module -Name 7Zip4Powershell -Force
-
-$sourcedownload = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.zip"
-$DLDestination = "C:\OSDCloud\downloads\PowerShell\7"
-
-Write-Verbose "Processing: PowerShell 7.5.0 download..." -Verbose
-Write-Host
-Save-WebFile -SourceUrl $sourcedownload -DestinationDirectory $DLDestination
-
-Write-Verbose "Completed: PowerShell 7.5.0 download" -Verbose
-Write-Host
-
-# Settings and File Paths
-
-$sourcedownload = "https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.zip"
-$DLDestination = "C:\OSDCloud\downloads\PowerShell\7"
-$mountdir = "C:\Mount"
-$sourceWIMDir = "\Media\sources"
-$WorkspacePath = Get-OSDCloudWorkspace
-$WimFile = Join-Path -Path $WorkspacePath -ChildPath $sourceWIMDir
-
-Write-Verbose "Processing: Creating mount directory" -Verbose
-Write-Host
-# Create Mount Directory
-New-Item -Path $mountdir -ItemType Directory -Force
-
-Write-Verbose "Processing: Mounting OSDCloud boot.wim" -Verbose
-# Mount the image
-Mount-WindowsImage -ImagePath "$WimFile\boot.wim" -Path $mountdir -Index 1
-
-Write-Host
-Write-Verbose "Processing: Expanding Windows PowerShell 7 ZIP File" -Verbose
-Write-Host
-
-Expand-Archive -Path "$DLDestination\PowerShell-7.5.0-win-x64.zip" -DestinationPath "$mountdir\Program Files\PowerShell\7" -Force
-
-Write-Verbose "Processing: Updating environment PATHS" -Verbose
-Write-Host
-
-# Update the offline environment PATH for PowerShell 7
-$HivePath = "$mountdir\Windows\System32\config\SYSTEM"
-reg load "HKLM\OfflineWinPE" $HivePath 
-Start-Sleep -Seconds 5
-
-Write-Verbose "Processing: Adding PowerShell 7 Paths to PATH and PSModulePath" -Verbose
-Write-Host
-# Add PowerShell 7 Paths to Path and PSModulePath
-$RegistryKey = "HKLM:\OfflineWinPE\ControlSet001\Control\Session Manager\Environment"
-$CurrentPath = (Get-Item -path $RegistryKey ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
-$NewPath = $CurrentPath + ";%ProgramFiles%\PowerShell\7\"
-$Result = New-ItemProperty -Path $RegistryKey -Name "Path" -PropertyType ExpandString -Value $NewPath -Force 
-
-$CurrentPSModulePath = (Get-Item -path $RegistryKey ).GetValue('PSModulePath', '', 'DoNotExpandEnvironmentNames')
-$NewPSModulePath = $CurrentPSModulePath + ";%ProgramFiles%\PowerShell\;%ProgramFiles%\PowerShell\7\;%SystemRoot%\system32\config\systemprofile\Documents\PowerShell\Modules\"
-$Result = New-ItemProperty -Path $RegistryKey -Name "PSModulePath" -PropertyType ExpandString -Value $NewPSModulePath -Force 
-
-Write-Verbose "Processing: Adding additional variables for PowerShell Gallery Support" -Verbose
-Write-Host
-
-$APPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Roaming"
-$Result = New-ItemProperty -Path $RegistryKey -Name "APPDATA" -PropertyType String -Value $APPDATA -Force 
-
-$HOMEDRIVE = "%SystemDrive%"
-$Result = New-ItemProperty -Path $RegistryKey -Name "HOMEDRIVE" -PropertyType String -Value $HOMEDRIVE -Force 
-
-$HOMEPATH = "%SystemRoot%\System32\Config\SystemProfile"
-$Result = New-ItemProperty -Path $RegistryKey -Name "HOMEPATH" -PropertyType String -Value $HOMEPATH -Force 
-
-$LOCALAPPDATA = "%SystemRoot%\System32\Config\SystemProfile\AppData\Local"
-$Result = New-ItemProperty -Path $RegistryKey -Name "LOCALAPPDATA" -PropertyType String -Value $LOCALAPPDATA -Force 
-
-Write-Verbose "Processing: Cleaning up" -Verbose
-Write-Host
-
-# Cleanup (to prevent access denied issue unloading the registry hive)
-Get-Variable Result | Remove-Variable
-Get-Variable RegistryKey | Remove-Variable
-[gc]::collect()
-Start-Sleep -Seconds 5
-
-Write-Verbose "Processing: Unloading the registry hive" -Verbose
-Write-Host
-
-# Unload the registry hive
-reg unload "HKLM\OfflineWinPE"
-
-Write-Verbose "Processing: Dismounting and saving the OSDCloud boot.wim" -Verbose
-Write-Host
-# dismounting and saving the image
-Dismount-WindowsImage -Path $mountdir -Save
+Write-Host 
+Write-Host '                                                                                              ' -BackgroundColor White                                                              
+Write-Host '    This scripts creates all ASRock OSCloud ISO files automatically:                          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ALL ASRock Socket AM4                                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASRock Socket AM4 Extreme Motherboards                                                 ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASRock Socket AM4 Fatal1ty Gaming Motherboards                                         ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASRock Socket AM4 PRO Motherboards                                                     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASRock Socket AM4 Phantom Gaming Motherboards                                          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASRock Socket AM4 Steel Legend Motherboards                                            ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - ASRock Socket AM4 Taichi Motherboards                                                  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                                              ' -BackgroundColor White
+pause
+Clear-Host
 
 
-#########################################
-##     ADD VBS Scripting Support
-#########################################
+$ASRock1 = "ALL ASRock Socket AM4 Motherboards OSDCloud ISO script"
+$ASRock2 = "ASRock Socket AM4 Extreme Motherboards OSDCloud ISO script"
+$ASRock3 = "ASRock Socket AM4 Fatal1ty Gaming Motherboards OSDCloud ISO script"
+$ASRock4 = "ASRock Socket AM4 PRO Motherboards OSDCloud ISO script"
+$ASRock5 = "ASRock Socket AM4 Phantom Gaming Motherboards OSDCloud ISO script"
+$ASRock6 = "ASRock Socket AM4 Steel Legend Motherboards OSDCloud ISO script"
+$ASRock7 = "ASRock Socket AM4 TaiChi Motherboards OSDCloud ISO script"
 
-# VBS Scripting Support
+Write-Verbose "Processing: $ASRock1" -Verbose
 
-$OSDCloudVBS_Extract = "C:\OSDCloud\GitHub\downloads\VBSScript"
-$OSDCloudGHdownloads = "C:\OSDCloud\GitHub\downloads"
+$ASRock1Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/ALL-ASRock-AM4.ps1")
+Invoke-Expression $($ASRock1Script.Content)
 
-$VBSScriptSupport_URL = "https://github.com/osdcloudcline/OSDCloud/raw/main/Extra%20Files/Scripting%20Support/VBS%20Scripting/VBScriptCABFiles.zip"
+Write-Verbose "Processing: $ASRock2" -Verbose
 
-Install-Module -Name OSD -Force -AllowClobber -SkipPublisherCheck -Verbose
-Import-Module -Name OSD -Force
+$ASRock2Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Extreme.ps1")
+Invoke-Expression $($ASRock2Script.Content)
 
-Install-Module -Name 7Zip4Powershell -Force -AllowClobber -SkipPublisherCheck -Verbose
-Import-Module -Name 7Zip4Powershell -Force
+Write-Verbose "Processing: $ASRock3" -Verbose
 
-Write-Verbose  "Acquiring VBS Script Support CAB Files from GitHub repository..." -Verbose
-Save-WebFile -SourceUrl $VBSScriptSupport_URL -DestinationDirectory $OSDCloudGHdownloads
-Expand-7Zip -ArchiveFileName "$OSDCloudGHdownloads\VBScriptCABFiles.zip" -TargetPath $OSDCloudVBS_Extract -ErrorAction SilentlyContinue
-Write-Host
+$ASRock3Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Fatal1tyGaming.ps1")
+Invoke-Expression $($ASRock3Script.Content)
+
+Write-Verbose "Processing: $ASRock4" -Verbose
+
+$ASRock4Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/PRO.ps1")
+Invoke-Expression $($ASRock4Script.Content)
+
+Write-Verbose "Processing: $ASRock5" -Verbose
+
+$ASRock5Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/PhantomGaming.ps1")
+Invoke-Expression $($ASRock5Script.Content)
+
+Write-Verbose "Processing: $ASRock6" -Verbose
+
+$ASRock6Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/SteelLegend.ps1")
+Invoke-Expression $($ASRock6Script.Content)
+
+Write-Verbose "Processing: $ASRock7" -Verbose
+
+$ASRock7Script = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Taichi.ps1")
+Invoke-Expression $($ASRock7Script.Content)
 
 
-$OSDCloudVBS_Extract = "C:\OSDCloud\GitHub\downloads\VBSScript"
-$VBSName1 = "Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~amd64~~.cab"
-$VBSName2 = "Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~amd64~en-us~.cab"
-$VBSName3 = "Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~wow64~~.cab"
-$VBSName4 = "Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~wow64~en-us~.cab"
-$VBS1 = "$OSDCloudVBS_Extract\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~amd64~~.cab"
-$VBS2 = "$OSDCloudVBS_Extract\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~amd64~en-us~.cab"
-$VBS3 = "$OSDCloudVBS_Extract\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~wow64~~.cab"
-$VBS4 = "$OSDCloudVBS_Extract\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~wow64~en-us~.cab"
-
-$sourceWIMDir = "\Media\sources"
-$WorkspacePath = Get-OSDCloudWorkspace
-$WimFile = Join-Path -Path $WorkspacePath -ChildPath $sourceWIMDir
-$mountdir = "C:\Mount"
-
-Write-Verbose "Processing: Mounting OSDCloud boot.wim" -Verbose
-# Mount the image
-
-Mount-WindowsImage -ImagePath "$WimFile\boot.wim" -Path $mountdir -Index 1
-
-Write-Host
-Write-Verbose "Processing: VBS Scripting Support..." -Verbose 
-
-Write-Host
-Write-Verbose "Processing Item: $VBSName1..." -Verbose
-Add-WindowsPackage -Path $mountdir -PackagePath $VBS1
-Write-Host
-
-Write-Verbose "Processing Item: $VBSName2..." -Verbose
-Add-WindowsPackage -Path $mountdir -PackagePath $VBS2
-Write-Host
-
-Write-Verbose "Processing Item: $VBSName3..." -Verbose
-Add-WindowsPackage -Path $mountdir -PackagePath $VBS3
-Write-Host
-
-Write-Verbose "Processing Item: $VBSName4..." -Verbose
-Add-WindowsPackage -Path $mountdir -PackagePath $VBS4
-
-Write-Host
-Write-Verbose "Completed: Integration of VBS Scripting Support for OSDCloud..." -Verbose
-Write-Host
-
-##########################################
-# OSDCloud WebScript for Startnet.cmd
-##########################################
-
-Write-Verbose "Adding a customized PowerShell based Startnet script and configuring OSDCloud to execute it on startup..." -Verbose
-Edit-OSDCloudWinPE -WebPSScript https://raw.githubusercontent.com/osdcloudcline/OSDCloud/refs/heads/main/Scripts/ISO%20Files/OSDCloud%20Startup%20Scripts/OSDCloudStartnet.ps1
-Write-Host
-
-###########################################
-# Rename OSDCloud ISO
-#########################################
-
-$OSDCloudISOPath = "C:\OSDCloud\ALL-ASRock-AM4"
-
-Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
-Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ALL-ASRock-AM4\OSDCloud-ALL-ASRock-AM4.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ALL-ASRock-AM4\OSDCloud-ALL-ASRock-AM4_NoPrompt.iso" -Force
-
-###########################################
-# Copy OSDCloud ISO Files
-#########################################
-
-$ISO1 = "C:\OSDCloud\ALL-ASRock-AM4\OSDCloud-ALL-ASRock-AM4.iso"
-$ISO2 = "C:\OSDCloud\ALL-ASRock-AM4\OSDCloud-ALL-ASRock-AM4_NoPrompt.iso"
-
-$OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM4\ASRock"
-
-Write-Verbose "Processing: Creating ISO directory" -Verbose
-Write-Host
-# Create ISO Directory
-New-Item -Path $OSDISODestination -ItemType Directory -Force
-
-Write-Verbose "Copying OSDCloud ISO Files..." -Verbose
-Write-Host
-
-Copy-Item -Path $ISO1 -Destination $OSDISODestination -Force
-Copy-Item -Path $ISO2 -Destination $OSDISODestination -Force
 
