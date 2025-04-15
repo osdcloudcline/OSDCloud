c46fd0e14aa3d4e1fe373305adacf0d35370f37d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-15 18:51:40 -0400

    Update Eagle.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/Gigabyte/Eagle.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/Gigabyte/Eagle.ps1
@@ -8,7 +8,7 @@ Get-OSDCloudTemplate
 
 Write-Host
 Write-Verbose "Creating New OSDCloud WinRE Template specific for ALL Gigabyte Eagle AM5 motherboards to enable wireless networking support..." -Verbose
-New-OSDCloudTemplate -Name Gigabyte-AM4 -WinRE
+New-OSDCloudTemplate -Name Gigabyte-Eagle-AM5 -WinRE
 
 Write-Host
 Write-Verbose "Confirming OSDCloudTemplate names......" -Verbose
