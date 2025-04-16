9b6a242fd17997d7e72ed98737b720e8b872061d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 13:21:48 -0400

    Update ConfirmApps.ps1

--- Scripts/ISO Files/Extra Apps/ConfirmApps.ps1
+++ Scripts/ISO Files/Extra Apps/ConfirmApps.ps1
@@ -1,4 +1,7 @@
 $mountdir = "C:\Mount"
 
 $ChromePath = "$mountdir\Windows\System32\Chrome.exe"
-$ExplorerPPPath = "$mountdir\Windows\System32Explorer++.exe"
+$ExplorerPPPath = "$mountdir\Windows\System32\Explorer++.exe"
+$Ghost64Path = "$mountdir\Windows\System32\Ghost64.exe"
+$GhostExplorerPath = "$mountdir\Windows\System32\Ghostexp.exe"
+$GhostServPath = "$mountdir\Windows\System32\GhostSrv.exe"
