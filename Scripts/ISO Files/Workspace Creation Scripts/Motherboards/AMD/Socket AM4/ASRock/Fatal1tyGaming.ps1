6c5fea1d212aecfdf502925930f9e23b8e9a15ef
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 10:49:46 -0400

    Update Fatal1tyGaming.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/Fatal1tyGaming.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/Fatal1tyGaming.ps1
@@ -208,7 +208,7 @@ Invoke-Expression $($OSDCloudPS7xMods.Content)
 ################################################
 
 Write-Host
-Write-Verbose "Processing: ASRock Fatal1ty Gaming Socket AM$ Motherboard drivers download......" -Verbose
+Write-Verbose "Processing: ASRock Fatal1ty Gaming Socket AM4 Motherboard drivers download......" -Verbose
 Write-Host
 
 $Fatal1tyGamingDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Fatal1ty%20Gaming%20Motherboards/Fatal1tyGaming.ps1")
