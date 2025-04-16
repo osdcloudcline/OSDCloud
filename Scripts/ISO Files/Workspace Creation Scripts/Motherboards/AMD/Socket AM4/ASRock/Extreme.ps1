5636bab8854d97e6aeb520b57946953e77b9a980
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 10:49:21 -0400

    Update Extreme.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/Extreme.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/Extreme.ps1
@@ -209,7 +209,7 @@ Invoke-Expression $($OSDCloudPS7xMods.Content)
 ################################################
 
 Write-Host
-Write-Verbose "Processing: ASRock Extreme Socket AM$ Motherboard drivers download......" -Verbose
+Write-Verbose "Processing: ASRock Extreme Socket AM4 Motherboard drivers download......" -Verbose
 Write-Host
 
 $ExtremeDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/Extreme%20Motherboards/Extreme.ps1")
