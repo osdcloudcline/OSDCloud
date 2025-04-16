c5ce8e4a852135516a4f48b40439ca3c7f9b6d88
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 12:34:50 -0400

    Update DownloadApps.ps1

--- Scripts/ISO Files/Extra Apps/DownloadApps.ps1
+++ Scripts/ISO Files/Extra Apps/DownloadApps.ps1
@@ -5,3 +5,21 @@ Write-Host '     - Google Chrome Portable Web Browser                           
 Write-Host '                                                                                   ' -BackgroundColor White
 Write-Host '                                                                                   '
 pause
+
+######################################################################################
+####    OSDCloud Web Browser URL                                                 #######
+######################################################################################
+$OSDBrowser = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Extra%20Files/Browser/Chrome.exe"
+
+
+######################################################################################
+####    OSDCloud Chrome Browser EXTRACTION LOCATION                            #######
+######################################################################################
+$ChromePath = "C:\OSDCloud Software\Browsers\Chrome"
+$ChromeDownloadPath = "C:\OSDCloud Software\Browsers\Chrome" 
+
+Import-Module -Name OSD -Force
+Import-Module -Name 7Zip4PowerShell -Force
+
+Write-Verbose "Acquiring Google Chrome Portable Web Browser from GitHub repository..." -Verbose
+Save-WebFile -SourceUrl $OSDBrowser  -DestinationDirectory $ChromeDownloadPath 
