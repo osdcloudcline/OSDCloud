8e5e6d3b5746e07af383743b998efab8474d8b58
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 08:54:57 -0400

    Update MainMenu.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/MainMenu.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/MainMenu.ps1
@@ -23,26 +23,11 @@ pause
 Clear-Host
 
 Write-Host "======= $Title ======"
-Write-Host " 1. OSDCloud: ASRock Socket AM4 motherboards"
-Write-Host " 2. OSDCloud: ASRock Socket AM5 motherboards"
-Write-Host " 3. OSDCloud: ASRock Socket LGA1700 motherboards"
-Write-Host " 4. OSDCloud: ASRock Socket LGA1851 motherboards"
-Write-Host " 5. OSDCloud: ASUS Socket AM4 motherboards"
-Write-Host " 6. OSDCloud: ASUS Socket AM5 motherboards"
-Write-Host " 7. OSDCloud: ASUS Socket LGA1700 motherboards"
-Write-Host " 8. OSDCloud: ASUS Socket LGA1851 motherboards"
-Write-Host " 9. OSDCloud: Gigabyte Socket AM4 motherboards"
-Write-Host " 10. OSDCloud: Gigabyte Socket AM5 motherboards"
-Write-Host " 11. OSDCloud: Gigabyte Socket LGA1700 motherboards"
-Write-Host " 12. OSDCloud: Gigabyte Socket LGA1851 motherboards"
-Write-Host " 13. OSDCloud: MSI Socket AM5 motherboards"
-Write-Host " 14. OSDCloud: MSI Socket LGA1700 motherboards"
-Write-Host " 15. OSDCloud: MSI Socket LGA1851 motherboards"
-Write-Host " 5. OSDCloud: OEM Desktops"
-Write-Host " 6. OSDCloud: OEM Laptops"
-Write-Host " 7. OSDCloud: OEM Tablet PCs"
-Write-Host " 8. OSDCloud: Virtualization"
-Write-Host " 9. Return to OSD Cloud Main Menu"
+Write-Host " 1. OSDCloud: Socket AM4 motherboards"
+Write-Host " 2. OSDCloud: Socket AM5 motherboards"
+Write-Host " 3. OSDCloud: Socket LGA1700 motherboards"
+Write-Host " 4. OSDCloud: Socket LGA1851 motherboards"
+Write-Host " 5. Return to OSD Cloud Main Menu"
 
 do 
 {
@@ -51,8 +36,8 @@ do
   {
 
   '1' { cls
-        $ASRockAM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/MainMenu.ps1")
-        Invoke-Expression $($ASRockAM4.Content)
+        $AM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/MainMenu.ps1")
+        Invoke-Expression $($AM4.Content)
         }
   '2' { cls
         $CPUSocketType = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/By%20CPU%20and%20Socket%20Type/MainMenu.ps1")
@@ -65,23 +50,7 @@ do
   '4' { cls
         $Motherboards = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/MainMenu.ps1")
         Invoke-Expression $($Motherboards.Content)
-        }
-  '5' { cls
-        $OEMDesktops = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/OEM%20Desktops/MainMenu.ps1")
-        Invoke-Expression $($OEMDesktops.Content)
-        }
-  '6' { cls
-        $OEMLaptops = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/OEM%20Laptops/MainMenu.ps1")
-        Invoke-Expression $($OEMLaptops.Content)
-        }
-  '7' { cls
-        $OEMTablets = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/OEM%20Tablet%20PCs/MainMenu.ps1")
-        Invoke-Expression $($OEMTablets.Content)
-        }
-  '8' { cls
-        $Virtualization = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Virtualization/MainMenu.ps1")
-        Invoke-Expression $($Virtualization.Content)
-        }    
+        }   
   '9' { cls
         $OSDCloudMain = Invoke-WebRequest("")
         Invoke-Expression $($OSDCloudMain.Content)
