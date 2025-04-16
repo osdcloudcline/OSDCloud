e65f48391272943f4f45f40e0a46cbb683e628f4
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 10:50:12 -0400

    Update PRO.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/PRO.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASRock/PRO.ps1
@@ -208,7 +208,7 @@ Invoke-Expression $($OSDCloudPS7xMods.Content)
 ################################################
 
 Write-Host
-Write-Verbose "Processing: ASRock PRO Socket AM$ Motherboard drivers download......" -Verbose
+Write-Verbose "Processing: ASRock PRO Socket AM4 Motherboard drivers download......" -Verbose
 Write-Host
 
 $PRODrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/AMD/AM4/PRO%20Motherboards/PRO.ps1")
