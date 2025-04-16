20b8a11bcec70ce7681364514df677231ec91ebb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 08:48:05 -0400

    Update MainMenu.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/MainMenu.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/MainMenu.ps1
@@ -24,9 +24,20 @@ Clear-Host
 
 Write-Host "======= $Title ======"
 Write-Host " 1. OSDCloud: ASRock Socket AM4 motherboards"
-Write-Host " 2. OSDCloud: By CPU and Socket Type"
-Write-Host " 3. OSDCloud: Custom Scripts"
-Write-Host " 4. OSDCloud: Motherboards"
+Write-Host " 2. OSDCloud: ASRock Socket AM5 motherboards"
+Write-Host " 3. OSDCloud: ASRock Socket LGA1700 motherboards"
+Write-Host " 4. OSDCloud: ASRock Socket LGA1851 motherboards"
+Write-Host " 5. OSDCloud: ASUS Socket AM4 motherboards"
+Write-Host " 6. OSDCloud: ASUS Socket AM5 motherboards"
+Write-Host " 7. OSDCloud: ASUS Socket LGA1700 motherboards"
+Write-Host " 8. OSDCloud: ASUS Socket LGA1851 motherboards"
+Write-Host " 9. OSDCloud: Gigabyte Socket AM4 motherboards"
+Write-Host " 10. OSDCloud: Gigabyte Socket AM5 motherboards"
+Write-Host " 11. OSDCloud: Gigabyte Socket LGA1700 motherboards"
+Write-Host " 12. OSDCloud: Gigabyte Socket LGA1851 motherboards"
+Write-Host " 13. OSDCloud: MSI Socket AM5 motherboards"
+Write-Host " 14. OSDCloud: MSI Socket LGA1700 motherboards"
+Write-Host " 15. OSDCloud: MSI Socket LGA1851 motherboards"
 Write-Host " 5. OSDCloud: OEM Desktops"
 Write-Host " 6. OSDCloud: OEM Laptops"
 Write-Host " 7. OSDCloud: OEM Tablet PCs"
@@ -40,7 +51,8 @@ do
   {
 
   '1' { cls
-        Show-ASRockAM4
+        $ASRockAM4 = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/MainMenu.ps1")
+        Invoke-Expression $($ASRockAM4.Content)
         }
   '2' { cls
         $CPUSocketType = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/By%20CPU%20and%20Socket%20Type/MainMenu.ps1")
@@ -77,59 +89,5 @@ do
     }
     }
      until ($selection -eq '9'){Invoke-Expression $($OSDCloudMain.Content)}
-
-Function Show-ASRockAM4{ 
-
-Write-Host "======= $Title ======"
-Write-Host " 1. OSDCloud: ALL ASRock Socket AM4 motherboards"
-Write-Host " 2. OSDCloud: ASRock Extreme Socket AM4 Motherboard"
-Write-Host " 3. OSDCloud: ASRock Fatal1ty Gaming Socket AM4 Motherboard"
-Write-Host " 4. OSDCloud: ASRock PRO Socket AM4 Motherboard"
-Write-Host " 5. OSDCloud: ASRock Phantom Gaming Socket AM4 Motherboard"
-Write-Host " 6. OSDCloud: ASRock Steel Legend Socket AM4 Motherboard"
-Write-Host " 7. OSDCloud: ASRock Taichi Socket AM4 Motherboard"
-Write-Host " 8. Return to OSD Cloud Motherboard Main Menu"
-
-do 
-{
-  $selection = Read-Host 'Please choose an option'
-  switch($selection)
-  {
-
-  '1' { cls
-        $ASRock = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/ALL-ASRock-AM4.ps1")
-        Invoke-Expression $($ASRock.Content)
-        }
-  '2' { cls
-        $Extreme = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Extreme.ps1")
-        Invoke-Expression $($Extreme.Content)
-        }
-  '3' { cls
-        $Fatal1tyGaming = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Fatal1tyGaming.ps1")
-        Invoke-Expression $($Fatal1tyGaming.Content)
-        }
-  '4' { cls
-        $PRO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/PRO.ps1")
-        Invoke-Expression $($PRO.Content)
-        }
-  '5' { cls
-        $PhantomGaming = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/PhantomGaming.ps1")
-        Invoke-Expression $($PhantomGaming.Content)
-        }
-  '6' { cls
-        $SteelLegend = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/SteelLegend.ps1")
-        Invoke-Expression $($SteelLegend.Content)
-        }
-  '7' { cls
-        $Taichi = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/ASRock/Taichi.ps1")
-        Invoke-Expression $($Taichi.Content)
-        }   
-  '8' { cls
-        Show-MainMenu
-        }
-    }
-    }
-     until ($selection -eq '8'){Show-MainMenu}
-    }
 
 Show-MainMenu
