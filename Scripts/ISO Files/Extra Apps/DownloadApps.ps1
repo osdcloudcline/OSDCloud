10a2075c464eeab5ce6bdef688e55079baec1244
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 12:32:41 -0400

    Update DownloadApps.ps1

--- Scripts/ISO Files/Extra Apps/DownloadApps.ps1
+++ Scripts/ISO Files/Extra Apps/DownloadApps.ps1
@@ -1,1 +1,7 @@
-
+Write-Host '                                                                                   '
+Write-Host '                                                                                   ' -BackgroundColor White                                                              
+Write-Host ' The following extras will be downloaded to be integrated into OSDCloud boot.wim:  ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '     - Google Chrome Portable Web Browser                                          ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host '                                                                                   ' -BackgroundColor White
+Write-Host '                                                                                   '
+pause
