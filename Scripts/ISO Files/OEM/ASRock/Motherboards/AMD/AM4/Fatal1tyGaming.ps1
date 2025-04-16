d95001aa18b3e30ba8a5e3c7c036dd7934a59012
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-16 08:04:55 -0400

    Update Fatal1tyGaming.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/Fatal1tyGaming.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/Fatal1tyGaming.ps1
@@ -5,30 +5,24 @@
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Fatal1ty Gaming Motherboard Ethernet Drivers..." -Verbose 
-$Fatal1tyGamingEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Ethernet1"
-$Fatal1tyGamingEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Ethernet2"
+$Fatal1tyGamingEthernet1 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Ethernet1"
+$Fatal1tyGamingEthernet2 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Ethernet2"
 
 Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingEthernet1
 Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingEthernet2
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Fatal1ty Gaming Motherboard WiFi Drivers..." -Verbose 
-$Fatal1tyGamingWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\WiFi1"
-$Fatal1tyGamingWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\WiFi2"
+$Fatal1tyGamingWiFi1 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\WiFi1"
+$Fatal1tyGamingWiFi2 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\WiFi2"
 
 Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingWiFi1
 Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingWiFi2
 
 Write-Host
-Write-Verbose "Processing: ASRock AM 4 Fatal1ty Gaming Motherboard Bluetooth Drivers..." -Verbose 
-$Fatal1tyGamingBluetooth = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Bluetooth"
-
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingBluetooth
-
-Write-Host
 Write-Verbose "Processing: ASRock AM 4 Extreme Motherboard Storage Drivers..." -Verbose 
-$Fatal1tyGamingStorage1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Storage1"
-$Fatal1tyGamingStorage2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Storage2"
+$Fatal1tyGamingStorage1 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Storage1"
+$Fatal1tyGamingStorage2 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Storage2"
 
 Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingStorage1
 Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingStorage2
