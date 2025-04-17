6ef98c52080c1c856aada7feb521690df6454c8d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-17 07:44:00 -0400

    Update ALL-Gigabyte-AM4.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/Gigabyte/ALL-Gigabyte-AM4.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/Gigabyte/ALL-Gigabyte-AM4.ps1
@@ -371,6 +371,13 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating Gigabyte Socket AM4 AERO motherboard drivers" -Verbose
+Write-Host
+
+$AERO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/Gigabyte/Motherboards/AMD/AM4/AERO.ps1")
+Invoke-Expression $($AERO.Content)
+
 
 
 Write-Host
