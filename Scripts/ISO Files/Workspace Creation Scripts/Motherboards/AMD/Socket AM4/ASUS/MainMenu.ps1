c455e89eb4a81d55fffe88c47bfadc83f0798fc9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-17 00:11:34 -0400

    Update MainMenu.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASUS/MainMenu.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/ASUS/MainMenu.ps1
@@ -4,6 +4,7 @@ Function Show-ASRockAM4{
     [string]$Title = 'OSD Cloud ASUS Socket AM4 ISO Creation - Main Menu',
     [string]$Question = 'What type of action do you want to perform?'
 )
+cls
 
 Write-Host "======= $Title ======"
 Write-Host " 1. OSDCloud: ALL ASUS Socket AM4 motherboards"
