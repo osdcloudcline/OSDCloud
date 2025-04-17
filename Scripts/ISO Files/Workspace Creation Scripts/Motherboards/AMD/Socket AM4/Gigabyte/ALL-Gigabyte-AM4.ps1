06c046fcc83d870002497e5b600c81c2d5806aa9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-17 07:38:40 -0400

    Update ALL-Gigabyte-AM4.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/Gigabyte/ALL-Gigabyte-AM4.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/Gigabyte/ALL-Gigabyte-AM4.ps1
@@ -275,6 +275,17 @@ $GamingDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDri
 Invoke-Expression $($GamingDrivers.Content)
 
 #################################
+# Download Gigabyte UD-Ultra Durable Socket AM4 motherboard drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: Gigabyte Socket AM4 UD - Ultra Durable motherboard drivers" -Verbose
+Write-Host
+
+$Ultra DurableDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/AMD/AM4/Ultra%20Durable/UltraDurable.ps1")
+Invoke-Expression $($UltraDurableDrivers.Content)
+
+#################################
 # Download OSDCloud - Cloud Drivers
 #################################
 
