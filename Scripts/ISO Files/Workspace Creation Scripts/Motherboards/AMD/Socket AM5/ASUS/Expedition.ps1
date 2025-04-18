062043f46bce0d2c77da6e9469ab560d119da50e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-17 23:56:18 -0400

    Update Expedition.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/Expedition.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/Expedition.ps1
@@ -338,7 +338,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASUS Socket AM5 Expedition motherboard drivers" -Verbose
+Write-Host
 
+$Expedition = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASUS/Motherboards/AMD/AM5/Expedition.ps1")
+Invoke-Expression $($Expedition.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
