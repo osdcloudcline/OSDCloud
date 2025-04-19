7c4ef00f2e4ad8ca367250aae63be95ec6a3fe8b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-19 06:51:04 -0400

    Update MEG.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/MSI/MEG.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/MSI/MEG.ps1
@@ -338,7 +338,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating MSI Socket AM5 MEG motherboard drivers" -Verbose
+Write-Host
 
+$MEG = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/MSI/Motherboards/AMD/AM%205/MEG.ps1")
+Invoke-Expression $($MEG.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
