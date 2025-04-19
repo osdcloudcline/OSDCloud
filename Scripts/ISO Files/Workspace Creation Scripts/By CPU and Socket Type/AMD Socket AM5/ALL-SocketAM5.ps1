17f380139d67df1b90c24627c38d6183429d3e31
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-19 08:00:59 -0400

    Update ALL-SocketAM5.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/By CPU and Socket Type/AMD Socket AM5/ALL-SocketAM5.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/By CPU and Socket Type/AMD Socket AM5/ALL-SocketAM5.ps1
@@ -573,3 +573,24 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASRock PRO Socket AM5 Motherboard Drivers" -Verbose
+Write-Host
+
+$PRO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASRock/Motherboards/AMD/AM5/PRO.ps1")
+Invoke-Expression $($PRO.Content)
+
+Write-Host
+Write-Verbose "Processing: Integrating ASRock Steel Legend Socket AM5 Motherboard Drivers" -Verbose
+Write-Host
+
+$SteelLegend = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASRock/Motherboards/AMD/AM5/SteelLegend.ps1")
+Invoke-Expression $($SteelLegend.Content)
+
+Write-Host
+Write-Verbose "Processing: Integrating ASRock Taichi Socket AM5 Motherboard Drivers" -Verbose
+Write-Host
+
+$Taichi = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASRock/Motherboards/AMD/AM5/Taichi.ps1")
+Invoke-Expression $($Taichi.Content)
+
