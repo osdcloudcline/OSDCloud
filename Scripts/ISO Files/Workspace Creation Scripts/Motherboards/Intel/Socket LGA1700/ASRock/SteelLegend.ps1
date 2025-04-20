26eee95d3f2139157eb1f6d1c74b4bca94ceef06
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-20 07:34:29 -0400

    Update SteelLegend.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/SteelLegend.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/SteelLegend.ps1
@@ -334,7 +334,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASRock Steel Legend Socket LGA 1700 Motherboard Drivers" -Verbose
+Write-Host
 
+$SteelLegend = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASRock/Motherboards/Intel/LGA1700/SteelLegend.ps1")
+Invoke-Expression $($SteelLegend.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
