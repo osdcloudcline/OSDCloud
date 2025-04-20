3ae347c1e4bc0f15053c75900824349ec25123bf
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-20 07:29:44 -0400

    Update AQUA.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/AQUA.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/AQUA.ps1
@@ -334,7 +334,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASRock AQUA Socket LGA 1700 Motherboard Drivers" -Verbose
+Write-Host
 
+$AQUA = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASRock/Motherboards/Intel/LGA1700/AQUA.ps1")
+Invoke-Expression $($AQUA.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
