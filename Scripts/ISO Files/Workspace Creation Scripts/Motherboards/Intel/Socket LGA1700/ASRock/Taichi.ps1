f75c34384df7d52fc0d271dfbfe59bdc0d28dd47
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-20 07:35:05 -0400

    Update Taichi.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/Taichi.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/Taichi.ps1
@@ -334,7 +334,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASRock Taichi Socket LGA 1700 Motherboard Drivers" -Verbose
+Write-Host
 
+$Taichi = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASRock/Motherboards/Intel/LGA1700/Taichi.ps1")
+Invoke-Expression $($Taichi.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
