952c78382b79f904e98804bbbbf21ea8a37fa379
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-19 23:47:34 -0400

    Update Extreme.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/Extreme.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/ASRock/Extreme.ps1
@@ -248,10 +248,6 @@ Write-Host
 $ExtremeDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASRock/Desktops/Intel/LGA%201700/Extreme%20Motherboards/Extreme.ps1")
 Invoke-Expression $($ExtremeDrivers.Content)
 
-Write-Host
-Write-Verbose "Completed: ASRock Extreme Socket LGA 1700 Motherboard drivers download..." -Verbose
-Write-Host
-
 #################################
 # Download OSDCloud - Cloud Drivers
 #################################
