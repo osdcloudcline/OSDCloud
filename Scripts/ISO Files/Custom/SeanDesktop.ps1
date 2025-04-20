d68f43d589c7b0b9cbfa2556a303da2dc37663d1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-20 11:17:04 -0400

    Update SeanDesktop.ps1

--- Scripts/ISO Files/Custom/SeanDesktop.ps1
+++ Scripts/ISO Files/Custom/SeanDesktop.ps1
@@ -7,25 +7,19 @@ Write-Verbose "Processing: Alienware M18 R2 drivers" -Verbose
 Write-Host
 
 Write-Verbose "Processing: Alienware M18 R2 Ethernet Driver..." -Verbose
-$EthernetDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\Ethernet"
+$EthernetDriver = "C:\Drivers\Custom\Sean Laptops\Alienware M18 R2\Ethernet"
 
 Edit-OSDCloudWinPE -DriverPath $EthernetDriver
 
 Write-Host
-Write-Verbose "Processing: Alienware M18 R2 Bluetooth Driver..." -Verbose
-$BluetoothDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\Bluetooth"
-
-Edit-OSDCloudWinPE -DriverPath $BluetoothDriver
-
-Write-Host
 Write-Verbose "Processing: Alienware M18 R2 WiFi Driver..." -Verbose
-$WiFiDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\WiFi"
+$WiFiDriver = "C:\Drivers\Custom\Sean Laptops\Alienware M18 R2\WLAN"
 
 Edit-OSDCloudWinPE -DriverPath $WiFiDriver 
 
 Write-Host
 Write-Verbose "Processing: Alienware M18 R2 Storage Driver..." -Verbose
-$StorageDriver = "C:\OSDCloud\Drivers\Custom\Sean Laptops\Alienware M18 R2\Storage"
+$StorageDriver = "C:\Drivers\Custom\Sean Laptops\Alienware M18 R2\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $StorageDriver 
 
