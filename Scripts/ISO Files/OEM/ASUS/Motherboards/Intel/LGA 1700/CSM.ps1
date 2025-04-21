9eaa5e664ecbf5a96de00808f36245f5b14f29ff
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-21 14:19:48 -0400

    Update CSM.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/CSM.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/CSM.ps1
@@ -4,10 +4,10 @@
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1700 CSM Motherboard Ethernet Drivers..." -Verbose 
-$CSMEthernet1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet1"
-$CSMEthernet2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet2"
-$CSMEthernet3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet3"
-$CSMEthernet4 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet4"
+$CSMEthernet1 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet1"
+$CSMEthernet2 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet2"
+$CSMEthernet3 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet3"
+$CSMEthernet4 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\Ethernet4"
 
 Edit-OSDCloudWinPE -DriverPath $CSMEthernet1
 Edit-OSDCloudWinPE -DriverPath $CSMEthernet2
@@ -16,9 +16,9 @@ Edit-OSDCloudWinPE -DriverPath $CSMEthernet4
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1700 CSM Motherboard WiFi Drivers..." -Verbose 
-$CSMWiFi1 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\WLAN1"
-$CSMWiFi2 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\WLAN2"
-$CSMWiFi3 = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1700\CSM\WLAN3"
+$CSMWiFi1 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\WLAN1"
+$CSMWiFi2 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\WLAN2"
+$CSMWiFi3 = "C:\Drivers\Motherboards\ASUS\LGA1700\CSM\WLAN3"
 
 Edit-OSDCloudWinPE -DriverPath $CSMWiFi1
 Edit-OSDCloudWinPE -DriverPath $CSMWiFi2
@@ -26,7 +26,7 @@ Edit-OSDCloudWinPE -DriverPath $CSMWiFi3
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1700 CSM Motherboard Storage Drivers..." -Verbose 
-$CSMStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA700\CSM\Storage"
+$CSMStorage = "C:\Drivers\Motherboards\ASUS\LGA700\CSM\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $CSMStorage
 
