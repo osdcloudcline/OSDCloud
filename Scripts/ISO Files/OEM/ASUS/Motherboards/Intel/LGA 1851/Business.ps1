4143d6e94cea69b4a010fa2528645fb43d5b9dc1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-21 15:14:22 -0400

    Update Business.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/Business.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/Business.ps1
@@ -4,19 +4,19 @@
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1851 Business Motherboard Ethernet Drivers..." -Verbose 
-$BusinessEthernet = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\Business\Ethernet"
+$BusinessEthernet = "C:\Drivers\Motherboards\ASUS\LGA1851\Business\Ethernet"
 
 Edit-OSDCloudWinPE -DriverPath $BusinessEthernet
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1851 Business Motherboard WiFi Drivers..." -Verbose 
-$BusinessWiFi = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\Business\WLAN"
+$BusinessWiFi = "C:\Drivers\Motherboards\ASUS\LGA1851\Business\WLAN"
 
 Edit-OSDCloudWinPE -DriverPath $BusinessWiFi
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1851 Business Motherboard Storage Drivers..." -Verbose 
-$BusinessStorage = "C:\OSDCloud\Drivers\Motherboards\ASUS\LGA1851\Business\Storage"
+$BusinessStorage = "C:\Drivers\Motherboards\ASUS\LGA1851\Business\Storage"
 
 Edit-OSDCloudWinPE -DriverPath $BusinessStorage
 
