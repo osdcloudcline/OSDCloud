ca9acbf0f50bfa0638abcaf7e852ba160405f292
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-20 23:55:58 -0400

    Update PRO.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/PRO.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/PRO.ps1
@@ -6,9 +6,9 @@ Write-Host
 Write-Verbose "Processing: ASRock Intel LGA1851 PRO Motherboard drivers..." -Verbose
 Write-Host
 
-$PROEthernetPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\PRO\Ethernet"
-$PROStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\PRO\Storage"
-$PROWLANPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\PRO\WLAN"
+$PROEthernetPath = "C:\Drivers\Motherboards\ASRock\LGA1851\PRO\Ethernet"
+$PROStoragePath = "C:\Drivers\Motherboards\ASRock\LGA1851\PRO\Storage"
+$PROWLANPath = "C:\Drivers\Motherboards\ASRock\LGA1851\PRO\WLAN"
 
 Edit-OSDCloudWinPe -DriverPath $PROEthernetPath
 Edit-OSDCloudWinPe -DriverPath $PROStoragePath
