edc92da1c59e01c4715cf5a736821f0d2e5e158e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-20 23:57:57 -0400

    Update AQUA.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/AQUA.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1851/AQUA.ps1
@@ -6,17 +6,15 @@ Write-Host
 Write-Verbose "Processing: ASRock Intel LGA1851 AQUA Motherboard drivers..." -Verbose
 Write-Host
 
-$AQUAEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet1"
-$AQUAEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet2"
-$AQUAEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet3"
-$AQUAEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet4"
-$AQUAEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet5"
-$AQUAEthernetPath6 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet6"
-$AQUAEthernetPath7 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet7"
-
-$AQUAStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\Storage"
-
-$AQUAWLANPath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1851\AQUA\WLAN"
+$AQUAEthernetPath1 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet1"
+$AQUAEthernetPath2 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet2"
+$AQUAEthernetPath3 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet3"
+$AQUAEthernetPath4 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet4"
+$AQUAEthernetPath5 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet5"
+$AQUAEthernetPath6 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet6"
+$AQUAEthernetPath7 = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Ethernet7"
+$AQUAStoragePath = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\Storage"
+$AQUAWLANPath = "C:\Drivers\Motherboards\ASRock\LGA1851\AQUA\WLAN"
 
 Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath1
 Edit-OSDCloudWinPe -DriverPath $AQUAEthernetPath2
