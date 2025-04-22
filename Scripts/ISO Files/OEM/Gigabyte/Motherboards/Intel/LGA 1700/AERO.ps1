5a3b4f6d9d50f46f291d73a0f29afa7da1bb1f75
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-22 08:16:00 -0400

    Update AERO.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/Intel/LGA 1700/AERO.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/Intel/LGA 1700/AERO.ps1
@@ -4,24 +4,24 @@
 
 Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 AERO Motherboard Ethernet Drivers..." -Verbose 
-$AEROEthernet1 = "C:\Drivers\Motherboards\Gigabyte\LGA 1700\AERO\Ethernet1"
-$AEROEthernet2 = "C:\Drivers\Motherboards\Gigabyte\LGA 1700\AERO\Ethernet2"
+$AEROEthernet1 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\AERO\Ethernet1"
+$AEROEthernet2 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\AERO\Ethernet2"
 
 Edit-OSDCloudWinPE -DriversPath $AEROEthernet1
 Edit-OSDCloudWinPE -DriversPath $AEROEthernet2
 
 Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 AERO Motherboard WLAN Drivers..." -Verbose 
-$AEROWiFi1 = "C:\Drivers\Motherboards\Gigabyte\LGA 1700\AERO\WLAN1"
-$AEROWiFi2 = "C:\Drivers\Motherboards\Gigabyte\LGA 1700\AERO\WLAN2"
+$AEROWiFi1 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\AERO\WLAN1"
+$AEROWiFi2 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\AERO\WLAN2"
 
 Edit-OSDCloudWinPE -DriversPath $AEROWiFi1
 Edit-OSDCloudWinPE -DriversPath $AEROWiFi2
 
 Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 AERO Motherboard Storage Drivers..." -Verbose 
-$AEROStorage1 = "C:\Drivers\Motherboards\Gigabyte\LGA 1700\AERO\Storage1"
-$AEROStorage2 = "C:\Drivers\Motherboards\Gigabyte\LGA 1700\AERO\Storage2"
+$AEROStorage1 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\AERO\Storage1"
+$AEROStorage2 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\AERO\Storage2"
 
 Edit-OSDCloudWinPE -DriversPath  $AEROStorage1
 Edit-OSDCloudWinPE -DriversPath  $AEROStorage2
