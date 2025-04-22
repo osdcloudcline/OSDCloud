23a9fee10e6b16fe8976320cd58e0da1d44f9fa3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-22 00:40:17 -0400

    Update Other.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/Other.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1700/Other.ps1
@@ -12,7 +12,7 @@ Edit-OSDCloudWinPE -DriverPath $OtherEthernet2
 
 Write-Host
 Write-Verbose "Processing: ASUS LGA 1700 Other Motherboard WiFi Drivers..." -Verbose 
-$OtherWiFi1 = "C:\Drivers\Motherboards\ASUS\LGA1700\Other\WLAN"
+$OtherWiFi1 = "C:\Drivers\Motherboards\ASUS\LGA1700\Other\WLAN1"
 $OtherWiFi2 = "C:\Drivers\Motherboards\ASUS\LGA1700\Other\WLAN2"
 
 Edit-OSDCloudWinPE -DriverPath $OtherWiFi1
