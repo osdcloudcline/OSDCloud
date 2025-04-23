cd92c9482e3f3d0778d533020f97c16dbc48bc5e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-23 10:09:09 -0400

    Update ALL-Gigabyte-LGA1700.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/Gigabyte/ALL-Gigabyte-LGA1700.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/Gigabyte/ALL-Gigabyte-LGA1700.ps1
@@ -238,6 +238,17 @@ Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
 Dismount-WindowsImage -Path $mountdir -Save
 
 #################################
+# Download Gigabyte AERO Socket LGA1700 motherboard drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: Gigabyte Socket LGA 1700 AERO motherboard drivers" -Verbose
+Write-Host
+
+$AERODrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/Gigabyte/Desktops/Intel/LGA%201700/AERO%20Motherboards/AERO.ps1")
+Invoke-Expression $($AERODrivers.Content)
+
+#################################
 # Download OSDCloud - Cloud Drivers
 #################################
 
