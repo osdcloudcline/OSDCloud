c5d88502de77325af0b53506e0a532ff0ee6a937
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-27 14:34:54 -0400

    Update ALL-SocketLGA1700.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/By CPU and Socket Type/Intel Socket 1700/ALL-SocketLGA1700.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/By CPU and Socket Type/Intel Socket 1700/ALL-SocketLGA1700.ps1
@@ -688,6 +688,13 @@ $AORUS = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/h
 Invoke-Expression $($AORUS.Content)
 
 Write-Host
+Write-Verbose "Processing: Integrating Gigabyte Socket LGA 1700 Eagle motherboard drivers" -Verbose
+Write-Host
+
+$Eagle = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/Gigabyte/Motherboards/Intel/LGA%201700/Eagle.ps1")
+Invoke-Expression $($Eagle.Content)
+
+Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
 Write-Host
 
