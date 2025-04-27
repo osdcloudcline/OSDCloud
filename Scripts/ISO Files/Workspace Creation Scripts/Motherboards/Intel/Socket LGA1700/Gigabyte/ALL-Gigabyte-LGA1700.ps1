0e39a0e24facb61bf193466ee2f0f6d34f7d5a27
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-04-27 14:23:04 -0400

    Update ALL-Gigabyte-LGA1700.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/Gigabyte/ALL-Gigabyte-LGA1700.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/Gigabyte/ALL-Gigabyte-LGA1700.ps1
@@ -357,7 +357,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating Gigabyte Socket LGA 1700 AERO motherboard drivers" -Verbose
+Write-Host
 
+$AERO = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/Gigabyte/Motherboards/Intel/LGA%201700/AERO.ps1")
+Invoke-Expression $($AERO.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
