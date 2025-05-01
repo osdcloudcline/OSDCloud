e43e8799e6683f0c44ae47d3231e5c611a0a1d37
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-01 00:04:58 -0400

    Update Gaming.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/Intel/LGA 1700/Gaming.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/Intel/LGA 1700/Gaming.ps1
@@ -12,9 +12,11 @@ Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 Gaming Motherboard WLAN Drivers..." -Verbose 
 $GamingWLAN1 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Gaming\WLAN1"
 $GamingWLAN2 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Gaming\WLAN2"
+$GamingWLAN3 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Gaming\WLAN3"
 
 Edit-OSDCloudWinPE -DriversPath $GamingWLAN1
 Edit-OSDCloudWinPE -DriversPath $GamingWLAN2
+Edit-OSDCloudWinPE -DriversPath $GamingWLAN3
 
 Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 Gaming Motherboard Storage Drivers..." -Verbose 
