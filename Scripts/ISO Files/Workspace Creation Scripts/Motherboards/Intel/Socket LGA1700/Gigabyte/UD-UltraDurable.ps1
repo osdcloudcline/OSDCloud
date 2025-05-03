b937a94ef6cfdd164c231d8b0f29356349788397
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-03 13:27:17 -0400

    Update UD-UltraDurable.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/Gigabyte/UD-UltraDurable.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/Gigabyte/UD-UltraDurable.ps1
@@ -334,7 +334,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating Gigabyte Socket LGA 1700 Ultra Durable motherboard drivers" -Verbose
+Write-Host
 
+$UltraDurable = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/Gigabyte/Motherboards/Intel/LGA%201700/UD-Ultra%20Durable.ps1")
+Invoke-Expression $($UltraDurable.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
