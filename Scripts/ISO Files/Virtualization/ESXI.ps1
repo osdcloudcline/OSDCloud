b09c8de042ee105daa5080ab4b15f9c735fa37b2
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-09 23:33:34 -0400

    Update ESXI.ps1

--- Scripts/ISO Files/Virtualization/ESXI.ps1
+++ Scripts/ISO Files/Virtualization/ESXI.ps1
@@ -1,96 +1,12 @@
-############################################
-# Other Drivers
-############################################
-
-# Virtualization Drivers - VMWare ESXI
+#################################
+# OSDCloud - ESXI VT Drivers
+#################################
 
 Write-Host
-Write-Verbose "Processing: VMWare ESXI vSphere Ethernet Drivers..." -Verbose 
-$ESXIEthernet = "C:\OSDCloud\Drivers\Virtualization\ESXI\Network"
+Write-Verbose "Processing: VMWare ESXI Virtualization Drivers..." -Verbose 
 
-Edit-OSDCloudWinPE -DriverPath  $ESXIEthernet
+$ESXIEthernet = "C:\Drivers\Virtualization\ESXI\Network"
+$ESXIStorage = "C:\Drivers\Virtualization\ESXI\Storage"
 
-Write-Host
-Write-Verbose "Processing: VMWare ESXI vSphere Storage Drivers..." -Verbose 
-$ESXIStorage = "C:\OSDCloud\Drivers\Virtualization\ESXI\Storage"
-
+Edit-OSDCloudWinPE -DriverPath $ESXIEthernet
 Edit-OSDCloudWinPE -DriverPath $ESXIStorage
-
-Write-Host
-Write-Verbose "Completed: Integration of VMWare ESXI vSphere Network and Storage Drivers..." -Verbose
-Write-Host
-
-########################################
-# 3rd party software and utilities
-########################################
-
-# Portable Browsers
-
-Write-Host
-Write-Verbose "Processing: Google Chrome Portable Browser for OSDCloud..." -Verbose 
-$ChromePath = "C:\OSDCloud\GitHub\downloads\Chrome.exe"
-$ChromeDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $ChromePath -Destination $ChromeDestination -Force
-
-Write-Host
-Write-Verbose "Completed: Integration of Google Chrome Portable Browser for OSDCloud..." -Verbose
-Write-Host
-
-# MS DaRT Remote Connections and Troubleshooting
-
-# CloudPC
-
-Write-Host
-Write-Verbose "Processing: Microsoft Remote Desktop - Cloud PC for OSDCloud..." -Verbose 
-$CloudPCPath1 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files"
-$CloudPCPath2 = "C:\OSDCloud\downloads\GitHub\CloudPC\Sys32Files\en-us"
-$CloudPCDestination1 = "$mountdir\Windows\System32"
-$CloudPCDestination2 = "$mountdir\Windows\System32\en-US"
-
-Copy-Item -Path "$CloudPCPath1\*" -Destination $CloudPCDestination1  -Force
-Copy-Item -Path "$CloudPCPath2\*" -Destination $CloudPCDestination2  -Force
-
-# CMTrace 
-
-Write-Verbose "Processing: Microsoft Endpoint Configuration Manager Log Viewer CM Trace for OSDCloud..." -Verbose 
-$CMTracePath = "C:\OSDCloud\downloads\GitHub\MECM-LogViewer"
-$CMTraceDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $CMTracePath -Destination $CMTraceDestination -Force
-
-# Ghost Imaging
-Write-Host
-Write-Verbose "Processing: Ghost Imaging for OSDCloud..." -Verbose 
-$Ghost64Path = "C:\OSDCloud\GitHub\downloads\Ghost\Ghost64.exe"
-$GhostExplorerPath = "C:\OSDCloud\GitHub\downloads\Ghost\Ghostexp.exe"
-$GhostServPath = "C:\OSDCloud\GitHub\downloads\Ghost\GhostSrv.exe"
-$GhostDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $Ghost64Path -Destination $GhostDestination -Force
-Copy-Item -Path $GhostExplorerPath -Destination $GhostDestination -Force
-Copy-Item -Path $GhostServPath -Destination $GhostDestination -Force
-
-Write-Host
-Write-Verbose "Completed: Integration of Ghost Imaging for OSDCloud..." -Verbose
-Write-Host
-
-# User Profile Backup and Restore 
-
-Write-Host
-Write-Verbose "Processing: User Profile Backup/Restore for OSDCloud..." -Verbose 
-$UPBRFilePath = "C:\OSDCloud\GitHub\downloads\UserProfileBackupRestore.exe"
-$UBPRDestination = "$mountdir\Windows\System32"
-
-Copy-Item -Path $UPBRFilePath -Destination $UBPRDestination -Force
-
-Write-Host
-Write-Verbose "Completed: Integration of User Profile Backup/Restore for OSDCloud..." -Verbose
-Write-Host
-
-# Disount the image
-Write-Host
-Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
-
-Dismount-WindowsImage -Path $mountdir -Save
-
