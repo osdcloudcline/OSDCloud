4f88773ec04eac7c2bdb22c89c5b0ac9fc4b7ad1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-09 23:40:26 -0400

    Update ALL-VT-Drivers.ps1

--- Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
+++ Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
@@ -46,6 +46,11 @@ Edit-OSDCloudWinPE -DriverPath $ProxmoxPath4
 Write-Host
 Write-Verbose "Processing: VMWare Workstation Pro Virtualization Drivers..." -Verbose 
 
+$VMWareWSPROPath1 = "C:\Drivers\Virtualization\VMWareWSPRO\Network"
+$VMWareWSPROPath2 = "C:\Drivers\Virtualization\VMWareWSPRO\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $VMWareWSPROPath1
+Edit-OSDCloudWinPE -DriverPath $VMWareWSPROPath2
 
 #################################
 # OSDCloud - unRAID VT Drivers
