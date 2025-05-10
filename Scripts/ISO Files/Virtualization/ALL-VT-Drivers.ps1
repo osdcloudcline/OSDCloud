17193e59533e36a3787a366d65f01d48c3147229
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-09 23:35:37 -0400

    Update ALL-VT-Drivers.ps1

--- Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
+++ Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
@@ -18,6 +18,9 @@ Edit-OSDCloudWinPE -DriverPath $ESXIStorage
 Write-Host
 Write-Verbose "Processing: Microsoft Hyper-V Virtualization Drivers..." -Verbose 
 
+$HyperVEthernet = "C:\Drivers\Virtualization\HyperV\Network"
+
+Edit-OSDCloudWinPE -DriverPath $HyperVEthernet
 
 #################################
 # OSDCloud - Proxmox VT Drivers
