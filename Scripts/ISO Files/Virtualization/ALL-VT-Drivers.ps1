74699e0ecf4e3dfbfdd84b25fc284c719d66bd45
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-09 23:38:16 -0400

    Update ALL-VT-Drivers.ps1

--- Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
+++ Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
@@ -29,6 +29,16 @@ Edit-OSDCloudWinPE -DriverPath $HyperVEthernet
 Write-Host
 Write-Verbose "Processing: Proxmox Virtualization Drivers..." -Verbose 
 
+$ProxmoxPath1 = "C:\Drivers\Virtualization\Proxmox\Network\Windows11"
+$ProxmoxPath2 = "C:\Drivers\Virtualization\Proxmox\Network\WindowsServer2025"
+$ProxmoxPath3 = "C:\Drivers\Virtualization\Proxmox\Storage\Windows11"
+$ProxmoxPath4 = "C:\Drivers\Virtualization\Proxmox\Storage\WindowsServer2025"
+
+Edit-OSDCloudWinPE -DriverPath $ProxmoxPath1
+Edit-OSDCloudWinPE -DriverPath $ProxmoxPath2
+Edit-OSDCloudWinPE -DriverPath $ProxmoxPath3
+Edit-OSDCloudWinPE -DriverPath $ProxmoxPath4
+
 #################################
 # OSDCloud - VMWare Workstation Pro VT Drivers
 #################################
