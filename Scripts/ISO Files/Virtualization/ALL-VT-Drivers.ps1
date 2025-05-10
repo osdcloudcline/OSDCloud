e171c2f1d348c4896a6e98badf39d04dd14162e3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-09 23:29:00 -0400

    Update ALL-VT-Drivers.ps1

--- Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
+++ Scripts/ISO Files/Virtualization/ALL-VT-Drivers.ps1
@@ -2,10 +2,38 @@
 # OSDCloud - ESXI VT Drivers
 #################################
 
+Write-Host
+Write-Verbose "Processing: VMWare ESXI Virtualization Drivers..." -Verbose 
 
 
 
 
+#################################
+# OSDCloud - Hyper-V VT Drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: Microsoft Hyper-V Virtualization Drivers..." -Verbose 
 
 
+#################################
+# OSDCloud - Proxmox VT Drivers
+#################################
 
+Write-Host
+Write-Verbose "Processing: Proxmox Virtualization Drivers..." -Verbose 
+
+#################################
+# OSDCloud - VMWare Workstation Pro VT Drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: VMWare Workstation Pro Virtualization Drivers..." -Verbose 
+
+
+#################################
+# OSDCloud - unRAID VT Drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: unRAID Virtualization Drivers..." -Verbose 
