e2ecdb91adaf72feb340c830c3c2b6f909a80885
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-10 09:52:29 -0400

    Update HyperV.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Virtualization/HyperV.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Virtualization/HyperV.ps1
@@ -284,6 +284,24 @@ Write-Host
 # Integrate Drivers
 ########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating Microsoft Hyper-V Virtualization Technology Drivers" -Verbose
+Write-Host
+
+$HyperV = Invoke-WebRequest("")
+Invoke-Expression $($HyperV.Content)
+
+Write-Host
+Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
+Write-Host
+
+$CloudDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Cloud%20Drivers/CloudDrivers.ps1")
+Invoke-Expression $($CloudDrivers.Content) 
+
+Write-Host
+Write-Verbose "Completed: Integrating Drivers" -Verbose
+Write-Host
+
 ##########################################
 # OSDCloud WebScript for Startnet.cmd
 ##########################################
