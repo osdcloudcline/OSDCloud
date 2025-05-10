5d9565a2ff7f401542223eebdf6307e2ca032c66
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-10 09:49:25 -0400

    Update ESXI.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Virtualization/ESXI.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Virtualization/ESXI.ps1
@@ -285,6 +285,24 @@ Write-Host
 # Integrate Drivers
 ########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating VMWare ESXI Virtualization Technology Drivers" -Verbose
+Write-Host
+
+$ESXI = Invoke-WebRequest("")
+Invoke-Expression $($ESXI.Content)
+
+Write-Host
+Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
+Write-Host
+
+$CloudDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Cloud%20Drivers/CloudDrivers.ps1")
+Invoke-Expression $($CloudDrivers.Content) 
+
+Write-Host
+Write-Verbose "Completed: Integrating Drivers" -Verbose
+Write-Host
+
 ##########################################
 # OSDCloud WebScript for Startnet.cmd
 ##########################################
