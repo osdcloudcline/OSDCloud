8e6178e510843cc465dcabd2e1029e35b6c240ad
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:37:59 -0400

    Update MPG.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MPG.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MPG.ps1
@@ -6,25 +6,22 @@ Write-Host
 Write-Verbose "Processing: MSI AM 5 MPG Motherboard Ethernet Drivers..." -Verbose 
 $MPGEthernet = "C:\Drivers\Motherboards\MSI\AM5\MPG\Ethernet"
 
-Edit-OSDCloudWinPE -DriversPath $MPGEthernet
+Edit-OSDCloudWinPE -DriverPath $MPGEthernet
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 MPG Motherboard WiFi Drivers..." -Verbose 
 $MPGWiFi1 = "C:\Drivers\Motherboards\MSI\AM5\MPG\WLAN1"
 $MPGWiFi2 = "C:\Drivers\Motherboards\MSI\AM5\MPG\WLAN2"
 
-Edit-OSDCloudWinPE -DriversPath $MPGWiFi1
-Edit-OSDCloudWinPE -DriversPath $MPGWiFi2
+Edit-OSDCloudWinPE -DriverPath $MPGWiFi1
+Edit-OSDCloudWinPE -DriverPath $MPGWiFi2
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 MPG Motherboard Storage Drivers..." -Verbose 
 $MPGStorage = "C:\Drivers\Motherboards\MSI\AM5\MPG\Storage\RAID\NVMe"
 
-Edit-OSDCloudWinPE -DriversPath $MPGStorage
+Edit-OSDCloudWinPE -DriverPath $MPGStorage
 
 Write-Host
 Write-Verbose "Completed: Integration of MSI AMD AM 5 MPG Motherboard Drivers..." -Verbose 
 Write-Host
-
-
-
