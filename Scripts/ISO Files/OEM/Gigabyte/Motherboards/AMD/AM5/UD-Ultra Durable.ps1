2ef68d2c37958e548405414b5444efd2db0f291a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:31:05 -0400

    Update UD-Ultra Durable.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/UD-Ultra Durable.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/UD-Ultra Durable.ps1
@@ -9,10 +9,10 @@ $UDEthernet2 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\A620\Ethern
 $UDEthernet3 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\B650\Ethernet1"
 $UDEthernet4 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\B650\Ethernet2"
 
-Edit-OSDCloudWinPE -DriversPath $UDEthernet1
-Edit-OSDCloudWinPE -DriversPath $UDEthernet2
-Edit-OSDCloudWinPE -DriversPath $UDEthernet3
-Edit-OSDCloudWinPE -DriversPath $UDEthernet4
+Edit-OSDCloudWinPE -DriverPath $UDEthernet1
+Edit-OSDCloudWinPE -DriverPath $UDEthernet2
+Edit-OSDCloudWinPE -DriverPath $UDEthernet3
+Edit-OSDCloudWinPE -DriverPath $UDEthernet4
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 Ultra Durable Motherboard WiFi Drivers..." -Verbose 
@@ -25,22 +25,22 @@ $UDWLAN6 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\B650\WLAN4"
 $UDWLAN7 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\B650\WLAN5"
 $UDWLAN8 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\B650\WLAN6"
 
-Edit-OSDCloudWinPE -DriversPath $UDWLAN1
-Edit-OSDCloudWinPE -DriversPath $UDWLAN2
-Edit-OSDCloudWinPE -DriversPath $UDWLAN3
-Edit-OSDCloudWinPE -DriversPath $UDWLAN4
-Edit-OSDCloudWinPE -DriversPath $UDWLAN5
-Edit-OSDCloudWinPE -DriversPath $UDWLAN6
-Edit-OSDCloudWinPE -DriversPath $UDWLAN7
-Edit-OSDCloudWinPE -DriversPath $UDWLAN8
+Edit-OSDCloudWinPE -DriverPath $UDWLAN1
+Edit-OSDCloudWinPE -DriverPath $UDWLAN2
+Edit-OSDCloudWinPE -DriverPath $UDWLAN3
+Edit-OSDCloudWinPE -DriverPath $UDWLAN4
+Edit-OSDCloudWinPE -DriverPath $UDWLAN5
+Edit-OSDCloudWinPE -DriverPath $UDWLAN6
+Edit-OSDCloudWinPE -DriverPath $UDWLAN7
+Edit-OSDCloudWinPE -DriverPath $UDWLAN8
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 Ultra Durable Motherboard Storage Drivers..." -Verbose 
 $UDStorage1 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\A620\Storage\RAID\NVMe"
 $UDStorage2 = "C:\Drivers\Motherboards\Gigabyte\AM5\UD-UltraDurable\B650\Storage\RAID\NVMe"
 
-Edit-OSDCloudWinPE -DriversPath $UDStorage1
-Edit-OSDCloudWinPE -DriversPath $UDStorage2
+Edit-OSDCloudWinPE -DriverPath $UDStorage1
+Edit-OSDCloudWinPE -DriverPath $UDStorage2
 
 Write-Host
 Write-Verbose "Completed: Integration of Gigabyte AMD AM 5 Ultra Durable Motherboard Drivers..." -Verbose 
