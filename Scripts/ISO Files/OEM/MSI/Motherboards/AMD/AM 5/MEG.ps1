5a808ba227a968bbb7e4694154c1ec0d5a2e6388
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:37:20 -0400

    Update MEG.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MEG.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/MEG.ps1
@@ -1,4 +1,3 @@
-
 ##############################
 # MSI AM 5 Motherboards
 ###############################
@@ -12,26 +11,25 @@ $MEGEthernet2 = "C:\Drivers\Motherboards\MSI\AM5\MEG\Ethernet2"
 $MEGEthernet3 = "C:\Drivers\Motherboards\MSI\AM5\MEG\Ethernet3"
 $MEGEthernet4 = "C:\Drivers\Motherboards\MSI\AM5\MEG\Ethernet4"
 
-Edit-OSDCloudWinPE -DriversPath $MEGEthernet1
-Edit-OSDCloudWinPE -DriversPath $MEGEthernet2
-Edit-OSDCloudWinPE -DriversPath $MEGEthernet3
-Edit-OSDCloudWinPE -DriversPath $MEGEthernet4
+Edit-OSDCloudWinPE -DriverPath $MEGEthernet1
+Edit-OSDCloudWinPE -DriverPath $MEGEthernet2
+Edit-OSDCloudWinPE -DriverPath $MEGEthernet3
+Edit-OSDCloudWinPE -DriverPath $MEGEthernet4
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 MEG Motherboard WiFi Drivers..." -Verbose 
 $MEGWiFi1 = "C:\Drivers\Motherboards\MSI\AM5\MEG\WLAN1"
 $MEGWiFi2 = "C:\Drivers\Motherboards\MSI\AM5\MEG\WLAN2"
 
-Edit-OSDCloudWinPE -DriversPath $MEGWiFi1
-Edit-OSDCloudWinPE -DriversPath $MEGWiFi2
+Edit-OSDCloudWinPE -DriverPath $MEGWiFi1
+Edit-OSDCloudWinPE -DriverPath $MEGWiFi2
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 MEG Motherboard Storage Drivers..." -Verbose 
 $MEGStorage = "C:\Drivers\Motherboards\MSI\AM5\MEG\Storage\RAID\NVMe"
 
-Edit-OSDCloudWinPE -DriversPath $MEGStorage
+Edit-OSDCloudWinPE -DriverPath $MEGStorage
 
 Write-Host
 Write-Verbose "Completed: Integration of MSI AMD AM 5 MEG Motherboard Drivers..." -Verbose 
 Write-Host
-
