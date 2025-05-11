d8a6031be03b81e3cd460de4992e36209fca66da
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:38:48 -0400

    Update PRO.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/PRO.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/PRO.ps1
@@ -1,8 +1,12 @@
+##############################
+# MSI AM 5 Motherboards
+###############################
+
 Write-Host
 Write-Verbose "Processing: MSI AM 5 PRO Motherboard Ethernet Drivers..." -Verbose 
 $PROEthernet = "C:\Drivers\Motherboards\MSI\AM5\PRO\Ethernet"
 
-Edit-OSDCloudWinPE -DriversPath $PROEthernet
+Edit-OSDCloudWinPE -DriverPath $PROEthernet
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 PRO Motherboard WiFi Drivers..." -Verbose 
@@ -10,18 +14,16 @@ $PROWiFi1 = "C:\Drivers\Motherboards\MSI\AM5\PRO\WLAN1"
 $PROWiFi2 = "C:\Drivers\Motherboards\MSI\AM5\PRO\WLAN2"
 $PROWiFi3 = "C:\Drivers\Motherboards\MSI\AM5\PRO\WLAN3"
 
-Edit-OSDCloudWinPE -DriversPath $PROWiFi1
-Edit-OSDCloudWinPE -DriversPath $PROWiFi2
-Edit-OSDCloudWinPE -DriversPath $PROWiFi3
+Edit-OSDCloudWinPE -DriverPath $PROWiFi1
+Edit-OSDCloudWinPE -DriverPath $PROWiFi2
+Edit-OSDCloudWinPE -DriverPath $PROWiFi3
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 PRO Motherboard Storage Drivers..." -Verbose 
 $PROStorage = "C:\Drivers\Motherboards\MSI\AM5\PRO\Storage\RAID\NVMe"
 
-Edit-OSDCloudWinPE -DriversPath $PROStorage
+Edit-OSDCloudWinPE -DriverPath $PROStorage
 
 Write-Host
 Write-Verbose "Completed: Integration of MSI AMD AM 5 PRO Motherboard Drivers..." -Verbose 
 Write-Host
-
-
