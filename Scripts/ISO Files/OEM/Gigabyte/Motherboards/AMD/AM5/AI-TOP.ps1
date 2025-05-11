814c7aaff5f4673b738c874193c9dbb2a1c14fd6
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:28:33 -0400

    Update AI-TOP.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/AI-TOP.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/AI-TOP.ps1
@@ -7,20 +7,20 @@ Write-Verbose "Processing: Gigabyte AM 5 AI TOP Motherboard Ethernet Drivers..."
 $AITOPEthernet1 = "C:\Drivers\Motherboards\Gigabyte\AM5\AI-TOP\Ethernet1"
 $AITOPEthernet2 = "C:\Drivers\Motherboards\Gigabyte\AM5\AI-TOP\Ethernet2"
 
-Edit-OSDCloudWinPE -DriversPath $AITOPEthernet1
-Edit-OSDCloudWinPE -DriversPath $AITOPEthernet2
+Edit-OSDCloudWinPE -DriverPath $AITOPEthernet1
+Edit-OSDCloudWinPE -DriverPath $AITOPEthernet2
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 AI TOP Motherboard WiFi Drivers..." -Verbose 
 $AITOPWLAN = "C:\Drivers\Motherboards\Gigabyte\AM5\AI-TOP\WLAN"
 
-Edit-OSDCloudWinPE -DriversPath $AITOPWLAN
+Edit-OSDCloudWinPE -DriverPath $AITOPWLAN
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 AI TOP Motherboard Storage Drivers..." -Verbose 
 $AITOPStorage = "C:\Drivers\Motherboards\Gigabyte\AM5\AI-TOP\Storage"
 
-Edit-OSDCloudWinPE -DriversPath  $AITOPStorage
+Edit-OSDCloudWinPE -DriverPath  $AITOPStorage
 
 Write-Host
 Write-Verbose "Completed: Integration of Gigabyte AMD AM 5 AI TOP Motherboard Drivers..." -Verbose 
