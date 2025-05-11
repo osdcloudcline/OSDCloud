bd895a9ecfb5eee4f15602d01415699dcea2a562
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:11:31 -0400

    Update PRO.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/PRO.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/PRO.ps1
@@ -6,13 +6,13 @@ Write-Host
 Write-Verbose "Processing: ASRock Intel LGA 1700 PRO Motherboard drivers..." -Verbose
 Write-Host
 
-$PROEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet1"
-$PROEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet2"
-$PROEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet3"
-$PROEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet4"
-$PROEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet5"
-$PROEthernetPath6 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet6"
-$PROStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\PRO\Storage"
+$PROEthernetPath1 = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet1"
+$PROEthernetPath2 = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet2"
+$PROEthernetPath3 = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet3"
+$PROEthernetPath4 = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet4"
+$PROEthernetPath5 = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet5"
+$PROEthernetPath6 = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Ethernet6"
+$PROStoragePath = "C:\Drivers\Motherboards\ASRock\LGA1700\PRO\Storage"
 
 Edit-OSDCloudWinPe -DriverPath $PROEthernetPath1
 Edit-OSDCloudWinPe -DriverPath $PROEthernetPath2
@@ -25,6 +25,10 @@ Edit-OSDCloudWinPe -DriverPath $PROStoragePath
 Write-Host
 Write-Verbose "Completed: ASRock Intel LGA 1700 PRO Motherboard drivers..." -Verbose
 Write-Host
+
+
+
+
 
 
 
