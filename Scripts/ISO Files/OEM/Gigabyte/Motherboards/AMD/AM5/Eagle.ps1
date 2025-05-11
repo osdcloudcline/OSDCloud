9ad0bd8d9393e25fc85949708f9f16b73fabdcce
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:29:49 -0400

    Update Eagle.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Eagle.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/Eagle.ps1
@@ -9,10 +9,10 @@ $EagleEthernet2 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\B650\Ethernet2"
 $EagleEthernet3 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\X870\Ethernet1"
 $EagleEthernet4 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\X870\Ethernet2"
 
-Edit-OSDCloudWinPE -DriversPath $EagleEthernet1
-Edit-OSDCloudWinPE -DriversPath $EagleEthernet2
-Edit-OSDCloudWinPE -DriversPath $EagleEthernet3
-Edit-OSDCloudWinPE -DriversPath $EagleEthernet4
+Edit-OSDCloudWinPE -DriverPath $EagleEthernet1
+Edit-OSDCloudWinPE -DriverPath $EagleEthernet2
+Edit-OSDCloudWinPE -DriverPath $EagleEthernet3
+Edit-OSDCloudWinPE -DriverPath $EagleEthernet4
 
 
 Write-Host
@@ -22,10 +22,10 @@ $EagleWLAN2 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\B650\WLAN2"
 $EagleWLAN3 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\X870\WLAN1"
 $EagleWLAN4 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\X870\WLAN2"
 
-Edit-OSDCloudWinPE -DriversPath $EagleWLAN1
-Edit-OSDCloudWinPE -DriversPath $EagleWLAN2
-Edit-OSDCloudWinPE -DriversPath $EagleWLAN3
-Edit-OSDCloudWinPE -DriversPath $EagleWLAN4
+Edit-OSDCloudWinPE -DriverPath $EagleWLAN1
+Edit-OSDCloudWinPE -DriverPath $EagleWLAN2
+Edit-OSDCloudWinPE -DriverPath $EagleWLAN3
+Edit-OSDCloudWinPE -DriverPath $EagleWLAN4
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 Eagle Motherboard Storage Drivers..." -Verbose 
@@ -33,8 +33,8 @@ $EagleStorage1 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\B650\Storage\RAID\N
 $EagleStorage2 = "C:\Drivers\Motherboards\Gigabyte\AM5\Eagle\X870\Storage\RAID\NVMe"
 
 
-Edit-OSDCloudWinPE -DriversPath $EagleStorage1
-Edit-OSDCloudWinPE -DriversPath $EagleStorage2
+Edit-OSDCloudWinPE -DriverPath $EagleStorage1
+Edit-OSDCloudWinPE -DriverPath $EagleStorage2
 
 Write-Host
 Write-Verbose "Completed: Integration of Gigabyte AMD AM 5 Eagle Motherboard Drivers..." -Verbose 
