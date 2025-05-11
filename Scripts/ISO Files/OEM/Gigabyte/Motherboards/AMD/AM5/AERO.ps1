97339cde64afdfd91e9ca0cf77cca10ad762b3ab
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:27:59 -0400

    Update AERO.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/AERO.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/AMD/AM5/AERO.ps1
@@ -6,21 +6,20 @@ Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 AERO Motherboard Ethernet Drivers..." -Verbose 
 $AEROEthernet = "C:\Drivers\Motherboards\Gigabyte\AM5\AERO\Ethernet"
 
-Edit-OSDCloudWinPE -DriversPath $AEROEthernet
+Edit-OSDCloudWinPE -DriverPath $AEROEthernet
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 AERO Motherboard WiFi Drivers..." -Verbose 
 $AEROWLAN = "C:\Drivers\Motherboards\Gigabyte\AM5\AERO\WLAN"
 
-Edit-OSDCloudWinPE -DriversPath $AEROWLAN
+Edit-OSDCloudWinPE -DriverPath $AEROWLAN
 
 Write-Host
 Write-Verbose "Processing: Gigabyte AM 5 AERO Motherboard Storage Drivers..." -Verbose 
 $AEROStorage = "C:\Drivers\Motherboards\Gigabyte\AM5\AERO\Storage"
 
-Edit-OSDCloudWinPE -DriversPath  $AEROStorage
+Edit-OSDCloudWinPE -DriverPath  $AEROStorage
 
 
 Write-Host
 Write-Verbose "Completed: Integration of Gigabyte AMD AM 5 AERO Motherboard Drivers..." -Verbose 
-
