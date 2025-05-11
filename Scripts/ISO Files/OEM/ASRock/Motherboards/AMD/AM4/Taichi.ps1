48af2e53c0668a84e24cec8b6f6c8439cf0055ea
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:07:24 -0400

    Update Taichi.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/Taichi.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/Taichi.ps1
@@ -6,21 +6,21 @@ Write-Host
 Write-Verbose "Processing: ASRock AM 4 Taichi Motherboard Ethernet Drivers..." -Verbose 
 $TaichiEthernet = "C:\Drivers\Motherboards\ASRock\AM4\Taichi\Ethernet"
 
-Edit-OSDCloudWinPE -DriversPath $TaichiEthernet
+Edit-OSDCloudWinPE -DriverPath $TaichiEthernet
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Taichi Motherboard WiFi Drivers..." -Verbose 
 $TaichiWLAN = "C:\Drivers\Motherboards\ASRock\AM4\Taichi\WiFi"
 
-Edit-OSDCloudWinPE -DriversPath $TaichiWLAN
+Edit-OSDCloudWinPE -DriverPath $TaichiWLAN
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Taichi Motherboard Storage Drivers..." -Verbose 
 $TaichiStorage1 = "C:\Drivers\Motherboards\ASRock\AM4\Taichi\Storage1"
 $TaichiStorage2 = "C:\Drivers\Motherboards\ASRock\AM4\Taichi\Storage2"
 
-Edit-OSDCloudWinPE -DriversPath $TaichiStorage1
-Edit-OSDCloudWinPE -DriversPath $TaichiStorage2
+Edit-OSDCloudWinPE -DriverPath $TaichiStorage1
+Edit-OSDCloudWinPE -DriverPath $TaichiStorage2
 
 Write-Host
 Write-Verbose "Completed: Integration of ASRock AMD AM 4 Taichi Motherboard Drivers..." -Verbose 
