e1f826b95da35682283f26a6948cf4a56abb3f04
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-10 22:39:13 -0400

    Update MAG.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/MSI/MAG.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/MSI/MAG.ps1
@@ -238,6 +238,17 @@ Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
 Dismount-WindowsImage -Path $mountdir -Save
 
 #################################
+# Download MSI MAG Series Socket LGA1700 motherboard drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: MSI Socket LGA 1700 MAG Series motherboard Drivers..." -Verbose
+Write-Host
+
+$MAGDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/MSI/Desktops/Intel/LGA%201700/MAG%20Series/MAG.ps1")
+Invoke-Expression $($MAGDrivers.Content)
+
+#################################
 # Download OSDCloud - Cloud Drivers
 #################################
 
