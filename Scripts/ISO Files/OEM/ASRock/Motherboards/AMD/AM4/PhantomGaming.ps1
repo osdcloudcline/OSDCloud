f498bc7d83eb5a3729ef36e5d8e6fe7ebf64b9b5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:06:09 -0400

    Update PhantomGaming.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PhantomGaming.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/PhantomGaming.ps1
@@ -11,25 +11,25 @@ $PhantomGamingEthernet3 = "C:\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethe
 $PhantomGamingEthernet4 = "C:\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet4"
 $PhantomGamingEthernet5 = "C:\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Ethernet5"
 
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet1
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet2
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet3
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet4
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingEthernet5
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingEthernet1
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingEthernet2
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingEthernet3
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingEthernet4
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingEthernet5
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Phantom Gaming Motherboard WiFi Drivers..." -Verbose 
 $PhantomGamingWLAN = "C:\Drivers\Motherboards\ASRock\AM4\PhantomGaming\WiFi"
 
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingWiFi
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingWiFi
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Phantom Gaming Motherboard Storage Drivers..." -Verbose 
 $PhantomGamingStorage1 = "C:\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Storage1"
 $PhantomGamingStorage2 = "C:\Drivers\Motherboards\ASRock\AM4\PhantomGaming\Storage2"
 
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingStorage1
-Edit-OSDCloudWinPE -DriversPath $PhantomGamingStorage2
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingStorage1
+Edit-OSDCloudWinPE -DriverPath $PhantomGamingStorage2
 
 Write-Host
 Write-Verbose "Completed: Integration of ASRock AMD AM 4 Motherboard Drivers..." -Verbose 
