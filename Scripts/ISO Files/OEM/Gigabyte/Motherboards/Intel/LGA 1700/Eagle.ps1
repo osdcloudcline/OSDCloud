543f4d12165350bc92089f798069878a44a4cf9a
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:33:09 -0400

    Update Eagle.ps1

--- Scripts/ISO Files/OEM/Gigabyte/Motherboards/Intel/LGA 1700/Eagle.ps1
+++ Scripts/ISO Files/OEM/Gigabyte/Motherboards/Intel/LGA 1700/Eagle.ps1
@@ -8,21 +8,18 @@ Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 Eagle Motherboard Ethernet Drivers..." -Verbose 
 $EagleEthernet = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Eagle\Ethernet"
 
-Edit-OSDCloudWinPE -DriversPath $EagleEthernet
+Edit-OSDCloudWinPE -DriverPath $EagleEthernet
 
 Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 Eagle Motherboard WiFi Drivers..." -Verbose 
 $EagleWiFi1 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Eagle\WLAN1"
 $EagleWiFi2 = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Eagle\WLAN2"
 
-Edit-OSDCloudWinPE -DriversPath $EagleWiFi1
-Edit-OSDCloudWinPE -DriversPath $EagleWiFi2
+Edit-OSDCloudWinPE -DriverPath $EagleWiFi1
+Edit-OSDCloudWinPE -DriverPath $EagleWiFi2
 
 Write-Host
 Write-Verbose "Processing: Gigabyte LGA 1700 Eagle Motherboard Storage Drivers..." -Verbose 
 $EagleStorage = "C:\Drivers\Motherboards\Gigabyte\LGA1700\Eagle\Storage"
 
-Edit-OSDCloudWinPE -DriversPath $EagleStorage
-
-
-
+Edit-OSDCloudWinPE -DriverPath $EagleStorage
