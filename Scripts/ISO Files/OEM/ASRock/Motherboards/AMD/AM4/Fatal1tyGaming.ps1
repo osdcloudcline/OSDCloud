96a41163bddf9ff5f60aac88abba3783f0abbf0b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:04:58 -0400

    Update Fatal1tyGaming.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/Fatal1tyGaming.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/Fatal1tyGaming.ps1
@@ -8,25 +8,24 @@ Write-Verbose "Processing: ASRock AM 4 Fatal1ty Gaming Motherboard Ethernet Driv
 $Fatal1tyGamingEthernet1 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Ethernet1"
 $Fatal1tyGamingEthernet2 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Ethernet2"
 
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingEthernet1
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingEthernet2
+Edit-OSDCloudWinPE -DriverPath $Fatal1tyGamingEthernet1
+Edit-OSDCloudWinPE -DriverPath $Fatal1tyGamingEthernet2
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Fatal1ty Gaming Motherboard WiFi Drivers..." -Verbose 
 $Fatal1tyGamingWiFi1 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\WiFi1"
 $Fatal1tyGamingWiFi2 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\WiFi2"
 
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingWiFi1
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingWiFi2
+Edit-OSDCloudWinPE -DriverPath $Fatal1tyGamingWiFi1
+Edit-OSDCloudWinPE -DriverPath $Fatal1tyGamingWiFi2
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Extreme Motherboard Storage Drivers..." -Verbose 
 $Fatal1tyGamingStorage1 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Storage1"
 $Fatal1tyGamingStorage2 = "C:\Drivers\Motherboards\ASRock\AM4\Fatal1tyGaming\Storage2"
 
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingStorage1
-Edit-OSDCloudWinPE -DriversPath $Fatal1tyGamingStorage2
+Edit-OSDCloudWinPE -DriverPath $Fatal1tyGamingStorage1
+Edit-OSDCloudWinPE -DriverPath $Fatal1tyGamingStorage2
 
 Write-Host
 Write-Verbose "Completed: Integration of ASRock AMD AM 4 Motherboard Drivers..." -Verbose 
-
