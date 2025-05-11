cc65fec95185cccb4525afb84789b0c82e041ac5
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 11:08:01 -0400

    Update MEG.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/MSI/MEG.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1700/MSI/MEG.ps1
@@ -238,6 +238,17 @@ Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
 Dismount-WindowsImage -Path $mountdir -Save
 
 #################################
+# Download MSI MEG Series Socket LGA1700 motherboard drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: MSI Socket LGA 1700 MEG Series motherboard Drivers..." -Verbose
+Write-Host
+
+$MEGDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/MSI/Desktops/Intel/LGA%201700/MEG%20Series/MEG.ps1")
+Invoke-Expression $($MEGDrivers.Content)
+
+#################################
 # Download OSDCloud - Cloud Drivers
 #################################
 
