0681c9ad86b2527417df5bc74d9c5e27e94c64f1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 01:05:17 -0400

    Update BryanDesktop.ps1

--- Scripts/ISO Files/Custom/BryanDesktop.ps1
+++ Scripts/ISO Files/Custom/BryanDesktop.ps1
@@ -1,4 +1,17 @@
+Write-Host
 Write-Verbose "Processing: Bryan Desktop - Intel Core Ultra 9 285K ASUS Z890-A ROG STRIX LGA1851 Drivers..." -Verbose
+Write-Host
 
+$BryanDesktopEthernet = "C:\Drivers\Custom\Bryan Desktop\ASUS Z890-A STRIX\LGA1851\Ethernet"
+$BryanDesktopStorage = "C:\Drivers\Custom\Bryan Desktop\ASUS Z890-A STRIX\LGA1851\Storage"
+$BryanDesktopWLANPath = "C:\Drivers\Custom\Bryan Desktop\ASUS Z890-A STRIX\LGA1851\WLAN"
+
+Edit-OSDCloudWinPe -DriverPath $BryanDesktopEthernetPath
+Edit-OSDCloudWinPe -DriverPath $BryanDesktopStorage
+Edit-OSDCloudWinPe -DriverPath $BryanDesktopWLAN
+
+Write-Host
+Write-Verbose "Completed: Bryan Desktop - Intel Core Ultra 9 285K ASUS Z890-A ROG STRIX LGA1851 Drivers..." -Verbose
+Write-Host
 
 
