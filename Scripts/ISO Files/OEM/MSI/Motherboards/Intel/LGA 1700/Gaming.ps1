b06561eaa6506356c328f261b94ad273728626ba
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:39:30 -0400

    Update Gaming.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/Intel/LGA 1700/Gaming.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/Intel/LGA 1700/Gaming.ps1
@@ -9,22 +9,22 @@ Write-Verbose "Processing: MSI LGA 1700 Gaming Motherboard Ethernet Drivers..." 
 $GamingEthernet1 = "C:\Drivers\Motherboards\MSI\LGA1700\Gaming\Ethernet1"
 $GamingEthernet2 = "C:\Drivers\Motherboards\MSI\LGA1700\Gaming\Ethernet2"
 
-Edit-OSDCloudWinPE -DriversPath $GamingEthernet1
-Edit-OSDCloudWinPE -DriversPath $GamingEthernet2
+Edit-OSDCloudWinPE -DriverPath $GamingEthernet1
+Edit-OSDCloudWinPE -DriverPath $GamingEthernet2
 
 Write-Host
 Write-Verbose "Processing: MSI LGA 1700 Gaming Motherboard WLAN Drivers..." -Verbose 
 $GamingWLAN1 = "C:\Drivers\Motherboards\MSI\LGA1700\Gaming\WLAN1"
 $GamingWLAN2 = "C:\Drivers\Motherboards\MSI\LGA1700\Gaming\WLAN2"
 
-Edit-OSDCloudWinPE -DriversPath $GamingWLAN1
-Edit-OSDCloudWinPE -DriversPath $GamingWLAN2
+Edit-OSDCloudWinPE -DriverPath $GamingWLAN1
+Edit-OSDCloudWinPE -DriverPath $GamingWLAN2
 
 Write-Host
 Write-Verbose "Processing: MSI LGA 1700 Gaming Motherboard Storage Drivers..." -Verbose 
 $GamingStorage = "C:\Drivers\Motherboards\MSI\LGA1700\Gaming\Storage"
 
-Edit-OSDCloudWinPE -DriversPath $GamingStorage
+Edit-OSDCloudWinPE -DriverPath $GamingStorage
 
 
 Write-Host
