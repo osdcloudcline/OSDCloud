88c9a975081603b30047ed62280c6473f85590ad
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:35:44 -0400

    Update Gaming.ps1

--- Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/Gaming.ps1
+++ Scripts/ISO Files/OEM/MSI/Motherboards/AMD/AM 5/Gaming.ps1
@@ -8,7 +8,7 @@ Write-Host
 Write-Verbose "Processing: MSI AM 5 Gaming Motherboard Ethernet Drivers..." -Verbose 
 $GamingEthernet = "C:\Drivers\Motherboards\MSI\AM5\Gaming\Ethernet"
 
-Edit-OSDCloudWinPE -DriversPath $GamingEthernet
+Edit-OSDCloudWinPE -DriverPath $GamingEthernet
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 Gaming Motherboard WiFi Drivers..." -Verbose 
@@ -16,17 +16,16 @@ $GamingWiFi1 = "C:\Drivers\Motherboards\MSI\AM5\Gaming\WLAN1"
 $GamingWiFi2 = "C:\Drivers\Motherboards\MSI\AM5\Gaming\WLAN2"
 $GamingWiFi3 = "C:\Drivers\Motherboards\MSI\AM5\Gaming\WLAN3"
 
-Edit-OSDCloudWinPE -DriversPath $GamingWiFi1
-Edit-OSDCloudWinPE -DriversPath $GamingWiFi2
-Edit-OSDCloudWinPE -DriversPath $GamingWiFi3
+Edit-OSDCloudWinPE -DriverPath $GamingWiFi1
+Edit-OSDCloudWinPE -DriverPath $GamingWiFi2
+Edit-OSDCloudWinPE -DriverPath $GamingWiFi3
 
 Write-Host
 Write-Verbose "Processing: MSI AM 5 Gaming Motherboard Storage Drivers..." -Verbose 
 $GamingStorage = "C:\Drivers\Motherboards\MSI\AM5\Gaming\Storage\RAID\NVMe"
 
-Edit-OSDCloudWinPE -DriversPath $GamingStorage
+Edit-OSDCloudWinPE -DriverPath $GamingStorage
 
 Write-Host
 Write-Verbose "Completed: Integration of MSI AMD AM 5 Gaming Motherboard Drivers..." -Verbose 
 Write-Host
-
