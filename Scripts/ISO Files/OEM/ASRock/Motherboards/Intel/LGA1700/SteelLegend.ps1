3dcbe804267097428aa60966d95ce3a7e9669f7b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:12:32 -0400

    Update SteelLegend.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/SteelLegend.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/Intel/LGA1700/SteelLegend.ps1
@@ -6,14 +6,14 @@ Write-Host
 Write-Verbose "Processing: ASRock Intel LGA 1700 Steel Legend Motherboard drivers..." -Verbose
 Write-Host
 
-$SteelLegendEthernetPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet1"
-$SteelLegendEthernetPath2 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet2"
-$SteelLegendEthernetPath3 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet3"
-$SteelLegendEthernetPath4 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet4"
-$SteelLegendEthernetPath5 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet5"
-$SteelLegendEthernetPath6 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet6"
-$SteelLegendStoragePath = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Storage"
-$SteelLegendWLANPath1 = "C:\OSDCloud\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\WLAN1"
+$SteelLegendEthernetPath1 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet1"
+$SteelLegendEthernetPath2 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet2"
+$SteelLegendEthernetPath3 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet3"
+$SteelLegendEthernetPath4 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet4"
+$SteelLegendEthernetPath5 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet5"
+$SteelLegendEthernetPath6 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Ethernet6"
+$SteelLegendStoragePath = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\Storage"
+$SteelLegendWLANPath1 = "C:\Drivers\Motherboards\ASRock\LGA1700\SteelLegend\WLAN1"
 
 Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath1
 Edit-OSDCloudWinPe -DriverPath $SteelLegendEthernetPath2
@@ -27,5 +27,4 @@ Edit-OSDCloudWinPe -DriverPath $SteelLegendWLANPath1
 Write-Host
 Write-Verbose "Completed: ASRock Intel LGA 1700 Steel Legend Motherboard drivers..." -Verbose
 Write-Host
-
 
