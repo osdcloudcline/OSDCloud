cc1e9c0ac1a1a609f41b0a6395438ebd87019adb
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-11 00:06:41 -0400

    Update SteelLegend.ps1

--- Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/SteelLegend.ps1
+++ Scripts/ISO Files/OEM/ASRock/Motherboards/AMD/AM4/SteelLegend.ps1
@@ -10,24 +10,24 @@ $SteelLegendEthernet2 = "C:\Drivers\Motherboards\ASRock\AM4\SteelLegend\Ethernet
 $SteelLegendEthernet3 = "C:\Drivers\Motherboards\ASRock\AM4\SteelLegend\Ethernet3"
 $SteelLegendEthernet4 = "C:\Drivers\Motherboards\ASRock\AM4\SteelLegend\Ethernet4"
 
-Edit-OSDCloudWinPE -DriversPath $SteelLegendEthernet1
-Edit-OSDCloudWinPE -DriversPath $SteelLegendEthernet2
-Edit-OSDCloudWinPE -DriversPath $SteelLegendEthernet3
-Edit-OSDCloudWinPE -DriversPath $SteelLegendEthernet4
+Edit-OSDCloudWinPE -DriverPath $SteelLegendEthernet1
+Edit-OSDCloudWinPE -DriverPath $SteelLegendEthernet2
+Edit-OSDCloudWinPE -DriverPath $SteelLegendEthernet3
+Edit-OSDCloudWinPE -DriverPath $SteelLegendEthernet4
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Steel Legend Motherboard WiFi Drivers..." -Verbose 
 $SteelLegendWLAN = "C:\Drivers\Motherboards\ASRock\AM4\SteelLegend\WiFi"
 
-Edit-OSDCloudWinPE -DriversPath $SteelLegendWiFi
+Edit-OSDCloudWinPE -DriverPath $SteelLegendWiFi
 
 Write-Host
 Write-Verbose "Processing: ASRock AM 4 Steel Legend Motherboard Storage Drivers..." -Verbose 
 $SteelLegendStorage1 = "C:\Drivers\Motherboards\ASRock\AM4\SteelLegend\Storage1"
 $SteelLegendStorage2 = "C:\Drivers\Motherboards\ASRock\AM4\SteelLegend\Storage2"
 
-Edit-OSDCloudWinPE -DriversPath $SteelLegendStorage1
-Edit-OSDCloudWinPE -DriversPath $SteelLegendStorage2
+Edit-OSDCloudWinPE -DriverPath $SteelLegendStorage1
+Edit-OSDCloudWinPE -DriverPath $SteelLegendStorage2
 
 Write-Host
 Write-Verbose "Completed: Integration of ASRock AMD AM 4 Motherboard Drivers..." -Verbose 
