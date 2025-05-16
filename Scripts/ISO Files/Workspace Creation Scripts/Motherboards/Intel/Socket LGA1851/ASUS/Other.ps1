14cb4e268ab5d6b0f1912f3dc1c7c3f787c7a5ff
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-16 10:43:15 -0400

    Update Other.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/Other.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/Other.ps1
@@ -238,6 +238,17 @@ Write-Verbose "Processing: Dismounting OSDCloud boot.wim" -Verbose
 Dismount-WindowsImage -Path $mountdir -Save
 
 #################################
+# Download ASUS LGA 1851 Other motherboard drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 Other motherboard Drivers..." -Verbose
+Write-Host
+
+$OtherDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Other%20Motherboards/Other.ps1")
+Invoke-Expression $($OtherDrivers.Content)
+
+#################################
 # Download OSDCloud - Cloud Drivers
 #################################
 
