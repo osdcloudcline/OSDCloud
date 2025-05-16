0d5f5cb1c5ada613332dd6d961a6df3a61f9d87b
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-16 10:33:23 -0400

    Update ALL-ASUS-LGA1851.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/ALL-ASUS-LGA1851.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/ALL-ASUS-LGA1851.ps1
@@ -246,8 +246,19 @@ Write-Host
 Write-Verbose "Processing: ASUS LGA 1851 Business motherboard Drivers..." -Verbose
 Write-Host
 
-$BusinessdDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Business.ps1")
-Invoke-Expression $($BusinessdDrivers.Content)
+$BusinessDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/Business%20Motherbords/Business.ps1")
+Invoke-Expression $($BusinessDrivers.Content)
+
+#################################
+# Download ASUS LGA 1851 CSM motherboard drivers
+#################################
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 CSM motherboard Drivers..." -Verbose
+Write-Host
+
+$CSMDrivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloudDrivers/raw/refs/heads/main/Network%20and%20Storage%20/ASUS/Desktops/Intel/LGA%201851/CSM%20Motherboards/CSM.ps1")
+Invoke-Expression $($CSMDrivers.Content)
 
 #################################
 # Download OSDCloud - Cloud Drivers
