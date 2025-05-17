ba9954f244194c9430ea9d83c34cb2ad3e4dbc1c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-17 09:38:05 -0400

    Update ProArt.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/ProArt.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/ProArt.ps1
@@ -334,7 +334,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASUS Socket LGA 1851 ProArt motherboard drivers" -Verbose
+Write-Host
 
+$ProArt = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASUS/Motherboards/Intel/LGA%201851/ProArt.ps1")
+Invoke-Expression $($ProArt.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
