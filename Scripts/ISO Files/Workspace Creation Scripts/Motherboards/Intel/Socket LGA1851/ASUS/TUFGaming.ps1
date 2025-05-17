2eabd1390d289ccc53423b8277e2d5653d9ee672
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-17 09:39:48 -0400

    Update TUFGaming.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/TUFGaming.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/Intel/Socket LGA1851/ASUS/TUFGaming.ps1
@@ -334,7 +334,12 @@ Invoke-Expression $($ProxmoxDrivers.Content)
 # Integrate Drivers
 #########################################
 
+Write-Host
+Write-Verbose "Processing: Integrating ASUS Socket LGA 1851 TUF Gaming motherboard drivers" -Verbose
+Write-Host
 
+$TUFGaming = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/OEM/ASUS/Motherboards/Intel/LGA%201851/TUF%20Gaming.ps1")
+Invoke-Expression $($TUFGaming.Content)
 
 Write-Host
 Write-Verbose "Processing: Integrating OSDCloud - Cloud Drivers" -Verbose
