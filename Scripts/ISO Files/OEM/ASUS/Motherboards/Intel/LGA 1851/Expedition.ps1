6d2de4d7065e6b62066d47674e2519188460f4f8
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-17 07:43:57 -0400

    Update Expedition.ps1

--- Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/Expedition.ps1
+++ Scripts/ISO Files/OEM/ASUS/Motherboards/Intel/LGA 1851/Expedition.ps1
@@ -1,1 +1,17 @@
+###############################
+# ASUS LGA 1851 Motherboards
+################################
 
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 Expedition Motherboard Ethernet Drivers..." -Verbose 
+$ExpeditionEthernet1 = "C:\Drivers\Motherboards\ASUS\LGA1851\Expedition\Ethernet1"
+$ExpeditionEthernet2 = "C:\Drivers\Motherboards\ASUS\LGA1851\Expedition\Ethernet2"
+
+Edit-OSDCloudWinPE -DriverPath $ExpeditionEthernet1
+Edit-OSDCloudWinPE -DriverPath $ExpeditionEthernet2
+
+Write-Host
+Write-Verbose "Processing: ASUS LGA 1851 Expedition Motherboard Storage Drivers..." -Verbose 
+$ExpeditionStorage = "C:\Drivers\Motherboards\ASUS\LGA1851\Expedition\Storage"
+
+Edit-OSDCloudWinPE -DriverPath $ExpeditionStorage
