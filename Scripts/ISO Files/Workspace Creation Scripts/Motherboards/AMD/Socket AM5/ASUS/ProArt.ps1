131822201585a183fbd6531d1605a6798c8ea1db
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:14:22 -0400

    Update ProArt.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/ProArt.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/ProArt.ps1
@@ -379,8 +379,8 @@ $OSDCloudISOPath = "C:\OSDCloud\ASUS-ProArt-AM5"
 
 Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
 Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ASUS-ProArt-AM5\OSDCloud-ASUS-ProArt-AM5.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ASUS-ProArt-AM5\OSDCloud-ASUS-ProArt-AM5_NoPrompt.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ASUS-ProArt-AM5\OSDCloud-Version 1.0-ASUS-ProArt-AM5.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ASUS-ProArt-AM5\OSDCloud-Version 1.0-ASUS-ProArt-AM5_NoPrompt.iso" -Force
 
 ###########################################
 # Copy OSDCloud ISO Files
