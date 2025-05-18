78b76d5ff806315f254721b92d0bf1887544619d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:24:55 -0400

    Update MEG.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/MSI/MEG.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/MSI/MEG.ps1
@@ -379,15 +379,15 @@ $OSDCloudISOPath = "C:\OSDCloud\MSI-MEG-AM5"
 
 Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
 Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-MSI-MEG-AM5.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-MSI-MEG-AM5_NoPrompt.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-Version 1.0-MSI-MEG-AM5.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-Version 1.0-MSI-MEG-AM5_NoPrompt.iso" -Force
 
 ###########################################
 # Copy OSDCloud ISO Files
 #########################################
 
-$ISO1 = "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-MSI-MEG-AM5.iso"
-$ISO2 = "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-MSI-MEG-AM5_NoPrompt.iso"
+$ISO1 = "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-Version 1.0-MSI-MEG-AM5.iso"
+$ISO2 = "C:\OSDCloud\MSI-MEG-AM5\OSDCloud-Version 1.0-MSI-MEG-AM5_NoPrompt.iso"
 
 $OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM5\MSI"
 
