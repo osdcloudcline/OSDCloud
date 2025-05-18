47b7ae88b130086abc947422fd39cbba285462ec
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:28:58 -0400

    Update ALL-SocketAM4.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/By CPU and Socket Type/AMD Socket AM4/ALL-SocketAM4.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/By CPU and Socket Type/AMD Socket AM4/ALL-SocketAM4.ps1
@@ -705,15 +705,15 @@ $OSDCloudISOPath = "C:\OSDCloud\ALL-AM4"
 
 Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
 Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ALL-AM4\OSDCloud-ALL-AM4.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ALL-AM4\OSDCloud-ALL-AM4_NoPrompt.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ALL-AM4\OSDCloud-Version 1.0-ALL-AM4.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ALL-AM4\OSDCloud-Version 1.0-ALL-AM4_NoPrompt.iso" -Force
 
 ###########################################
 # Copy OSDCloud ISO Files
 #########################################
 
-$ISO1 = "C:\OSDCloud\ALL-AM4\OSDCloud-ALL-AM4.iso"
-$ISO2 = "C:\OSDCloud\ALL-AM4\OSDCloud-ALL-AM4_NoPrompt.iso"
+$ISO1 = "C:\OSDCloud\ALL-AM4\OSDCloud-Version 1.0-ALL-AM4.iso"
+$ISO2 = "C:\OSDCloud\ALL-AM4\OSDCloud-Version 1.0-ALL-AM4_NoPrompt.iso"
 
 $OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM4"
 
