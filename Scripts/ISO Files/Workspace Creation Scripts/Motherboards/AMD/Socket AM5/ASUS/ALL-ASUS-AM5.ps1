f88aa8e08abb9bab62f9d9bb991a3cbae3ef302d
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:09:11 -0400

    Update ALL-ASUS-AM5.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/ALL-ASUS-AM5.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/ASUS/ALL-ASUS-AM5.ps1
@@ -505,15 +505,15 @@ $OSDCloudISOPath = "C:\OSDCloud\ALL-ASUS-AM5"
 
 Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
 Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-ALL-ASUS-AM5.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-ALL-ASUS-AM5_NoPrompt.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-Version 1.0-ALL-ASUS-AM5.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-Version 1.0-ALL-ASUS-AM5_NoPrompt.iso" -Force
 
 ###########################################
 # Copy OSDCloud ISO Files
 #########################################
 
-$ISO1 = "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-ALL-ASUS-AM5.iso"
-$ISO2 = "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-ALL-ASUS-AM5_NoPrompt.iso"
+$ISO1 = "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-Version 1.0-ALL-ASUS-AM5.iso"
+$ISO2 = "C:\OSDCloud\ALL-ASUS-AM5\OSDCloud-Version 1.0-ALL-ASUS-AM5_NoPrompt.iso"
 
 $OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM5\ASUS"
 
