f8e17fc7f0f6eb7c2fb75f063bf826da22d12988
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:43:42 -0400

    Update Other.ps1

--- Scripts/ISO Files/Virtualization/Other.ps1
+++ Scripts/ISO Files/Virtualization/Other.ps1
@@ -69,5 +69,22 @@ Edit-OSDCloudWinPE -DriverPath $ProxmoxStorage3
 Edit-OSDCloudWinPE -DriverPath $ProxmoxStorage4
 
 Write-Host
+Write-Verbose "Processing: unRAID Virtualization Drivers..." -Verbose 
+
+$unRAIDPath1 = "C:\Drivers\Virtualization\unRAID\Network1"
+$unRAIDPath2 = "C:\Drivers\Virtualization\unRAID\Network2"
+$unRAIDPath3 = "C:\Drivers\Virtualization\unRAID\Storage1"
+$unRAIDPath4 = "C:\Drivers\Virtualization\unRAID\FileSystem1"
+$unRAIDPath5 = "C:\Drivers\Virtualization\unRAID\Storage2"
+$unRAIDPath6 = "C:\Drivers\Virtualization\unRAID\FileSystem2"
+
+Edit-OSDCloudWinPE -DriverPath $unRAIDPath1
+Edit-OSDCloudWinPE -DriverPath $unRAIDPath2
+Edit-OSDCloudWinPE -DriverPath $unRAIDPath3
+Edit-OSDCloudWinPE -DriverPath $unRAIDPath4
+Edit-OSDCloudWinPE -DriverPath $unRAIDPath5
+Edit-OSDCloudWinPE -DriverPath $unRAIDPath6
+
+Write-Host
 Write-Verbose "Completed: Integration of Proxmox Virtualized IO Network and Storage Drivers..." -Verbose
 Write-Host
