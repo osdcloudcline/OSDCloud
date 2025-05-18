eb3f34ca4187d85f899700ef83d49832cdf7e862
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:16:11 -0400

    Update AERO.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/Gigabyte/AERO.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM5/Gigabyte/AERO.ps1
@@ -379,15 +379,15 @@ $OSDCloudISOPath = "C:\OSDCloud\Gigabyte-AERO-AM5"
 
 Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
 Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Gigabyte-AERO-AM5.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Gigabyte-AERO-AM5_NoPrompt.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Version 1.0-Gigabyte-AERO-AM5.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Version 1.0-Gigabyte-AERO-AM5_NoPrompt.iso" -Force
 
 ###########################################
 # Copy OSDCloud ISO Files
 #########################################
 
-$ISO1 = "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Gigabyte-AERO-AM5.iso"
-$ISO2 = "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Gigabyte-AERO-AM5_NoPrompt.iso"
+$ISO1 = "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Version 1.0-Gigabyte-AERO-AM5.iso"
+$ISO2 = "C:\OSDCloud\Gigabyte-AERO-AM5\OSDCloud-Version 1.0-Gigabyte-AERO-AM5_NoPrompt.iso"
 
 $OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM5\Gigabyte"
 
