b8c74bb052a23e2e93d95c2cd7f027ba7006fbc1
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-05-18 18:04:54 -0400

    Update Gaming.ps1

--- Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/Gigabyte/Gaming.ps1
+++ Scripts/ISO Files/Workspace Creation Scripts/Motherboards/AMD/Socket AM4/Gigabyte/Gaming.ps1
@@ -379,15 +379,15 @@ $OSDCloudISOPath = "C:\OSDCloud\Gigabyte-Gaming-AM4"
 
 Write-Verbose "Renaming OSDCloud ISO Files..." -Verbose
 Write-Host
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Gigabyte-Gaming-AM4.iso" -Force
-Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Gigabyte-Gaming-AM4_NoPrompt.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud.iso" -NewName "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Version 1.0-Gigabyte-Gaming-AM4.iso" -Force
+Rename-Item -Path "$OSDCloudISOPath\OSDCloud_NoPrompt.iso" -NewName "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Version 1.0-Gigabyte-Gaming-AM4_NoPrompt.iso" -Force
 
 ###########################################
 # Copy OSDCloud ISO Files
 #########################################
 
-$ISO1 = "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Gigabyte-Gaming-AM4.iso"
-$ISO2 = "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Gigabyte-Gaming-AM4_NoPrompt.iso"
+$ISO1 = "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Version 1.0-Gigabyte-Gaming-AM4.iso"
+$ISO2 = "C:\OSDCloud\Gigabyte-Gaming-AM4\OSDCloud-Version 1.0-Gigabyte-Gaming-AM4_NoPrompt.iso"
 
 $OSDISODestination = "C:\ISOs\OSDCloud\AMD\Socket AM4\Gigabyte"
 
