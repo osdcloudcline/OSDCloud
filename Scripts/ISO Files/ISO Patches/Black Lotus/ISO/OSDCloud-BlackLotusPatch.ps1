665f8fe464c4e56e81d3ebf33ef52c200d657e13
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2025-09-14 02:09:59 -0400

    Update OSDCloud-BlackLotusPatch.ps1

--- Scripts/ISO Files/ISO Patches/Black Lotus/ISO/OSDCloud-BlackLotusPatch.ps1
+++ Scripts/ISO Files/ISO Patches/Black Lotus/ISO/OSDCloud-BlackLotusPatch.ps1
@@ -1,3 +1,13 @@
 Import-Module -Name OSD
 
-$bootmgrfw_exEFIURL = ""
+$bootmgrfw_exEFIURL = "https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/ISO%20Patches/Black%20Lotus/ISO/bootmgfw_EX.efi"
+
+$tempdestination = "C:\OSDCloud\downloads\ISO Patches\Black Lotus"
+
+New-Item -Path $tempdestination -ItemType Directory -Force
+
+Save-WebFile -SourceUrl $bootmgrfw_exEFIURL -DestinationDirectory $tempdestination 
+
+
+Rename-Item -Path "C:\OSDCloud\downloads\ISO Patches\Black Lotus\bootmgfw_EX.efi"  -NewName "C:\OSDCloud\downloads\ISO Patches\Black Lotus\bootmgfw.efi"
+
